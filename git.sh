fc852a5784e569adeca2bc27e6b52f3952022432
sherylynn <352281674@qq.com>
2017-06-13 09:27:35 +0800

    git

---
+++ git.sh
@@ -0,0 +1,4 @@
+git config --global user.name "sherylynn"
+git config --global user.email "352281674@qq.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=2678400'
