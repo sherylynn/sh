cd73b76a6af0db78c2bf4796eb73a7ac3fcc45fd
sherylynn <352281674@qq.com>
2017-10-05 13:59:59 +0800

    git clone project

--- git.sh
+++ git.sh
@@ -2,3 +2,12 @@ git config --global user.name "sherylynn"
 git config --global user.email "352281674@qq.com"
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=2678400'
+
+#-----------------------
+# git clone project
+#-----------------------
+#git clone https://github.com/sherylynn/sign_admin.git ~/sign_admin
+#git clone https://github.com/sherylynn/sign_server.git ~/sign_server
+#git clone https://github.com/sherylynn/sign_db.git ~/sign_db
+git clone --recursive https://github.com/sherylynn/plugins4rmmv ~/plugins4rmmv
+git clone https://github.com/sherylynn/server4rmmv.git
