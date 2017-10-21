ce02a2c729c316d26f04a68d2be453869f0b1d1b
sherylynn <352281674@qq.com>
2017-10-21 13:22:16 +0800

    sudo

--- v2ray_docker.sh
+++ v2ray_docker.sh
@@ -1,2 +1,2 @@
 sudo apt install docker.io
-docker run -itd --name v2ray --net=host -v $HOME:/etc/v2ray v2ray/official
+sudo docker run -itd --name v2ray --net=host -v $HOME:/etc/v2ray v2ray/official
