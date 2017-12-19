8f1405801e4b10830f1efbe97f6a5ae1cc6c4b53
sherylynn <352281674@qq.com>
2017-12-19 10:13:32 +0800

    zero建立moon

--- zerotier.sh
+++ zerotier.sh
@@ -1,3 +1,11 @@
-
+只用在moon主机上执行所有命令
+zerotier-idtool initmoon /var/lib/zerotier-one/identity.public >> moon.json
+zerotier-idtool genmoon moon.json
+sudo mkdir /var/lib/zerotier-one/moons.d
+cp
+zerotier-cli listpeers
+zerotier-cli orbit e64d65a908 111.231.90.43
 sudo zerotier-cli join 12ac4a1e71394190
 sudo zerotier-cli set 12ac4a1e71394190 allowGlobal=1
+
+用moon直连的速度貌似只有moon的上限
