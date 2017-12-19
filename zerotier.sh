547c25ec8d21c3f33862700a895b5565da68c317
sherylynn <352281674@qq.com>
2017-12-19 15:16:18 +0800

    一些缺少的包

--- zerotier.sh
+++ zerotier.sh
@@ -10,7 +10,19 @@ cp
 sudo zerotier-cli listpeers
 sudo zerotier-cli orbit e64d65a908 111.231.90.43
 sudo zerotier-cli orbit 020ae11ef1 67.216.203.21
+sudo zerotier-cli deorbit 020ae11ef1
 sudo zerotier-cli join 12ac4a1e71394190
 sudo zerotier-cli set 12ac4a1e71394190 allowGlobal=1
 
 用moon直连的速度貌似只有moon的上限
+
+开启两个moon似乎第二个moon不能独立使用，并且对速度没有加成反而有消弱
+
+
+如果是systemd不全的机器
+sudo zerotier-idtool generate identity.secret
+sudo zerotier-idtool getpublic identity.secret
+echo 9993|sudo tee /var/lib/zerotier-one/zerotier-one.port
+sudo zerotier-idtool getpublic /var/lib/zerotier-one/identity.secret |sudo tee /var/lib/zerotier-one/identity.public
+
+缺少 authtoken
