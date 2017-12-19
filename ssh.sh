e43d4792eda0137f2c2573b6a9d88f3e778ba9ca
sherylynn <352281674@qq.com>
2017-12-19 14:32:39 +0800

    一些更新

---
+++ ssh.sh
@@ -0,0 +1,12 @@
+mkdir ~/.ssh
+cat ~/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+
+echo PasswordAuthentication no|sudo tee -a /etc/ssh/sshd_config
+sudo tee -a /etc/ssh/sshd_config <<-'EOF'
+AuthorizedKeysFile .ssh/authorized_keys
+PubkeyAuthentication yes
+EOF
+
+sudo service sshd restart
--- zerotier.sh
+++ zerotier.sh
@@ -1,10 +1,15 @@
+curl -s 'https://pgp.mit.edu/pks/lookup?op=get&search=0x1657198823E52A61' | gpg --import && \
+if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+
 只用在moon主机上执行所有命令
 zerotier-idtool initmoon /var/lib/zerotier-one/identity.public >> moon.json
 zerotier-idtool genmoon moon.json
 sudo mkdir /var/lib/zerotier-one/moons.d
+sudo cp 000000e64d65a908.moon /var/lib/zerotier-one/moons.d/
 cp
-zerotier-cli listpeers
-zerotier-cli orbit e64d65a908 111.231.90.43
+sudo zerotier-cli listpeers
+sudo zerotier-cli orbit e64d65a908 111.231.90.43
+sudo zerotier-cli orbit 020ae11ef1 67.216.203.21
 sudo zerotier-cli join 12ac4a1e71394190
 sudo zerotier-cli set 12ac4a1e71394190 allowGlobal=1
 
