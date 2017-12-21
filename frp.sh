a340bc1eb95c8e6a4eea3dd9ece34bb8e6a48f11
sherylynn <352281674@qq.com>
2017-12-21 22:46:48 +0800

    修复

--- frp.sh
+++ frp.sh
@@ -4,5 +4,5 @@ FRP_VERSION=0.14.1
 FRP_ARCH=amd64
 axel -n 10 https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
     tar -xzf frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
-sudo mv node-v${NODE_VERSION}-linux-${NODE_ARCH} ~/frp && \
+sudo mv frp_${FRP_VERSION}_linux_${FRP_ARCH} ~/frp && \
     rm frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz
