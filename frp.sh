38a89baac417133ba186d050b67a38c75de1c789
sherylynn <352281674@qq.com>
2017-12-21 22:48:33 +0800

    换成 wget 下载

--- frp.sh
+++ frp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt install axel -y
+sudo apt install axel wget -y
 FRP_VERSION=0.14.1
 FRP_ARCH=amd64
-axel -n 10 https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
+wget https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
+#axel -n 10 https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
     tar -xzf frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
 sudo mv frp_${FRP_VERSION}_linux_${FRP_ARCH} ~/frp && \
     rm frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz
