a17a50be105e6ccf803f100d199b6eb6d9e205f4
sherylynn <352281674@qq.com>
2017-12-22 08:06:06 +0800

    change to aria2

--- frp.sh
+++ frp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt install axel wget -y
+sudo apt install axel wget aria2 -y
 FRP_VERSION=0.14.1
 FRP_ARCH=amd64
-wget https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
+aria2c https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
 #axel -n 10 https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
     tar -xzf frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
 sudo mv frp_${FRP_VERSION}_linux_${FRP_ARCH} ~/frp && \
