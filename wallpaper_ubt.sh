f7499ea0959e5d9cf7078bf726bd4a11f6297b68
sherylynn <352281674@qq.com>
2017-12-22 10:39:13 +0800

    filepath

--- wallpaper_ubt.sh
+++ wallpaper_ubt.sh
@@ -3,8 +3,9 @@
 Size=1000
 Url=http://picsum.photos/${Size}/?random
 WALLS_PATH=$HOME/Wallpapers
-cd $WALLS_PATH
-
+if [ ! -d "${WALLS_PATH}" ]; then
+mkdir $WALLS_PATH
+fi
 wget $Url -O $HOME/Wallpapers/${Size}.jpg
 
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Wallpapers/${Size}.jpg"
