4e7cce185c8de742efb09ce008f036cd41d12d92
sherylynn <352281674@qq.com>
2017-12-22 09:47:55 +0800

    增加了服务写入

--- frp.sh
+++ frp.sh
@@ -3,13 +3,19 @@ sudo apt install axel wget aria2 -y
 #!/bin/bash
 FRP_VERSION=0.14.1
 FRP_ARCH=amd64
+Server=n
+Client=n
 #FRP_ARCH=arm
-while getopts 'v:a:' OPT; do
+while getopts 'v:a:sc' OPT; do
   case $OPT in
     v)
       FRP_VERSION="$OPTARG";;
     a)
       FRP_ARCH="$OPTARG";;
+    s)
+      Server="y";;
+    c)
+      Client="y";;
     ?)
       echo "Usage: `basename $0` [options] filename"
   esac
@@ -22,3 +28,47 @@ aria2c https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${F
     tar -xzf frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
 sudo mv frp_${FRP_VERSION}_linux_${FRP_ARCH} ~/frp && \
     rm frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz
+
+sudo mkdir /etc/frp
+sudo cp ~/frp/frps.ini /etc/frp/frps.ini
+sudo cp ~/frp/frpc.ini /etc/frp/frpc.ini
+
+sudo ln -s ~/frp/frps /usr/local/bin/frps
+sudo ln -s ~/frp/frpc /usr/local/bin/frpc
+if [ ${Server} = "y" ]; then  
+sudo tee /etc/systemd/system/frps.service <<-'EOF'
+[Unit]
+Description=frps Service
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/frps -c /etc/frp/frps.ini
+Restart=on-abnormal
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable frps.service
+sudo systemctl start frps.service
+fi  
+
+if [ ${Client} = "y" ]; then 
+sudo tee /etc/systemd/system/frpc.service <<-'EOF'
+[Unit]
+Description=frpc Service
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/frpc -c /etc/frp/frpc.ini
+Restart=on-abnormal
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable frpc.service
+sudo systemctl start frpc.service
+fi
