48ab50e667d901a37e9aa8724fd1779c860477e8
sherylynn <352281674@qq.com>
2017-12-22 08:34:34 +0800

    增加了参数

--- frp.sh
+++ frp.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 sudo apt install axel wget aria2 -y
+#!/bin/bash
 FRP_VERSION=0.14.1
 FRP_ARCH=amd64
+#FRP_ARCH=arm
+while getopts 'v:a:' OPT; do
+  case $OPT in
+    v)
+      FRP_VERSION="$OPTARG";;
+    a)
+      FRP_ARCH="$OPTARG";;
+    ?)
+      echo "Usage: `basename $0` [options] filename"
+  esac
+done
+
+shift $(($OPTIND - 1))
+
 aria2c https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
 #axel -n 10 https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
     tar -xzf frp_${FRP_VERSION}_linux_${FRP_ARCH}.tar.gz && \
