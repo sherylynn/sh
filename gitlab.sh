c0a833bee65a7bf15678c93896247317500de142
sherylynn <352281674@qq.com>
2017-12-29 08:09:19 +0800

    gitlab.sh

---
+++ gitlab.sh
@@ -0,0 +1,8 @@
+sudo docker run --detach \
+    --publish 8929:80 --publish 2289:22 \
+    --name gitlab \
+    --restart always \
+    --volume /srv/gitlab/config:/etc/gitlab \
+    --volume /srv/gitlab/logs:/var/log/gitlab \
+    --volume /srv/gitlab/data:/var/opt/gitlab \
+    gitlab/gitlab-ce:latest
