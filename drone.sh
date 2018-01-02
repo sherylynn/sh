77c465a346ab0bbd40bb410adce011668a8ace55
sherylynn <352281674@qq.com>
2018-01-02 22:33:48 +0800

    变量写入

--- drone.sh
+++ drone.sh
@@ -22,5 +22,17 @@ else
     echo export DRONE_SECRET=${DRONE_ENV_SECRET}|sudo tee -a $HOME/.bashrc
 fi 
 
+if [ $DRONE_HOST ];then  
+    echo DRONE_HOST=$DRONE_HOST |sudo tee -a /etc/drone/drone.env
+else
+    echo export DRONE_HOST=http://111.231.90.43/|sudo tee -a $HOME/.bashrc
+fi 
+
+
+if [ $DRONE_GOGS_URL ];then  
+    echo DRONE_GOGS_URL=$DRONE_GOGS_URL |sudo tee -a /etc/drone/drone.env
+else
+    echo export DRONE_GOGS_URL=http://111.231.90.43:3000/|sudo tee -a $HOME/.bashrc
+fi 
 
 docker-compose -f /etc/drone/docker-compose.yml up
