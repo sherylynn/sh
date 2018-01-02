c60040c7482cba2f20f4bcdb088c03db778ccb5e
sherylynn <352281674@qq.com>
2018-01-02 21:28:19 +0800

    test

--- drone.sh
+++ drone.sh
@@ -5,9 +5,15 @@ sudo mkdir /etc/drone
 sudo cp ~/sh/docker-compose.yml /etc/drone/
 
 sudo tee /etc/drone/drone.env <<-"EOF"
-GOGS_HOST=http://111.231.90.43:3000/
-DRONE_ENV_HOST=http://111.231.90.43/
+DRONE_HOST=http://111.231.90.43/
+DRONE_GOGS_URL=http://111.231.90.43:3000/
 EOF
-echo DRONE_ENV_SECRET="$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 65)" |sudo tee -a /etc/drone/drone.env
+
+if [ $DRONE_SECRET ];then  
+    echo DRONE_SECRET=$DRONE_SECRET |sudo tee -a /etc/drone/drone.env
+else  
+    echo DRONE_SECRET="$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 65)" |sudo tee -a /etc/drone/drone.env
+fi 
+
 
 docker-compose -f /etc/drone/docker-compose.yml up
