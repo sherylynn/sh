be8ccb72b81ff6397b23dc8b22b6802fe0102518
sherylynn <352281674@qq.com>
2018-01-02 21:14:19 +0800

    增加了环境变量的设置

--- drone.sh
+++ drone.sh
@@ -3,4 +3,11 @@ sudo apt install python-pip
 pip install docker-compose
 sudo mkdir /etc/drone
 sudo cp ~/sh/docker-compose.yml /etc/drone/
+
+sudo tee /etc/drone/drone.env <<-"EOF"
+GOGS_HOST=http://111.231.90.43:3000/
+DRONE_ENV_HOST=http://111.231.90.43/
+EOF
+echo DRONE_ENV_SECRET="$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 65)" |sudo tee -a /etc/drone/drone.env
+
 docker-compose -f /etc/drone/docker-compose.yml up
