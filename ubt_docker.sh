12f963303181e6eb67a0c9e9f15d900e8d857425
sherylynn <352281674@qq.com>
2018-01-12 09:59:10 +0800

    just for x86 not for amd64

--- ubt_docker.sh
+++
@@ -1,5 +0,0 @@
-sudo snap install docker
-if [ ! -f "/usr/local/bin/docker-compose" ]; then
-    sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-    sudo chmod 777 /usr/local/bin/docker-compose
-fi
---
+++ ubt_x86_docker.sh
@@ -0,0 +1,3 @@
+sudo snap install docker
+sudo apt install python-pip
+pip install docker-compose
