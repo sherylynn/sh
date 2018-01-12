59bf4b99c3a5aacdb9ce4f658c3cca6a17acf19d
sherylynn <352281674@qq.com>
2018-01-12 09:50:37 +0800

    compose

--- ubt_vps_docker.sh
+++ ubt_vps_docker.sh
@@ -26,4 +26,8 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker restart
 
+if [ ! -f "/usr/local/bin/docker-compose" ]; then
+    sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod 777 /usr/local/bin/docker-compose
+fi
 #echo DOCKER_OPTS="--registry-mirror=https://mirror.ccs.tencentyun.com" |sudo tee -a /etc/default/docker
