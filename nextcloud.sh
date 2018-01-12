5c285812020b85f1d9712c83fb0f0ab84b3172f4
sherylynn <352281674@qq.com>
2018-01-12 14:10:06 +0800

    更新设置

--- nextcloud.sh
+++ nextcloud.sh
@@ -2,7 +2,7 @@
 mkdir ~/data
 mkdir ~/data/cloud
 mkdir ~/data/cloud/mysql
-mkdir ~/data/cofig
-mkdir ~/data/apps
+mkdir ~/data/cloud/cofig
+mkdir ~/data/cloud/apps
 
 docker-compose -f ./sh/nextcloud.yml up -d
