b410a51e5885e82c34f5ae74a2623296ca35fd7e
sherylynn <352281674@qq.com>
2018-01-13 21:58:18 +0800

    新建了vm专版

---
+++ nextcloud_vm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+COMMAND="n"
+while getopts 'v:a:sc' OPT; do
+  case $OPT in
+    r)
+      COMMAND="y";;
+    s)
+      COMMAND="n";;
+    ?)
+      echo "Usage: `basename $0` [options] filename"
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+
+if [ ${COMMAND} = "y" ]; then
+docker-compose -f ./sh/nextcloud_vm.yml up -d
+fi
+if [ ${COMMAND} = "n" ]; then
+docker-compose -f ./sh/nextcloud_vm.yml stop
+fi
