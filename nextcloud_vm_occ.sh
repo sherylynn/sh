9c67c64806501bd915e5395491c8e4dbfc87b9dc
sherylynn <352281674@qq.com>
2018-01-16 12:22:40 +0800

    换成docker模式

---
+++ nextcloud_vm_occ.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+docker run -itd --name cloud_db --restart=always \
+  -v /mnt/hgfs/nextcloud/mysql:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD="nextcloud" \
+  -e MYSQL_DATABASE="nextcloud" \
+  mysql
+docker run -it --name occ --restart=always \
+  -v /mnt/hgfs/nextcloud/nextcloud:/var/www/html \
+  -v /mnt/hgfs/nextcloud/nextcloud:/usr/src/nextcloud \
+  --link cloud_db:db
