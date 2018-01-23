ee0b57cd867b938123e50fa501ad691c2c33fed4
sherylynn <352281674@qq.com>
2018-01-24 00:11:44 +0800

    更新了必备软件

--- seafile/seafile_install.sh
+++ seafile/seafile_install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#base
+#download seafile
 yum update -y
 yum install -y wget
 wget http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.2.4_x86-64.tar.gz
@@ -9,6 +9,12 @@ cd haiwen
 tar -xzf seafile-server_*
 mkdir installed
 mv seafile-server_* installed
+
+#base tool
+yum -y install epel-release
+rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+yum -y install python-imaging MySQL-python python-memcached python-ldap python-urllib3 ffmpeg ffmpeg-devel
+pip install pillow moviepy
 
 #memcached
 yum install gcc libffi-devel python-devel openssl-devel libmemcached libmemcached-devel
