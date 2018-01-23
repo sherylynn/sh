214a464cf9b3f8a237023a3c01891087a5820044
sherylynn <352281674@qq.com>
2018-01-23 21:31:13 +0800

    更新

--- seafile/seafile.sh
+++ seafile/seafile.sh
@@ -1,2 +1,3 @@
-docker build
-docker run
+docker build -t seafile .
+docker run -itd --name seafile_db --restart=always -v /mnt/hgfs/seafile/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="seafile" -e MYSQL_DATABASE="seafile" mysql
+docker run -itd --name seafile_app --restart=always -v /mnt/hgfs/seafile/seafile-data:/home/haiwen/seafile-data --link seafile_db:db seafile /bin/bash
