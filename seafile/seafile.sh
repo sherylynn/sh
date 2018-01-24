291301ed2bce6b05e7229b66fa672996bf751656
sherylynn <352281674@qq.com>
2018-01-25 00:35:54 +0800

    把下载放在外部

--- seafile/seafile.sh
+++ seafile/seafile.sh
@@ -1,3 +1,4 @@
 docker build -t seafile .
 docker run -itd --name seafile_db --restart=always -v /mnt/hgfs/seafile/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="seafile" -e MYSQL_DATABASE="seafile" mysql
-docker run -itd --name seafile_app --restart=always -v /mnt/hgfs/seafile/seafile-data:/home/haiwen/seafile-data --link seafile_db:db seafile /bin/bash
+#docker run -itd --name seafile_app --restart=always -p 8000:8000 -p 8082:8082 -v /mnt/hgfs/seafile/seafile-data:/home/haiwen/seafile-data --link seafile_db:db seafile /bin/bash
+docker run -itd --name seafile_app --restart=always -p 8000:8000 -p 8082:8082 -v /mnt/hgfs/seafile/haiwen:/home/haiwen --link seafile_db:db seafile /bin/bash
--- seafile/seafile_config.sh
+++ seafile/seafile_config.sh
@@ -1,4 +1,16 @@
 #/bin/bash
+#download seafile
+apt update -y
+apt install -y wget
+wget http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.2.4_x86-64.tar.gz
+if [ ! -d "/home/haiwen" ]; then
+sudo mkdir /home/haiwen
+fi
+mv seafile-server_* haiwen
+cd haiwen
+tar -xzf seafile-server_*
+mkdir installed
+mv seafile-server_* installed
 #configure
 cd seafile-server-*
 ./setup-seafile-mysql.sh
