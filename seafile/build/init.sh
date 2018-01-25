41624c9791cd3aa3bd437fa727452b0fe03a0bcf
sherylynn <352281674@qq.com>
2018-01-25 19:46:13 +0800

    fix_paramter

--- seafile/build/init.sh
+++ seafile/build/init.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 cd seafile-server-[0-9]* && \
   ./setup-seafile-mysql.sh auto -n sea -i 0.0.0.0 -p 8082 -d /home/haiwen/seafile-data/data \
-  -e 0 -o db -t 3306 -r seafile -u seafile -w seafile -q seafile -c ccnet-db \
+  -e 0 -o db -t 3306 -r seafile -u seafile -w seafile -q % -c ccnet-db \
   -s seafile-db -b seahub-db
