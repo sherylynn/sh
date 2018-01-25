fc4ba41a53001aff0c22344d84f5f5be8d6ad606
sherylynn <352281674@qq.com>
2018-01-25 17:10:00 +0800

    update rules

--- firewall.sh
+++ firewall.sh
@@ -10,6 +10,9 @@ firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --zone=public --add-port=26408/tcp --permanent
 firewall-cmd --zone=public --add-port=28000/tcp --permanent
 firewall-cmd --zone=public --add-port=29000/tcp --permanent
+firewall-cmd --zone=public --add-port=7000/tcp --permanent
+firewall-cmd --zone=public --add-port=8000/tcp --permanent
+firewall-cmd --zone=public --add-port=8082/tcp --permanent
 firewall-cmd --reload
 
 yum update -y
