079785fa3d3589bb8a2400de1f0f38ee5ec7c23c
sherylynn <352281674@qq.com>
2018-02-01 20:53:29 +0800

    port

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -2,5 +2,5 @@
 if [ ! -d "$HOME/gogs_data" ];then
 mkdir $HOME/gogs_data
 fi
-docker run -itd --name gogs --restart=always -p 10022:22 -p 10080:3000 -v $HOME/gogs_data:/data gogs/gogs
+docker run -itd --name gogs --restart=always -p 22000:22 -p 3000:3000 -v $HOME/gogs_data:/data gogs/gogs
 
