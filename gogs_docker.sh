8ea0c8d5a9f3f65ee1dfa0ee9b084bd6e2424630
sherylynn <352281674@qq.com>
2018-02-01 20:52:18 +0800

    if fi

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 if [ ! -d "$HOME/gogs_data" ];then
 mkdir $HOME/gogs_data
-if
-
+fi
 docker run -itd --name gogs --restart=always -p 10022:22 -p 10080:3000 -v $HOME/gogs_data:/data gogs/gogs
+
