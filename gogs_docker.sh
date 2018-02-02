fdc4ebc6829d78deda064bb6ace2a8e8e7d67756
sherylynn <352281674@qq.com>
2018-02-02 13:40:10 +0800

    fix

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -2,7 +2,7 @@
 if [ ! -d "$HOME/gogs_data" ];then
 mkdir $HOME/gogs_data
 fi
-if [ "$HOME/gogs"];then
+if [ -d "$HOME/gogs" ];then
 sudo systemctl stop gogs.service
 sudo systemctl disable gogs.service
 mv $HOME/gogs/custom $HOME/gogs_data/gogs
@@ -12,7 +12,7 @@ sed 's/ubuntu/git/g' /gogs_data/gogs/conf/app.ini
 sed 's/home/data/g' /gogs_data/gogs/conf/app.ini
 sed '46c ROOT_PATH = /app/gogs/log' /gogs_data/gogs/conf/app.ini  
 fi
-if ["$HOME/gogs-repositories"];then
+if [ -d "$HOME/gogs-repositories" ];then
 mv $HOME/gogs-repositories $HOME/gogs_data/git/
 fi
 docker run -itd --name gogs --restart=always -p 22000:22 -p 3000:3000 -v $HOME/gogs_data:/data gogs/gogs
