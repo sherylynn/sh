78f0f3d05aabe5cfd74f3fa43af2ff6df4f4d1aa
sherylynn <352281674@qq.com>
2018-02-02 13:55:04 +0800

    fix

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -12,9 +12,9 @@ sudo chgrp -R ${USER} $HOME/gogs-repositories
 mv $HOME/gogs/custom $HOME/gogs_data/gogs
 mv $HOME/gogs/data $HOME/gogs_data/gogs/
 mv $HOME/gogs/log $HOME/gogs_data/gogs/
-sed 's/${USER}/git/g' /gogs_data/gogs/conf/app.ini
-sed 's/home/data/g' /gogs_data/gogs/conf/app.ini
-sed '46c ROOT_PATH = /app/gogs/log' /gogs_data/gogs/conf/app.ini  
+sed 's/${USER}/git/g' $HOME/gogs_data/gogs/conf/app.ini
+sed 's/home/data/g' $HOME/gogs_data/gogs/conf/app.ini
+sed '46c ROOT_PATH = /app/gogs/log' $HOME/gogs_data/gogs/conf/app.ini  
 fi
 if [ -d "$HOME/gogs-repositories" ];then
 mv $HOME/gogs-repositories $HOME/gogs_data/git/
