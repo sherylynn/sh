8c4b3eac0c0e5d89c6bc42fb46c83f2aefed0641
sherylynn <352281674@qq.com>
2018-02-02 13:50:33 +0800

    增加了权限修改和变量操作

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -5,10 +5,14 @@ fi
 if [ -d "$HOME/gogs" ];then
 sudo systemctl stop gogs.service
 sudo systemctl disable gogs.service
+sudo chown -R ${USER} $HOME/gogs
+sudo chown -R ${USER} $HOME/gogs-repositories
+sudo chgrp -R ${USER} $HOME/gogs
+sudo chgrp -R ${USER} $HOME/gogs-repositories
 mv $HOME/gogs/custom $HOME/gogs_data/gogs
-mv $HOME/gogs/data $HOME/gogs_data/
-mv $HOME/gogs/logs $HOME/gogs_data/
-sed 's/ubuntu/git/g' /gogs_data/gogs/conf/app.ini
+mv $HOME/gogs/data $HOME/gogs_data/gogs/
+mv $HOME/gogs/logs $HOME/gogs_data/gogs/
+sed 's/${USER}/git/g' /gogs_data/gogs/conf/app.ini
 sed 's/home/data/g' /gogs_data/gogs/conf/app.ini
 sed '46c ROOT_PATH = /app/gogs/log' /gogs_data/gogs/conf/app.ini  
 fi
