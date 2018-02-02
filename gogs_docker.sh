762847f73b43db79f782f995a9c9ab729deb0d7a
sherylynn <352281674@qq.com>
2018-02-02 14:13:13 +0800

    迁移迁移妈卖批

--- gogs_docker.sh
+++ gogs_docker.sh
@@ -12,9 +12,10 @@ sudo chgrp -R ${USER} $HOME/gogs-repositories
 mv $HOME/gogs/custom $HOME/gogs_data/gogs
 mv $HOME/gogs/data $HOME/gogs_data/gogs/
 mv $HOME/gogs/log $HOME/gogs_data/gogs/
-sed 's/${USER}/git/g' $HOME/gogs_data/gogs/conf/app.ini
-sed 's/home/data/g' $HOME/gogs_data/gogs/conf/app.ini
-sed '46c ROOT_PATH = /app/gogs/log' $HOME/gogs_data/gogs/conf/app.ini  
+#-i 是直接修改 不带的话输出到屏幕 带变量需要在变量两边加单引号
+sed -i 's/'$USER'/git/g' $HOME/gogs_data/gogs/conf/app.ini
+sed -i 's/home/data/g' $HOME/gogs_data/gogs/conf/app.ini
+sed -i '46c ROOT_PATH = /app/gogs/log' $HOME/gogs_data/gogs/conf/app.ini  
 fi
 if [ -d "$HOME/gogs-repositories" ];then
 mv $HOME/gogs-repositories $HOME/gogs_data/git/
