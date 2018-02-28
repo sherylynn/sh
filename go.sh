85b521dc5f8ff149ec6b701f521c5ac8d914b072
sherylynn <352281674@qq.com>
2018-02-28 23:47:39 +0800

    修正

--- go.sh
+++ go.sh
@@ -7,7 +7,7 @@ if [ ! -d "$HOME/tools" ]; then
   mkdir $HOME/tools
 fi
 wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
-  tar -xzf -C $HOME/tools go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+  tar -C $HOME/tools -xzf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
   rm go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
 echo 'export GOROOT=$HOME/tools/go'>>~/.bashrc
 echo 'export PATH=$PATH:$GOROOT/bin'>>~/.bashrc
