80d6c7af0a6631d3ca02ac11f168873a6935060a
sherylynn <352281674@qq.com>
2018-02-28 17:23:40 +0800

    新增了go的安装

---
+++ go.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+lynn=$HOME
+GO_VERSION=1.10
+GO_ARCH=amd64
+cd $HOME
+wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+  tar -xzf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+  rm go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
+echo 'export GOROOT=$HOME/go'>>~/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin'>>~/.bashrc
