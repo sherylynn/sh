0706554fa1a6bcbcb2bdd4640ace2b3cee0075f7
sherylynn <352281674@qq.com>
2018-02-28 23:20:51 +0800

    change dir

--- go.sh
+++ go.sh
@@ -4,8 +4,7 @@ GO_VERSION=1.10
 GO_ARCH=amd64
 cd $HOME
 wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
-  tar -xzf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+  tar -xzf -C $HOME/tools go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
   rm go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
-echo 'export GOROOT=$HOME/go'>>~/.bashrc
-echo 'export GOPATH=$HOME/go'>>~/.bashrc
+echo 'export GOROOT=$HOME/tools/go'>>~/.bashrc
 echo 'export PATH=$PATH:$GOROOT/bin'>>~/.bashrc
