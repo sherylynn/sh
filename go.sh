80f4205d1f8d49079bf87ad32441343264e418bf
sherylynn <352281674@qq.com>
2018-02-28 23:23:40 +0800

    777

--- go.sh
+++ go.sh
@@ -3,6 +3,9 @@ lynn=$HOME
 GO_VERSION=1.10
 GO_ARCH=amd64
 cd $HOME
+if [ ! -d "$HOME/tools" ]; then
+  mkdir $HOME/tools
+fi
 wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
   tar -xzf -C $HOME/tools go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
   rm go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
