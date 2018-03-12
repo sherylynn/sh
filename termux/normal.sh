28727f2de41af0957a61041b9624bea21af2586e
sherylynn <352281674@qq.com>
2018-03-12 14:27:33 +0800

    add gui repo

--- termux/normal.sh
+++ termux/normal.sh
@@ -1,3 +1,8 @@
-apt install vim htop -y
+apt install vim htop neovim curl -y
 apt install termux-tools -y
+#wget https://xeffyr.github.io/termux-x-repository/pubkey.gpg
+#apt-key add pubkey.gpg
+#apt update
+cd ~
+curl https://raw.githubusercontent.com/xeffyr/termux-x-repository/master/enablerepo.sh |sh
 termux-setup-storage
