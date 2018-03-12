b00f1fb7140f34d748a5d28bb5c19aedf550b511
sherylynn <352281674@qq.com>
2018-03-12 14:57:39 +0800

    add desktop support run startx for start

--- termux/normal.sh
+++ termux/normal.sh
@@ -1,8 +1,9 @@
-apt install vim htop neovim curl -y
+apt install vim htop neovim curl ncdu -y
 apt install termux-tools -y
 #wget https://xeffyr.github.io/termux-x-repository/pubkey.gpg
 #apt-key add pubkey.gpg
 #apt update
 cd ~
 curl https://raw.githubusercontent.com/xeffyr/termux-x-repository/master/enablerepo.sh |sh
+pkg install termux-desktop
 termux-setup-storage
