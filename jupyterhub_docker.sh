ad9a9ae33d060973e84eefbbeb5b54cba2554952
sherylynn <352281674@qq.com>
2018-03-20 11:25:01 +0800

    降级后还是错，jupyterhub_docker各种垃圾和莫名其妙

--- jupyterhub_docker.sh
+++ jupyterhub_docker.sh
@@ -2,3 +2,4 @@ if [ ! -d "$HOME/jupyterhub" ]; then
   mkdir $HOME/jupyterhub
 fi
 docker run -p 10000:8000 -d -v $HOME/jupyterhub:/jupyterhub --name jupyterhub jupyterhub/jupyterhub:0.8.1 jupyterhub
+#就算是0.8.1 login完后还是有一个错误说can't start server
