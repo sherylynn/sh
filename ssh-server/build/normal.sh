9fcc0160c96bb2a85b6c22c57a33723dde79e585
sherylynn <352281674@qq.com>
2018-03-22 00:08:57 +0800

    增加了the_silver_searcher的安装

--- ssh-server/build/normal.sh
+++ ssh-server/build/normal.sh
@@ -1,7 +1,2 @@
 apk add vim git htop
-apk add git automake autoconf make g++ pcre-dev xz-dev
-cd ~
-git clone https://github.com/ggreer/the_silver_searcher.git
-cd the_silver_searcher
-./build.sh
-makr install
+apk add git automake autoconf make g++ pcre-dev xz-dev the_silver_searcher
