99097b74adf79b3218e81b64f02d23f954b8fca2
sherylynn <352281674@qq.com>
2018-03-29 01:00:08 +0800

    切换版本

--- debian_mirror.sh
+++ debian_mirror.sh
@@ -6,4 +6,5 @@ sudo vi /etc/apt/sources.list +%s/main$/main\ contrib\ non-free/g +wq!
 sudo vi /etc/apt/sources.list +%s/mirror\./mirrors/g +wq!
 #sudo vi /etc/apt/sources.list +%s#mirror.aliyun.com#mirrors.ustc.edu.cn#g +wq!
 #sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirrors.ustc.edu.cn#g +wq!
+sudo vi /etc/apt/sources.list +%s/stretch/testing/g +wq!
 sudo apt update
