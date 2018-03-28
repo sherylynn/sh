3f44fbcce67b8c34202bb1d8c2117a0d6f6c4eb9
林增 <352281674@qq.com>
2018-03-29 00:34:42 +0800

    fix

--- debian_mirror.sh
+++ debian_mirror.sh
@@ -1,7 +1,7 @@
 #sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirrors.ustc.edu.cn#g +wq!
 sudo vi /etc/apt/sources.list +%s#mirrors.ustc.edu.cn#mirror.aliyun.com#g +wq!
 sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirror.aliyun.com#g +wq!
-sudo vi /etc/apt/sources.list +%s#main%#main contrib non-free#g +wq!
+sudo vi /etc/apt/sources.list +%s#main$#main contrib non-free#g +wq!
 #sudo vi /etc/apt/sources.list +%s#mirror.aliyun.com#mirrors.ustc.edu.cn#g +wq!
 #sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirrors.ustc.edu.cn#g +wq!
 sudo apt update
