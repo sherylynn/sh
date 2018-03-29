651d0dcbd4fcbba3bb26af186566ac1bf690f120
sherylynn <352281674@qq.com>
2018-03-29 08:27:22 +0800

    fix again

--- debian_mirror.sh
+++ debian_mirror.sh
@@ -4,6 +4,7 @@ sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirror.aliyun.com#g +wq!
 sudo vi /etc/apt/sources.list +%s#security.debian.org#mirror.aliyun.com#g +wq!
 sudo vi /etc/apt/sources.list +%s/main$/main\ contrib\ non-free/g +wq!
 sudo vi /etc/apt/sources.list +%s/mirror\./mirrors/g +wq!
+sudo vi /etc/apt/sources.list +%s/mirrorsaliyun/mirrors\.aliyun/g +wq!
 #sudo vi /etc/apt/sources.list +%s#mirror.aliyun.com#mirrors.ustc.edu.cn#g +wq!
 #sudo vi /etc/apt/sources.list +%s#deb.debian.org#mirrors.ustc.edu.cn#g +wq!
 sudo vi /etc/apt/sources.list +%s/stretch/testing/g +wq!
