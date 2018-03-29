11be43081764a646c93a4ea70a83a136226b7ba8
sherylynn <352281674@qq.com>
2018-03-29 18:41:54 +0800

    改回默认的vim

--- termux/init.sh
+++ termux/init.sh
@@ -1,5 +1,6 @@
 pkg install apt openssh -y
 pkg install htop coreutils
 cat $HOME/id_rsa.pub >> $HOME/.ssh/authorized_keys
-cat bashrc >> $HOME/.bashrc
+#cat bashrc >> $HOME/.bashrc
+ln -s -f bashrc $HOME/.bashrc
 sshd
