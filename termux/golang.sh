4d0af6d1a30776b6cc85a97cfeb207df723c83b3
sherylynn <352281674@qq.com>
2018-04-24 01:19:55 +0800

    update

--- termux/golang.sh
+++
@@ -1,2 +0,0 @@
-#install golang for android (build by termux not offical)
-apt install golang -y
---
+++ termux/lang.sh
@@ -0,0 +1,4 @@
+#install golang for android (build by termux not offical)
+apt install golang -y
+apt install nodejs -y
+apt install python -y
