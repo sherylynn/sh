befb22ba50a055393e2dcd44a3408c82b5747dbf
sherylynn <352281674@qq.com>
2018-04-23 11:57:55 -0400

    add new

---
+++ debian/deb-openvm-tools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ ! -d "/mnt/hgfs" ]; then
+sudo mkdir -p /mnt/hgfs
+fi
+sudo apt-get install open-vm-tools-desktop -y
+sudo tee /etc/systemd/system/mnt.hgfs.service <<-'EOF'
+[Unit]
+Description=Load VMware shared folders
+Requires=vmware-vmblock-fuse.service
+After=vmware-vmblock-fuse.service
+ConditionPathExists=.host:/
+ConditionVirtualization=vmware
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=
+ExecStart=/usr/bin/vmhgfs-fuse -o allow_other -o auto_unmount .host:/ /mnt/hgfs
+;-o umask=007专门的权限给770
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable mnt.hgfs.service
+sudo systemctl start mnt.hgfs.service
