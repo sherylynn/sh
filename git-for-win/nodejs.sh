db6c42fd4e4fcc582019a895e32de8785513445b
sherylynn <352281674@qq.com>
2018-05-29 09:51:52 +0800

    still can't active

--- git-for-win/nodejs.sh
+++ git-for-win/nodejs.sh
@@ -18,10 +18,25 @@ if [ ! -f "${NODE_FILE_PACK}" ]; then
 fi
 
 if [ ! -d "${NODE_FILE_NAME}" ]; then
-  unzip ${NODE_FILE_PACK}
+  unzip -q ${NODE_FILE_PACK}
 fi
   rm -rf $INSTALL_PATH/node && \
   mv ${NODE_FILE_NAME} $INSTALL_PATH/node && \
   rm -rf ${NODE_FILE_PACK}
-echo 'export PATH=$PATH:'${INSTALL_PATH}'/node/bin'>>~/.bashrc
+
+#----------------------------
+# Set Global packages path
+#----------------------------
+if [ ! -d "$INSTALL_PATH/node-global" ]; then
+  mkdir $INSTALL_PATH/node-global
+fi
+if [ ! -d "$INSTALL_PATH/node-cache" ]; then
+  mkdir $INSTALL_PATH/node-cache
+fi
+#--------------new .toolsrc-----------------------
+echo 'test -f ~/.toolsrc && . ~/.toolsrc' >> ~/.bash_profile
+echo 'export PATH=$PATH:'${INSTALL_PATH}'/node/bin'>~/.toolsrc
+echo 'export PATH='$INSTALL_PATH'/node-global/bin:$PATH' >> ~/.toolsrc
+echo 'NPM_CONFIG_PREFIX='$INSTALL_PATH'/node-global' >> ~/.toolsrc
+echo 'NPM_CONFIG_CACHE='$INSTALL_PATH'/node-cache' >> ~/.toolsrc
 source ~/.bashrc
