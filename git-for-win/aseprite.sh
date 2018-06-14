839647c070b7712d696637ad200f894b9272ce3e
sherylynn <352281674@qq.com>
2018-06-15 01:00:14 +0800

    fix

--- git-for-win/aseprite.sh
+++ git-for-win/aseprite.sh
@@ -5,3 +5,20 @@ cd aseprite
 mkdir build
 cd build
 cmake -G Ninja ..
+#----or------------------------------------
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+cd ~
+git clone https://github.com/aseprite/skia.git
+cd skia
+git checkout aseprite-m67
+python tools/git-sync-deps
+
+gn gen out/Release --args="is_official_build=true skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false target_cpu=""x86"" msvc=2017"
+ninja -C out/Release
+
+cd ~
+cd aseprite
+mkdir skia
+cd skia
+cmake -DUSE_ALLEG4_BACKEND=OFF -DUSE_SKIA_BACKEND=ON -DSKIA_DIR=C:\Users\lynn\skia -G Ninja ..
+ninja aseprite
