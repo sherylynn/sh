35b3b972f83f904f7c944beb3cd9298a7c098828
sherylynn <352281674@qq.com>
2018-07-17 13:38:52 +0800

    add more app

--- mac/app.sh
+++ mac/app.sh
@@ -1,4 +1,8 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
-brew cask install onyx hwsensors kext-utility skim iina firefox chromium calibre v2rayx qq wechat
+#normal app
+brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp
+#macx86 app
+brew cask install clover-configurator kext-utility hwsensors
+#cli app
 brew install nodejs go htop
