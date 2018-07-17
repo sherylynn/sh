f8275f157433e34927fb6cb161e7d78dec1df9fc
sherylynn <352281674@qq.com>
2018-07-17 09:29:39 +0800

    update

--- mac/app.sh
+++ mac/app.sh
@@ -1,4 +1,4 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 brew cask install kext-utility
-brew cask install onyx
+brew cask install onyx hwsensors
