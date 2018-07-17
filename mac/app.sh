9a7a79ce90fcd535d8914e45fd4f27990be295ba
sherylynn <352281674@qq.com>
2018-07-17 13:13:45 +0800

    update

--- mac/app.sh
+++ mac/app.sh
@@ -1,4 +1,4 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
-brew cask install kext-utility
-brew cask install onyx hwsensors
+brew cask install onyx hwsensors kext-utility skim iina firefox chromium calibre v2rayx qq wechat
+brew install nodejs go htop
