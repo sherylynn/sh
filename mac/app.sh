01a06b7d169e93cb8b541a1b252cd123fc11a17a
sherylynn <352281674@qq.com>
2018-07-18 22:50:06 +0800

    add vscode

--- mac/app.sh
+++ mac/app.sh
@@ -1,7 +1,7 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 #normal app
-brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp squirrel
+brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp squirrel visual-studio-code
 #macx86 app
 brew cask install clover-configurator kext-utility hwsensors
 #cli app
