a74c7add5bc560e29805006650c2ee9412c1740e
sherylynn <352281674@qq.com>
2018-07-18 22:44:10 +0800

    add 鼠鬚管

--- mac/app.sh
+++ mac/app.sh
@@ -1,8 +1,10 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 #normal app
-brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp
+brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp squirrel
 #macx86 app
 brew cask install clover-configurator kext-utility hwsensors
 #cli app
 brew install nodejs go htop
+#gnu
+brew install evince 
