c770b7de360df000d16f39f788c16a701d08fa4d
sherylynn <352281674@qq.com>
2018-07-28 23:22:20 +0800

    update

--- py/jupyter-termux.sh
+++ py/jupyter-termux.sh
@@ -1,2 +1,7 @@
 apt install libcrypt-dev libzmq libzmq-dev
 pip3 install jupyter
+cd ~
+if [ ! -d "$HOME/.jupyter" ]; then
+  mkdir $HOME/.jupyter
+fi
+jupyter notebook password
--- py/jupyter.sh
+++ py/jupyter.sh
@@ -1,1 +1,6 @@
 sudo pip3 install jupyter
+cd ~
+if [ ! -d "$HOME/.jupyter" ]; then
+  mkdir $HOME/.jupyter
+fi
+jupyter notebook password
