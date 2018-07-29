ae346a52b086fb8dcd7b85b5cae4d6351ab2ee53
sherylynn <352281674@qq.com>
2018-07-29 19:11:24 +0800

    add vim binding

--- py/jupyter.sh
+++ py/jupyter.sh
@@ -1,6 +1,12 @@
-pip3 install --user jupyter numpy
+#just for termux ,so it is lack of sudo
+apt install libcrypt-dev libzmq libzmq-dev
+pip3 install --user jupyter numpy 
 cd ~
 if [ ! -d "$HOME/.jupyter" ]; then
   mkdir $HOME/.jupyter
 fi
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+jupyter nbextension enable vim_binding/vim_binding
 jupyter notebook password
