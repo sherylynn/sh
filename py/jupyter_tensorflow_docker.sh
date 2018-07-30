ea86ff5fb32e67b225cbd6c8bd667d1d02a07498
sherylynn <352281674@qq.com>
2018-07-30 00:43:18 -0400

    干死你娘的权限理

--- py/jupyter_tensorflow_docker.sh
+++ py/jupyter_tensorflow_docker.sh
@@ -1,3 +1,6 @@
+if [ ! -d "$HOME/.local" ]; then
+  mkdir $HOME/.local
+fi
 if [ ! -d "$HOME/.jupyter" ]; then
   mkdir $HOME/.jupyter
 fi
@@ -5,7 +8,7 @@ fi
 #没直接挂载$HOME目录，是因为主机是root用户，没法挂载
 #如果主机用户非root，可以直接挂载
 #主机用户为root时候，ubuntu下直接安装jupyter都有问题，干
-chown -R 1000 $HOME/toys
+chown -R 1000 $HOME/.jupyter $HOME/.local $HOME/toys
 docker run --name jupyter \
   --privileged=true \
   -p 10000:8888 \
