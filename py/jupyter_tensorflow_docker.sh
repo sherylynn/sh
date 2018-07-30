979e1cdc260f8a75bfd179e923a85c1b18bfbcc1
sherylynn <352281674@qq.com>
2018-07-29 23:45:40 -0400

    fix 权限问题

--- py/jupyter_tensorflow_docker.sh
+++ py/jupyter_tensorflow_docker.sh
@@ -1,8 +1,14 @@
 if [ ! -d "$HOME/.jupyter" ]; then
   mkdir $HOME/.jupyter
 fi
+#因为权限原因，需要把挂载的文件权限改为uid为1000，在docker中可以更改保存
+#没直接挂载$HOME目录，是因为主机是root用户，没法挂载
+#如果主机用户非root，可以直接挂载
+#主机用户为root时候，ubuntu下直接安装jupyter都有问题，干
+chown -R 1000 $HOME/toys
 docker run --name jupyter \
+  --privileged=true \
   -p 10000:8888 \
   -itd --restart=always \
-  -v $HOME:/home/jovyan \
+  -v $HOME/toys:/home/jovyan/toys \
   jupyter/tensorflow-notebook
