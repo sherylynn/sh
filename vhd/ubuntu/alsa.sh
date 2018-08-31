1afa4a803c116f67d19cf0e60988b5bcc6ed4815
sherylynn <352281674@qq.com>
2018-08-31 10:21:21 +0800

    添加了声卡驱动安装脚本

---
+++ vhd/ubuntu/alsa.sh
@@ -0,0 +1,3 @@
+sudo apt-get install dkms
+wget https://code.launchpad.net/~ubuntu-audio-dev/+archive/ubuntu/alsa-daily/+files/oem-audio-hda-daily-dkms_0.201808050301~ubuntu18.04.1_all.deb
+sudo dpkg -i oem-audio*
