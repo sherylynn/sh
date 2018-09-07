9392c198faf53c41f1328962510b25143c83d560
sherylynn <352281674@qq.com>
2018-09-07 08:31:53 +0800

    rm before update

--- vhd/fedora/vhd_boot.sh
+++ vhd/fedora/vhd_boot.sh
@@ -8,6 +8,7 @@ cd ~/ntfs-3g_ntfsprogs-2017.3.23-fixed/
 make
 sudo make install
 
+sudo rm -rf ~/dracut-fedora-kloop
 sudo dracut  -i ~/sh/vhd/fedora/10-vhdmount-kloop.sh /lib/dracut/hooks/pre-mount/10-vhdmount-kloop.sh  --no-hostonly  --install " vi /etc/virc ps grep cat rm blkid losetup  kpartx partx mount.fuse mount.ntfs-3g ntfs-3g shutdown  lvm  vgchange  vgmknodes  vgscan  dmsetup dmeventd  "   --add-drivers  "fuse dm-mod "  -o " plymouth btrfs crypt  cifs fcoe fcoe-uefi iscsi nfs nbd"  ~/dracut-fedora-kloop
 
 #fedora not exist /vmlinuz in root dir
