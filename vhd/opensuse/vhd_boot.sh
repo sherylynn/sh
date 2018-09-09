8297a8e4a92bf149fb472fe75deab58aa9aab215
sherylynn <352281674@qq.com>
2018-09-09 09:37:38 +0800

    add vhd for opensuse

---
+++ vhd/opensuse/vhd_boot.sh
@@ -0,0 +1,16 @@
+cd ~
+wget https://github.com/sherylynn/sh/releases/download/ntfs-3g/ntfs-3g_ntfsprogs-2017.3.23-fixed.zip
+unzip ntfs-3g_ntfsprogs-2017*
+#first time to see "too many args error"
+#cd ~/ntfs*
+cd ~/ntfs-3g_ntfsprogs-2017.3.23-fixed/ 
+./configure
+make
+sudo make install
+
+sudo rm -rf ~/dracut-opensuse-kloop
+sudo dracut  -i ~/sh/vhd/opensuse/10-vhdmount-kloop.sh /lib/dracut/hooks/pre-mount/10-vhdmount-kloop.sh  --no-hostonly  --install " blkid kpartx  partx ntfs-3g fusermount  mount.fuse mount.ntfs-3g vgscan vgchange lvm  "   --add-drivers  "fuse loop dm-mod "  -o " plymouth btrfs crypt  cifs dmraid multipath fcoe fcoe-uefi iscsi nfs nbd"  ~/dracut-opensuse-kloop
+
+#fedora not exist /vmlinuz in root dir
+#sudo cp /vmlinuz ~/vmlinuz-fedora
+
