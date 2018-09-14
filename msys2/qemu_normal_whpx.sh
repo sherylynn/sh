ec169cc2f1196f767ecd5e6e0cb9777f0db276f5
sherylynn <352281674@qq.com>
2018-09-14 14:18:18 +0800

    sync

--- msys2/qemu_normal_whpx.sh
+++ msys2/qemu_normal_whpx.sh
@@ -5,6 +5,13 @@ export http_proxy=http://${HOST}:${PORT}/
 export https_proxy=http://${HOST}:${PORT}/
 git config http.proxy http://${HOST}:${PORT}/
 
+git clone --depth 1 https://android.googlesource.com/platform/prebuilts/gcc/lin\
+ux-x86/host/x86_64-w64-mingw32-4.8
+cp -Rf x86_64-w64-mingw32-4.8/x86_64-w64-mingw32/include/WinHv*.h /mingw64/x86_\
+64-w64-mingw32/include/
+cp -Rf x86_64-w64-mingw32-4.8/x86_64-w64-mingw32/lib/libWinHv*.a /mingw64/x86_6\
+4-w64-mingw32/lib/
+
 git clone git://git.qemu-project.org/qemu.git
 cd qemu
 git checkout v2.12.1
@@ -15,8 +22,7 @@ git submodule update --init dtc
 #git clone --depth 1 https://github.com/qemu/dtc
 
 ./configure --enable-gtk --enable-sdl \
-	    --enable-whpx --static --disable-werror
-#--disable-capstone
+	    --enable-whpx --static --disable-werror --disable-capstone
 #make -j 后跟数字就是限制进程最大数目 不跟数字就是直接最大
 make -j
 
