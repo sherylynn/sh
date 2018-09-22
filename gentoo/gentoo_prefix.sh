0bad932aa5813767fb957b12f46e73dbab7aa138
sherylynn <352281674@qq.com>
2018-09-22 23:35:30 +0800

    gentoo_prefix.sh

---
+++ gentoo/gentoo_prefix.sh
@@ -0,0 +1,7 @@
+#cd /data/data/com.termux/files/home/toys/gentoo
+#busybox sh self
+busybox wget http://distfiles.gentoo.org/experimental/prefix/arm/prefix-stage3-arm64-latest.tar.xz
+busybox tar -C /data -xf prefix-stage3-arm64-latest.tar.xz#cd /data/data/com.termux/files/home/toys/gentoo
+#busybox sh self
+busybox wget http://distfiles.gentoo.org/experimental/prefix/arm/prefix-stage3-arm64-latest.tar.xz
+busybox tar -C /data -xf prefix-stage3-arm64-latest.tar.xz
