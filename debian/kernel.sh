ee4702bce1726427c10ed80bc990deca0a7840bb
sherylynn <352281674@qq.com>
2018-09-26 15:19:54 +0800

    增加了debian下桌面环境的脚本和编译内核的内容

---
+++ debian/kernel.sh
@@ -0,0 +1,9 @@
+sudo apt-get install build-essential kernel-package libncurses5-dev fakeroot linux-source -y
+sudo apt install flex bison -y 
+cd /usr/src/linux
+#解压内核文件
+#sudo -s 执行下列命
+#make mrproper
+#make clean
+#make localmodconfig
+#make menuconfig
