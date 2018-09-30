46c0cd40a2b85b47f3c85014ed6091c352d259b5
sherylynn <352281674@qq.com>
2018-09-30 15:37:43 +0800

    调整了顺序

--- raspberry/install_i3wm.sh
+++ raspberry/install_i3wm.sh
@@ -2,12 +2,13 @@ sudo apt install xinit -y
 #echo exec startx > $HOME/.bash_profile
 #will be error in ssh login
 # or
-sudo apt install lightdm -y
 
 sudo apt install i3 suckless-tools -y
 sudo apt install xfce4-terminal -y
 # or 
 #sudo apt install lxterminal -y
+
+sudo apt install lightdm -y
 
 #sudo apt install network-manager network-manager-gnome
 #sed -i 's/false/true/g' /etc/NetworkManager.conf
