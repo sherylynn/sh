9c7c44075a952ef563f9026c63b0bb786da7a0e2
sherylynn <352281674@qq.com>
2018-09-30 15:05:39 +0800

    update and fix

--- raspberry/install_i3wm.sh
+++ raspberry/install_i3wm.sh
@@ -1,17 +1,18 @@
 sudo apt install xinit -y
+#echo exec startx > $HOME/.bash_profile
+#will be error in ssh login
 # or
-#sudo apt install lightdm -y
+sudo apt install lightdm -y
 
 sudo apt install i3 suckless-tools -y
 sudo apt install xfce4-terminal -y
 # or 
 #sudo apt install lxterminal -y
-echo exec startx > $HOME/.bash_profile
 
-sudo apt install network-manager network-manager-gnome
-sed -i 's/false/true/g' /etc/NetworkManager.conf
+#sudo apt install network-manager network-manager-gnome
+#sed -i 's/false/true/g' /etc/NetworkManager.conf
 #sudo systemctl enable network-manager
 #sudo systemctl restart network-manager
 
 #---------or wicd
-#sudo apt install wicd
+sudo apt install wicd
