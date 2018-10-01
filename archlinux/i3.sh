acb0623d70d789d737c7ff45e3096744d2730d61
sherylynn <352281674@qq.com>
2018-10-01 15:39:31 +0000

    fuck

--- archlinux/i3.sh
+++ archlinux/i3.sh
@@ -1,2 +1,2 @@
-pacman -S xorg-server xorg-xinit i3-gaps i3status i3blocks i3lock --needed --noconfirm
+pacman -S xorg-server xorg-xinit i3-gaps i3status i3blocks i3lock dmenu --needed --noconfirm
 echo exec i3 > ~/.xinitrc
