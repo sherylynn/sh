ebbc11bd4cd626f498924bcf8dfa961ae98f25ad
sherylynn <352281674@qq.com>
2018-10-02 15:27:48 +0800

    add archlinux to termux

---
+++ termux/archlinux_get.sh
@@ -0,0 +1,4 @@
+cd ~
+apt install wget proot bsdtar
+export http_proxy="http://127.0.0.1:10808" 
+wget -c http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz
---
+++ termux/archlinux_mount.sh
@@ -0,0 +1,4 @@
+cd ~
+mkdir -p root mount
+mount /dev/block/sda1 boot
+mount /dev/block/sda2 root
