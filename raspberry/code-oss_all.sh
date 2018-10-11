1e8a9a04d763a737590105adb3bf3341dc2351fa
sherylynn <352281674@qq.com>
2018-10-12 07:20:12 +0800

    fix

--- raspberry/code-oss_all.sh
+++ raspberry/code-oss_all.sh
@@ -1,3 +1,4 @@
-proxychains wget -O code-oss_armhf.deb https://packagecloud.io/headmelted/codebuilds/packages/ubuntu/xenial/code-oss_1.28.0-1537780162_armhf.deb
+wget -O code-oss_armhf.deb --content-disposition https://packagecloud.io/headmelted/codebuilds/packages/ubuntu/xenial/code-oss_1.28.0-1537780162_armhf.deb/download.deb
 sudo dpkg -i code-oss_armhf.deb
 sudo apt install -f
+rm -f code-oss_armhf.deb
