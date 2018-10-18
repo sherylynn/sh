326715ca945c2613353ac72cb6c15242d9b6c43a
sherylynn <352281674@qq.com>
2018-10-18 18:20:25 +0800

    修复错误权限操作

--- raspberry/android_chroot.sh
+++ raspberry/android_chroot.sh
@@ -4,11 +4,5 @@ sudo groupadd -g 3003 aid_inet
 sudo groupadd -g 3004 aid_inet_raw
 sudo groupadd -g 3005 aid_inet_admin
 
-sudo gpasswd -a $(whoami) aid_net_bt_admin
-sudo gpasswd -a $(whoami) aid_net_bt
-sudo gpasswd -a $(whoami) aid_inet
-sudo gpasswd -a $(whoami) aid_inet_raw
-sudo gpasswd -a $(whoami) aid_inet_admin
-
 sudo usermod -g aid_inet _apt
 sudo usermod -g aid_inet $(whoami)
