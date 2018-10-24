250a2dfe91d85f7302d11e7d6e5d21572afe96b7
sherylynn <352281674@qq.com>
2018-10-24 22:22:11 +0800

    fix

---
+++ mac/mirror_brew.sh
@@ -0,0 +1,11 @@
+ # 替换brew.git:
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+# 替换homebrew-core.git:
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+# 应用生效
+brew update
+# 替换homebrew-bottles:
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bashrc
+source ~/.bashrc
