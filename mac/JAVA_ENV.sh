89b2529f4fb4ea3ba3a851ff34fd49b35168648f
sherylynn <352281674@qq.com>
2018-11-16 01:47:31 +0800

    add ncdu and android env

---
+++ mac/JAVA_ENV.sh
@@ -0,0 +1,3 @@
+echo "export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home/" >> ~/.bashrc
+echo "export GRADLE_HOME=/Applications/Android\ Studio.app/Contents/gradle/gradle-4.6" >> ~/.bashrc
+echo 'export PATH=$PATH:$GRADLE_HOME/bin' >> ~/.bashrc
