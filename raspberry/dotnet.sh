eb2cc0e51dd615fc748192ca5b4583b3e93d392d
sherylynn <352281674@qq.com>
2018-11-24 14:37:00 +0800

    add dotnet core to raspberry

---
+++ raspberry/dotnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#sudo apt update
+lynn=$HOME
+INSTALL_PATH=$HOME/tools
+DOTNET_PATH=$INSTALL_PATH/dotnet
+rm -rf $DOTNET_VERSION
+mkdir -p $DOTNET_PATH
+DOTNET_VERSION=2.1
+DOTNET_ARCH=arm
+axel -n 10 https://dotnetcli.blob.core.windows.net/dotnet/Runtime/release/${DOTNET_VERSION}/dotnet-runtime-latest-linux-${DOTNET_ARCH}.tar.gz 
+#axel -n 10 https://dotnetcli.blob.core.windows.net/dotnet/Runtime/master/dotnet-runtime-latest-linux-${NODE_ARCH}.tar.gz && \
+  tar -xzf dotnet-runtime-latest-linux-${DOTNET_ARCH}.tar.gz -C ${DOTNET_PATH}
+rm -rf dotnet-runtime-latest-linux-${DOTNET_ARCH}.tar.gz
+export PATH=$PATH:${DOTNET_PATH} 
+echo 'export PATH=$PATH:'${DOTNET_PATH} >>~/.bashrc
