1174ceef23c2de104541e6993aff9c17cfc1c0b6
sherylynn <352281674@qq.com>
2018-11-27 00:21:09 +0800

    add tensorflow

---
+++ py/tensorflow.sh
@@ -0,0 +1,4 @@
+#for mac
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+brew switch python 3.6.5_1
+pip3 install tensorflow --user
