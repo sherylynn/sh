d22c10ed85d4305409b4e628baa43e18d97ee62e
sherylynn <352281674@qq.com>
2019-01-06 05:20:58 +0800

    fix error

--- win-git/python.sh
+++ win-git/python.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # source
 INSTALL_PATH=$HOME/tools
 PYTHON_HOME=$INSTALL_PATH/python
@@ -8,6 +9,7 @@ PYTHON_VERSION=3.6.8
 PYTHON_ARCH=amd64
 GET_PIP=get-pip.py
 GET_PIP_PATH=$PYTHON_HOME/$GET_PIP
+PIP_USERBASE=$INSTALL_PATH/python-pip
 OS=windows
 PYTHON_FILE_NAME=python-${PYTHON_VERSION}-embed-${PYTHON_ARCH}
 PYTHON_FILE_PACK=${PYTHON_FILE_NAME}.zip
@@ -37,18 +39,38 @@ fi
 #--------------new .toolsrc-----------------------
 echo 'test -f ~/.pythonrc && . ~/.pythonrc' >> ~/.bash_profile
 #windows下和linux下的不同
+# windows 下pip user时候的bin也不一样，不在 --user-base下 ,或者说
+# bin即script的位置会随着bash执行时候的位置改变，如在~
+# 下，会安装到 ~/users/lynn/,而不是C://users/lynn
+# 此时除非在c盘根目录不然无法正确执行
+# 如果手动设置pythonuserbase为反斜杠路径，就可以正常运行
+# 这里取巧用单引号中不解释反斜杠来执行
+# linux下 PIP_PATH=$(python -m site --user-base)"/bin"
+# win下
+export PATH=$PATH:${PYTHON_HOME}
+export PYTHONUSERBASE="'"$USERPROFILE\\tools\\python-pip"'"
+python -m site
+# userbase 变量需要在 python -m site 前
+PIP_BIN_PATH=$(python -m site --user-site)
+echo show_PIP_PATH:$PIP_BIN_PATH
+export PATH=$PATH:${PYTHON_SCRIPTS}
+export PATH=$PATH:'$PIP_BIN_PATH\\..\\Scripts'
+export PYTHONPATH=${PYTHON_HOME}:${PYTHON_LIB}:${PYTHON_PACKAGES}
 echo 'export PATH=$PATH:'${PYTHON_HOME}>~/.pythonrc
 echo 'export PATH=$PATH:'${PYTHON_SCRIPTS}>>~/.pythonrc
+echo 'export PATH=$PATH:'"'"$PIP_BIN_PATH\\..\\Scripts"'">>~/.pythonrc
 echo 'export PYTHONPATH='${PYTHON_HOME}:${PYTHON_LIB}:${PYTHON_PACKAGES}>>~/.pythonrc
-export PATH=$PATH:${PYTHON_HOME}
-export PATH=$PATH:${PYTHON_SCRIPTS}
-export PYTHONPATH=${PYTHON_HOME}:${PYTHON_LIB}:${PYTHON_PACKAGES}
+echo "export PYTHONUSERBASE="'$USERPROFILE\\tools\\python-pip'>>~/.pythonrc
 
+#-----------pip install path-----------------------------------
+if [ ! -d "${PIP_USERBASE}" ]; then
+  mkdir ${PIP_USERBASE}
+fi
 
 #-------get pip-------------------------------------------
 cd $INSTALL_PATH/python
 if [ ! -f "${GET_PIP_PATH}" ]; then
   curl -o ${GET_PIP} https://bootstrap.pypa.io/get-pip.py
 fi
-python ${GET_PIP}
+python ${GET_PIP} --user
 mv python*._pth python._pth.save
