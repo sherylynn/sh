235c54adfc1e70c09e02a056e33bf5375438c4f1
sherylynn <352281674@qq.com>
2019-01-09 14:09:16 +0800

    最终版本

--- win-git/python.sh
+++ win-git/python.sh
@@ -93,8 +93,9 @@ setx PYTHONPATH $(winPath ${PYTHONPATH})
 setx PYTHONUSERBASE $(winPath ${PYTHONUSERBASE})
 setx PYTHON_BIN $(winPath ${PYTHON_HOME})";"$(winPath ${PYTHON_SCRIPTS})";"$(winPath ${PIP_BIN_PATH}/../Scripts)
 
-# 因为最后一个$PATH中的值是没有:分隔符的，这里补充一个;
-winENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -)"
+# 替换掉linux path中:为;因为最后一个$PATH中的值是没有:分隔符的，这里补充一个; 用sort排序 uniq 去重 cypath换win字符 tr去换行
+# 有换行时path全部不识别，只有powershell能识别，神奇
+winENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
 #cygpath 是 msys带的处理路径的工具
 echo $winENV
 powershell -C "[environment]::SetEnvironmentvariable('path', \"$winENV\", [System.EnvironmentVariableTarget]::User)"
