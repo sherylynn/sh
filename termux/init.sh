fd547b4e143cc4650c150a48901fe8f5df4b0b37
sherylynn <352281674@qq.com>
2019-01-21 12:43:47 +0800

    add termux.properties for extra keys

--- termux/init.sh
+++ termux/init.sh
@@ -1,4 +1,6 @@
 pkg install apt openssh -y
 pkg install htop coreutils
 #cat bashrc >> $HOME/.bashrc
+mkdir -p ~/.termux
+ln -s -f ~/sh/termux/termux.properties $HOME/termux.properties
 ln -s -f ~/sh/termux/bashrc $HOME/.bashrc
