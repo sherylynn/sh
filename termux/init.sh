8a01e7f0e71c5fad6356c9c6a9a463bf6f726168
sherylynn <352281674@qq.com>
2019-02-02 13:09:57 +0800

    fix termux.properties

--- termux/init.sh
+++ termux/init.sh
@@ -2,5 +2,5 @@ pkg install apt openssh -y
 pkg install htop coreutils
 #cat bashrc >> $HOME/.bashrc
 mkdir -p ~/.termux
-ln -s -f ~/sh/termux/termux.properties $HOME/termux.properties
+ln -s -f ~/sh/termux/termux.properties $HOME/.termux/termux.properties
 ln -s -f ~/sh/termux/bashrc $HOME/.bashrc
