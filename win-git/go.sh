63027e11cfa062837564d25306ddbbcc571f403b
sherylynn <352281674@qq.com>
2019-02-10 22:31:46 +0800

    add update go install bash

--- win-git/go.sh
+++ win-git/go.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-lynn=$HOME
+INSTALL_PATH=$HOME/tools
+GO_HOME=$INSTALL_PATH/go
 GO_VERSION=1.11
 GO_ARCH=amd64
 #GO_ARCH=arm64
 #GO_ARCH=armv6l
 #arm64
+OS=win
+#linux
 cd $HOME
 if [ ! -d "$HOME/tools" ]; then
   mkdir $HOME/tools
@@ -26,9 +29,9 @@ done
 
 shift $(($OPTIND - 1))
 
-wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
-  tar -C $HOME/tools -xzf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
-  rm go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
+wget https://dl.google.com/go/go${GO_VERSION}.${OS}-${GO_ARCH}.tar.gz && \
+  tar -C $HOME/tools -xzf go${GO_VERSION}.${OS}-${GO_ARCH}.tar.gz && \
+  rm go${GO_VERSION}.${OS}-${GO_ARCH}.tar.gz
 echo 'export GOROOT=$HOME/tools/go'>>~/.bashrc
 echo 'export PATH=$PATH:$GOROOT/bin'>>~/.bashrc
 echo 'export PATH=$PATH:$HOME/go/bin'>>~/.bashrc
