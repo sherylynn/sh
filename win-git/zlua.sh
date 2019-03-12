186a5a431c9ad4e0e50f3e56ae9da3d88d76adc8
sherylynn <352281674@qq.com>
2019-03-12 23:05:28 +0800

    add z.lua for cd

---
+++ win-git/zlua.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+INSTALL_PATH=$HOME/tools
+zluaHOME=$INSTALL_PATH/zlua
+BASH_DIR=$INSTALL_PATH/rc
+TOOLSRC_NAME=zluarc
+TOOLSRC=$BASH_DIR/${TOOLSRC_NAME}
+if [[ "$(uname)" == *MINGW* ]]; then
+  BASH_FILE=~/.bash_profile
+elif [[ "$(uname)" == *Linux* ]]; then
+  BASH_FILE=~/.bashrc
+elif [[ "$(uname)" == *Darwin* ]]; then
+  BASH_FILE=~/.bash_profile
+fi
+if [ ! -d "${BASH_DIR}" ]; then
+  mkdir $BASH_DIR
+fi
+if [[ "$(cat ${BASH_FILE})" != *${TOOLSRC_NAME}* ]]; then
+  echo "test -f ${TOOLSRC} && . ${TOOLSRC}" >> ${BASH_FILE}
+fi
+echo 'eval "$(lua '${zluaHOME}'/z.lua --init bash)"' > $TOOLSRC
+cd $INSTALL_PATH
+git clone https://github.com/skywind3000/z.lua $zluaHOME
