59f0ce990cea9468d2ab953880e4b44d2164dddd
sherylynn <352281674@qq.com>
2019-03-16 04:14:13 +0800

    change spacemacs to dev branch

--- spacemacs.sh
+++ spacemacs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 rm -rf ~/.spacemacs
 git clone https://github.com/sherylynn/spacemacs-private ~/.spacemacs.d
