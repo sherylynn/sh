5bc6a7a1a510783e4b562b56db2ced7dc25a2e7a
sherylynn <352281674@qq.com>
2019-03-17 20:06:02 +0800

    add install script lua

--- msys2/lua_build.sh
+++ msys2/lua_build.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+INSTALL_PATH=$HOME/tools
+SOFT_HOME=$INSTALL_PATH/lua
+BASH_DIR=$INSTALL_PATH/rc
+TOOLSRC_NAME=luarc
+TOOLSRC=$BASH_DIR/${TOOLSRC_NAME}
 SOFT_VERSION=5.3.5
-SOFT_NAME=lua-${SOFT_VERSION}
-SOFT_PACK=${SOFT_NAME}.tar.gz
+if [[ "$(uname)" =~ (MINGW)|(MSYS) ]]; then
+  BASH_FILE=~/.bash_profile
+  PLATFORM=win
+elif [[ "$(uname)" == *Linux* ]]; then
+  BASH_FILE=~/.bashrc
+  PLATFORM=Linux
+elif [[ "$(uname)" == *Darwin* ]]; then
+  BASH_FILE=~/.bash_profile
+  PLATFORM=MacOS
+fi
+SOFT_FILE_NAME=lua-${SOFT_VERSION}
+SOFT_FILE_PACK=${SOFT_FILE_NAME}.tar.gz
 cd ~
-curl -o ${SOFT_PACK} -L http://www.lua.org/ftp/${SOFT_PACK}
-mkdir -p ${SOFT_NAME}
-tar -xzf ${SOFT_PACK} -C ${SOFT_NAME}
-cd ${SOFT_NAME}/${SOFT_NAME}
+curl -o ${SOFT_FILE_PACK} -L http://www.lua.org/ftp/${SOFT_FILE_PACK}
+mkdir -p ${SOFT_FILE_NAME}
+tar -xzf ${SOFT_FILE_PACK} -C ${SOFT_FILE_NAME}
+cd ${SOFT_FILE_NAME}/${SOFT_FILE_NAME}
 make mingw
 cp -R src ~/tools/lua
+#--------------------------
+if [ ! -d "${BASH_DIR}" ]; then
+  mkdir $BASH_DIR
+fi
+if [[ "$(cat ${BASH_FILE})" != *${TOOLSRC_NAME}* ]]; then
+  echo "test -f ${TOOLSRC} && . ${TOOLSRC}" >> ${BASH_FILE}
+fi
+echo "export PATH=$SOFT_HOME:"'$PATH'> $TOOLSRC
