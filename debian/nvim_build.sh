6755ecf144d316d06262594a8ee2e5c3d68e85cb
sherylynn <352281674@qq.com>
2019-03-22 13:58:21 +0800

    update nvim build type

--- debian/nvim_build.sh
+++ debian/nvim_build.sh
@@ -2,15 +2,27 @@
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 cd ~
 git clone https://github.com/neovim/neovim
+##
 cd neovim
-mkdir -p .deps
-cd .deps
-cmake ../third-party
 #core_num=$(cat /proc/cpuinfo|grep "processer"|wc -l)
 core_num=$(nproc)
-make -j$core_num
-cd ..
-mkdir -p build
-cd build
-cmake ..
-make -j$core_num
+if [[ ${BUILD_TYPE} =~ (DEBUG) ]]; then
+  mkdir -p .deps
+  cd .deps
+  cmake ../third-party
+  cd ..
+  mkdir -p build
+  cd build
+  cmake ..
+  make -j$core_num
+  sudo make install
+elif [[ ${BUILD_TYPE} =~ (RELEASE) ]]; then
+  rm -rf build
+  make clean
+  make CMAKE_BUILD_TYPE=Release -j$core_num
+  sudo make install
+else
+  rm -rf build
+  make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim" -j$core_num
+  make install
+fi
