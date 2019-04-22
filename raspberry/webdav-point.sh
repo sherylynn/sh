2bb3ecc1edbba56f4a58cebd300b14e98f9fc24d
sherylynn <352281674@qq.com>
2019-04-22 15:08:37 +0800

    update for webdav mount

--- raspberry/webdav-point.sh
+++ raspberry/webdav-point.sh
@@ -1,3 +1,5 @@
 #sudo apt install davfs2
-sudo mount -t davfs -o uid=33,gid=0,file_mode=007,dir_mode=007,username=${webdav_user},password=${webdav_password} ${webdav_url} /home/pi/webdav-point
+sudo mount -t davfs https://pi.sherylynn.win/remote.php/webdav /home/lynn/webdav-point
+#davfs does support username or password option
+#sudo mount -t davfs -o uid=33,gid=0,file_mode=007,dir_mode=007,username=${webdav_user},password=${webdav_password} ${webdav_url} /home/pi/webdav-point
 docker exec cloud_app chown www-data:root /webdav-point
