8c0cea6f7cc453cda03eff8c06bb683378a08e4c
sherylynn <352281674@qq.com>
2019-07-28 21:20:51 +0800

    add apktook.sh

---
+++ win-git/apktool.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+INSTALL_PATH=$HOME/tools
+SOFT_HOME=$INSTALL_PATH/apktool
+#LIBS_HOME=$INSTALL_PATH/lua
+LIBS_VERSION=2.4.0
+BASH_DIR=$INSTALL_PATH/rc
+TOOLSRC_NAME=apktoolrc
+TOOLSRC=$BASH_DIR/${TOOLSRC_NAME}
+if [[ "$(uname)" =~ (MINGW)|(MSYS) ]]; then
+  BASH_FILE=~/.bash_profile
+  PLATFORM=windows
+elif [[ "$(uname)" == *Linux* ]]; then
+  BASH_FILE=~/.bashrc
+  PLATFORM=linux
+elif [[ "$(uname)" == *Darwin* ]]; then
+  BASH_FILE=~/.bash_profile
+  PLATFORM=osx
+fi
+
+BIN_FILE_NAME=apktool
+if [[ ${PLATFORM} == windows ]]; then
+  BIN_FILE_PACK=${BIN_FILE_NAME}.bat
+else
+  BIN_FILE_PACK=${BIN_FILE_NAME}
+fi
+
+LIBS_FILE_NAME=${BIN_FILE_NAME}_${LIBS_VERSION}
+LIBS_FILE_PACK=${LIBS_FILE_NAME}.jar
+#--------------------------
+# Install LIBS
+#--------------------------
+if ! command -v apktool && ! command -v apktool.bat ;then
+  if [ ! -d "${INSTALL_PATH}" ]; then
+    mkdir $INSTALL_PATH
+  fi
+
+  if [ ! -f "${BIN_FILE_PACK}" ]; then
+    curl -o ${BIN_FILE_PACK} -L https://github.com/iBotPeaches/Apktool/blob/master/scripts/${PLATFORM}/${BIN_FILE_PACK}
+  fi
+  
+  if [ ! -f "${LIBS_FILE_PACK}" ]; then
+    curl -o ${LIBS_FILE_PACK} -L https://github.com/iBotPeaches/Apktool/releases/latest/download/${LIBS_FILE_PACK}
+  fi
+
+fi
+#--------------------------
+if [ ! -d "${BASH_DIR}" ]; then
+  mkdir $BASH_DIR
+fi
+if [[ "$(cat ${BASH_FILE})" != *${TOOLSRC_NAME}* ]]; then
+  echo "test -f ${TOOLSRC} && . ${TOOLSRC}" >> ${BASH_FILE}
+fi
+
+echo "export PATH=$SOFT_HOME:"'$PATH'> $TOOLSRC
