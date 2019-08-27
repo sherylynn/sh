db08ce3d8ee5642049f3e366f3b0b3da15345bb9
sherylynn <352281674@qq.com>
2019-08-27 21:11:29 +0800

    update jupyter.sh for win-git

---
+++ win-git/jupyter.sh
@@ -0,0 +1,17 @@
+#just for termux ,so it is lack of sudo
+echo "add .local/bin to path first"
+apt install libcrypt-dev libzmq libzmq-dev
+pip3 install --user jupyter numpy 
+# env
+#PIP3_PATH=$(python3 -m site --user-base)"/bin"
+#export PATH=$PATH:$PIP3_PATH
+#echo "export PATH=\$PATH:"${PIP3_PATH} >> ~/.bashrc
+cd ~
+if [ ! -d "$HOME/.jupyter" ]; then
+  mkdir $HOME/.jupyter
+fi
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+jupyter nbextension enable vim_binding/vim_binding
+jupyter notebook password
