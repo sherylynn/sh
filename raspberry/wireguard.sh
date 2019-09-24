0313a1e2ea667bde4c34cedafdef902229bbbae9
sherylynn <352281674@qq.com>
2019-09-24 11:11:49 +0800

    update for debian buster

--- raspberry/wireguard.sh
+++ raspberry/wireguard.sh
@@ -8,7 +8,9 @@ echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee --append /etc/
 
 sudo apt-get install dirmngr 
 sudo apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 8B48AD6246925553 
-printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | sudo tee --append /etc/apt/preferences.d/limit-unstable
+sudo apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 7638D0442B90D010
+sudo apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' | sudo tee --append /etc/apt/preferences.d/limit-unstable
 sudo apt-get update
 sudo apt-get install wireguard -y
 #wireguard需要内核模块,似乎在docker中也需要
