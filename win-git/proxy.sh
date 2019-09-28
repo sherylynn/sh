049e8d1392243d6e64066b68a2953676a7edff88
sherylynn <352281674@qq.com>
2019-09-29 02:26:34 +0800

    update proxy for rust and golang

--- win-git/proxy.sh
+++ win-git/proxy.sh
@@ -1,2 +1,6 @@
 #export http_proxy=http://127.0.0.1:8087
 export http_proxy=http://127.0.0.1:10808
+export https_proxy=http://127.0.0.1:10808
+
+git config --global https.proxy http://127.0.0.1:10808
+git config --global https.proxy https://127.0.0.1:10808
--- win-git/rustup.sh
+++ win-git/rustup.sh
@@ -19,6 +19,10 @@ mkdir -p $CARGO_HOME
 
 export RUSTUP_HOME=$RUSTUP_HOME
 export CARGO_HOME=$CARGO_HOME
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 echo 'export RUSTUP_HOME='${RUSTUP_HOME}>$TOOLSRC
 echo 'export CARGO_HOME='${CARGO_HOME}>>$TOOLSRC
+echo 'export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup'>>$TOOLSRC
+echo 'export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static'>>$TOOLSRC
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable
