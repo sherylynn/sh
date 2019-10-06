7977bf34c73e6e4decc619c07d79505581275b2d
sherylynn <352281674@qq.com>
2019-10-06 12:33:06 +0800

    update vscode for vnc

--- raspberry/vscode.sh
+++ raspberry/vscode.sh
@@ -1,6 +1,14 @@
 #/bin/bash
 cd ~
-wget -O vscode.deb  https://github.com/stevedesmond-ca/vscode-arm/releases/download/1.28.0/vscode-1.28.0.deb
+wget -O vscode.deb  https://github.com/stevedesmond-ca/vscode-arm/releases/download/1.28.2/vscode-1.28.2.deb
 sudo dpkg -i vscode.deb
 sudo apt install -f
 rm -f vscode.deb
+
+# hack libs for vnc run
+sudo cp /usr/lib/arm-linux-gnueabihf/libxcb.so.1.1.0 /usr/share/code/
+cd /usr/share/code/
+sudo ln -s libxcb.so.1.1.0  libxcb.so
+sudo ln -s libxcb.so.1.1.0  libxcb.so.1
+sudo sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' libxcb.so.1.1.0
+
