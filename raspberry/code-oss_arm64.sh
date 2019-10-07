590469a31cf434772d687398d8b6f1adf5d65ee5
sherylynn <352281674@qq.com>
2019-10-07 16:27:20 +0800

    change code-oss version to 1.28.0

--- raspberry/code-oss_arm64.sh
+++ raspberry/code-oss_arm64.sh
@@ -1,4 +1,4 @@
-wget -O code-oss_arm64.deb --content-disposition https://packagecloud.io/headmelted/codebuilds/packages/debian/stretch/code-oss_1.32.0-1550644676_arm64.deb/download.deb
+wget -O code-oss_arm64.deb --content-disposition https://packagecloud.io/headmelted/codebuilds/packages/ubuntu/xenial/code-oss_1.28.0-1537780154_arm64.deb/download.deb
 sudo dpkg -i code-oss_arm64.deb
 sudo apt install -f
 rm -f code-oss_arm64.deb
@@ -6,6 +6,7 @@ sudo ln -sf /usr/bin/code-oss /usr/bin/code
 #suit for sync extension
 sudo mkdir -p /usr/share/code/bin
 sudo ln -sf /usr/share/code-oss/bin/code-oss /usr/share/code/bin/code
+sudo ln -sf /usr/share/code-oss/bin/code-oss /usr/share/code-oss/bin/code
 # hack libs for vnc run
 sudo cp /usr/lib/aarch64-linux-gnu/libxcb.so.1.1.0 /usr/share/code-oss/
 cd /usr/share/code-oss/
