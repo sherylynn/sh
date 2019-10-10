56f77c559f9e58a09e2fee3b7936797927660951
sherylynn <352281674@qq.com>
2019-10-10 09:39:52 +0800

    update recovery.sh

--- win-git/linux_deploy_init.sh
+++ win-git/linux_deploy_init.sh
@@ -1,3 +1,3 @@
-sudo apt install htop lua5.3 curl wget vim pigz git adb -y
+sudo apt install htop lua5.3 curl wget vim neovim pigz git adb -y
 sudo apt install chromium -y
 sudo apt install chromium-browser build-essential -y
--- win-git/recovery.sh
+++ win-git/recovery.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. $(dirname "$0")/toolsinit.sh
 cd ~
 sudo apt install pigz -y
-tar -I pigz -xvf ~/download/backup.tar.gz
+tar -I pigz -xvf ~/download/backup_$(arch).tar.gz
