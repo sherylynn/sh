2d55ec4fa41ac2b6fc41aadd11ee966c2e9258e0
sherylynn <352281674@qq.com>
2019-10-11 02:21:09 +0800

    update script in batch

--- msys2/nvim_build_step_2.bat
+++ msys2/nvim_build_step_2.bat
@@ -7,5 +7,8 @@ mingw32-make -j
 cd ..
 mkdir build
 cd build
-cmake -G "MinGW Makefiles" -DGPERF_PRG="C:\msys64\usr\bin\gperf.exe" -DCMAKE_INSTALL_PREFIX="$HOME/neovim" ..
+cmake -G "MinGW Makefiles" -DGPERF_PRG="C:\msys64\usr\bin\gperf.exe" 
+::不再指定安装位置,或者安装在直接下面
+::因为bat是不识别$HOME
+::-DCMAKE_INSTALL_PREFIX="$HOME/neovim" ..
 mingw32-make -j
