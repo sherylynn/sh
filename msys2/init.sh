79af58e8a25f9e099867a4d99ee2bcf6bf77b6fd
sherylynn <352281674@qq.com>
2019-10-14 00:53:04 +0800

    update $HOME in msys2

--- msys2/init.sh
+++ msys2/init.sh
@@ -3,3 +3,9 @@
 #pacman -Su
 pacman -S git unzip zip
 #pacman -S mingw64/mingw-w64-x86_64-lua
+#------------------init function----------------
+. $(dirname "$0")/../win-git/toolsinit.sh
+INSTALL_PATH=$HOME/tools
+TOOLSRC_NAME=msys2rc
+TOOLSRC=$(toolsRC $TOOLSRC_NAME)
+echo export $HOME=$HOMEPATH>$TOOLSRC
