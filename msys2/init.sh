ce8b01e4e4faff5921d53097feaea84dd48c6086
sherylynn <352281674@qq.com>
2019-10-14 16:43:42 +0800

    update $HOME

--- msys2/init.sh
+++ msys2/init.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #pacman -Sy
 #pacman -Su
-pacman -S git unzip zip
+if command -v git >/dev/null 2>&1; then
+  echo 'git exists'
+else
+  pacman -S git unzip zip
+fi
 #pacman -S mingw64/mingw-w64-x86_64-lua
 #------------------init function----------------
 . $(dirname "$0")/../win-git/toolsinit.sh
+#. $(dirname "$0")/../win-git/winPath.sh
 INSTALL_PATH=$HOME/tools
 TOOLSRC_NAME=msys2rc
 TOOLSRC=$(toolsRC $TOOLSRC_NAME)
-echo 'export HOME=$HOMEPATH' > $TOOLSRC
+#echo 'export HOME='$(winPath $USERPROFILE)> $TOOLSRC
+#echo 'export HOME='$(cygpath -u $USERPROFILE)> $TOOLSRC
+
+#vi /etc/nsswitch.conf -c '%s/cygwin/windows cygwin/g'
+sed -i 's/: cygwin/: windows cygwin/g' /etc/nsswitch.conf
