81ac79a95f46f7f9d05488fbb20dad2619b74663
sherylynn <352281674@qq.com>
2019-10-18 00:34:28 +0800

    update zsh for msys

--- win-git/move2zsh.sh
+++ win-git/move2zsh.sh
@@ -2,3 +2,6 @@
 . $(dirname "$0")/toolsinit.sh
 echo $(bash_file)
 cat $(bash_file) |grep test >$(alltoolsrc_file)
+if [[ "$(cat $(bash_file))" != *zsh* ]]; then
+  echo zsh >> $(bash_file)
+fi
--- win-git/ohmyz.sh
+++ win-git/ohmyz.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
