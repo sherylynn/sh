7a0f57521215b24919f20421c345ddcfe0264377
sherylynn <352281674@qq.com>
2019-11-04 14:42:53 +0800

    update android.sh for build vlc

--- win-git/android.sh
+++ win-git/android.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
-INSTALL_PATH=$HOME/tools
-SOFT_HOME=$INSTALL_PATH/Android/Sdk
-LIBS_HOME=$INSTALL_PATH/Android/android-studio
-BASH_DIR=$INSTALL_PATH/rc
+. $(dirname "$0")/toolsinit.sh
+SOFT_HOME=$(install_path)/Android/Sdk
+LIBS_HOME=$(install_path)/Android/android-studio
 TOOLSRC_NAME=androidrc
-TOOLSRC=$BASH_DIR/${TOOLSRC_NAME}
-if [[ "$(uname)" =~ (MINGW)|(MSYS) ]]; then
-  BASH_FILE=~/.bash_profile
-  PLATFORM=win
-elif [[ "$(uname)" == *Linux* ]]; then
-  BASH_FILE=~/.bashrc
-  PLATFORM=Linux
-elif [[ "$(uname)" == *Darwin* ]]; then
-  BASH_FILE=~/.bash_profile
-  PLATFORM=MacOS
-fi
-
+TOOLSRC=$(toolsRC $TOOLSRC_NAME)
 #--------------------------
 #URL=
 #--------------------------
 #------load module---------
 . ./winPath.sh
 #--------------------------
-if [ ! -d "${BASH_DIR}" ]; then
-  mkdir $BASH_DIR
-fi
-if [[ "$(cat ${BASH_FILE})" != *${TOOLSRC_NAME}* ]]; then
-  echo "test -f ${TOOLSRC} && . ${TOOLSRC}" >> ${BASH_FILE}
-fi
 export ANDROID_STUDIO_HOME=$LIBS_HOME
 export SDK_HOME=$SOFT_HOME
 export CLASSPATH=.:$LIBS_HOME/jre/lib/tools.jar:$LIBS_HOME/jre/lib/dt.jar:$LIBS_HOME/jre/jre/lib/rt.jar
 export JAVA_HOME=$LIBS_HOME/jre
 export GRADLE_HOME=$LIBS_HOME/gradle/gradle-4.10.1
-echo "export ANDROID_STUDIO_HOME=$LIBS_HOME" > $TOOLSRC
-echo "export SDK_HOME=$SOFT_HOME" >> $TOOLSRC
-echo "export ANDROID_HOME=$SOFT_HOME" >> $TOOLSRC
-echo "export ANDROID_SDK_ROOT=$SOFT_HOME" >> $TOOLSRC
-echo "export CLASSPATH=.:$LIBS_HOME/jre/lib/tools.jar:$LIBS_HOME/jre/lib/dt.jar:$LIBS_HOME/jre/jre/lib/rt.jar" >> $TOOLSRC
-echo "export JAVA_HOME=$LIBS_HOME/jre" >> $TOOLSRC
-#echo "export JAVA_HOME=$(winDoublePath $LIBS_HOME/jre)" >> $TOOLSRC
-#echo "export JAVA_HOME=$(cygpath -w $LIBS_HOME/jre)" >> $TOOLSRC
-echo "export GRADLE_HOME=$LIBS_HOME/gradle/gradle-4.10.1" >> $TOOLSRC
-echo "export PATH="'$PATH'":$GRADLE_HOME/bin:$ANDROID_STUDIO_HOME/jre/jre/bin:$ANDROID_STUDIO_HOME/jre/bin:$ANDROID_STUDIO_HOME/bin:$SDK_HOME/emulator:$SDK_HOME/platform-tools" >> $TOOLSRC
+
+tee $TOOLSRC <<EOF
+export ANDROID_STUDIO_HOME=$LIBS_HOME
+export SDK_HOME=$SOFT_HOME
+export ANDROID_HOME=$SOFT_HOME
+export ANDROID_SDK_ROOT=$SOFT_HOME
+export ANDROID_SDK=$SOFT_HOME
+export ANDROID_NDK=$SOFT_HOME/ndk-bundle
+export CLASSPATH=.:$LIBS_HOME/jre/lib/tools.jar:$LIBS_HOME/jre/lib/dt.jar:$LIBS_HOME/jre/jre/lib/rt.jar
+export JAVA_HOME=$LIBS_HOME/jre
+export GRADLE_HOME=$LIBS_HOME/gradle/gradle-4.10.1
+export PATH=\$PATH:$GRADLE_HOME/bin:$ANDROID_STUDIO_HOME/jre/jre/bin:$ANDROID_STUDIO_HOME/jre/bin:$ANDROID_STUDIO_HOME/bin:$SDK_HOME/emulator:$SDK_HOME/platform-tools
+EOF
