5cf7951017434edfc16250630ad94754764ab8d3
sherylynn <352281674@qq.com>
2019-11-20 16:18:56 +0800

    add termux backup script

---
+++ termux/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#------------------init function----------------
+apt install getconf
+. $(dirname "$0")/../win-git/toolsinit.sh
+cd ~
+#sudo apt install pigz -y
+#npm cache clean --force
+termux-setup-storage
+#tar -zpcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache ./*
+#tar -pcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache -I pigz ./*
+
+#tar --use-compress-program="pigz -9" -pcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache  ./*
+#./* 会没有隐藏文件
+#tar --exclude=download --exclude=.cache  -pcvf - ./*  |pigz --best > ~/download/backup.tar.gz
+#tar --exclude=.dbus --exclude=.config --exclude=download --exclude=.x* --exclude=.vnc* --exclude=.cache  -pcvf - ./  |pigz --best > ~/download/backup.tar.gz
+
+tar -zpcvf ~/storage/downloads/backup_termux_$(arch).tar.gz --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config --exclude=storage --exclude=.x* --exclude=.vnc* --exclude=.cache ./*
---
+++ termux/recovery.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. $(dirname "$0")/../win-git/toolsinit.sh
+cd ~
+tar -xzvf ~/storage/downloads/backup_termux_$(arch).tar.gz
