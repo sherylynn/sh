9c0445aab00470254a8dcf730c66affcddd4ae38
sherylynn <352281674@qq.com>
2019-11-22 20:50:50 +0800

    update mirrors script for termux

--- termux/mirrors.sh
+++ termux/mirrors.sh
@@ -1,1 +1,3 @@
-sed -i 's,https://termux.net,https://mirrors.ustc.edu.cn/termux,' $PREFIX/etc/apt/sources.list
+#sed -i 's,https://termux.net,https://mirrors.ustc.edu.cn/termux,' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
+apt update && apt upgrade
