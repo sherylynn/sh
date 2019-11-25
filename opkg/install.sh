fb8f5ddb4d72a275fa3c27f83a6ae9b8ca6423c5
sherylynn <352281674@qq.com>
2019-11-25 20:27:05 +0800

    update opkg

--- opkg/install.sh
+++ opkg/install.sh
@@ -1,3 +1,4 @@
-opkg install curl wget node go git-http ca-bundle htop zsh
+opkg install curl wget git-http screen ca-bundle htop bash zsh ncdu
+opkg install node node-npm go python3 python3-pip lua
 #opkg install vim-full vim-runtime
 #opkg install vim
