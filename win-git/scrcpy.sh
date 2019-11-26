1dc837e03855d047b1ce45aefb50826b6706d72a
sherylynn <352281674@qq.com>
2019-11-26 15:11:26 +0800

    add scrcpy for windows

---
+++ win-git/scrcpy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+. $(dirname "$0")/toolsinit.sh
+NAME=scrcpy
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+SOFT_HOME=$(install_path)/${NAME}
+SOFT_VERSION=1.11
+SOFT_ARCH=64
+
+# uname Linux .bashrc uname Darwin MINGW64 .bash_profile
+PLATFORM=$(platform)
+case $(arch) in 
+  amd64) SOFT_ARCH=64;;
+  386) SOFT_ARCH=32;;
+esac
+
+SOFT_FILE_NAME=${NAME}-${PLATFORM}${SOFT_ARCH}-v${SOFT_VERSION}
+SOFT_FILE_PACK=$(soft_file_pack $SOFT_FILE_NAME)
+# init pwd
+cd $HOME
+
+SOFT_URL=https://github.com/Genymobile/${NAME}/releases/download/v${SOFT_VERSION}/${SOFT_FILE_PACK}
+#if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
+if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
+  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+  $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
+  
+  rm -rf ${SOFT_HOME} && \
+    mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME} 
+fi
+#--------------new .toolsrc-----------------------
+export PATH=$PATH:${SOFT_HOME}
+
+echo 'export PATH=$PATH:'${SOFT_HOME}>>${TOOLSRC}
+
+#  ----windows bat----
+if [[ $WIN_PATH  ]]; then
+  if [[ $PLATFORM == windows ]]; then
+    windowsENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
+    echo $windowsENV
+    setx Path "$windowsENV"
+  fi
+fi
