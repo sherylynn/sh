374af97e24bfef5c1d34ed0920a90ce0be7a1664
sherylynn <352281674@qq.com>
2019-11-30 00:36:41 +0800

    update crontab script for termux

--- termux/crontab.sh
+++ termux/crontab.sh
@@ -1,1 +1,3 @@
-echo "*/15 * * * * ~/sh/termux/namesilo.sh >> ~/cron.log 2>&1" > ~/crontab_conf && crontab ~/crontab_conf
+command="*/15 * * * * ~/sh/termux/namesilo.sh >> ~/cron.log 2>&1"
+#echo $command > ~/crontab_conf && crontab ~/crontab_conf
+echo $command > $PREFIX/var/spool/cron/crontabs/$(whoami)
