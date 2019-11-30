243cd0c112144b3d3a19c3630f8f155b35f85085
sherylynn <352281674@qq.com>
2019-12-01 01:48:51 +0800

    add ubt support seafile client

--- seafile/seafile_client.sh
+++ seafile/seafile_client.sh
@@ -1,3 +1,4 @@
+# for debian
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61
 
 echo deb http://deb.seadrive.org stretch main | sudo tee /etc/apt/sources.list.d/seafile.list
@@ -5,3 +6,11 @@ echo deb http://deb.seadrive.org stretch main | sudo tee /etc/apt/sources.list.d
 sudo apt update
 
 sudo apt install seafile-gui
+# for ubuntu
+sudo add-apt-repository ppa:seafile/seafile-client
+
+sudo apt-get update
+
+sudo apt-get install seafile-gui
+### miss for 19.10 just for 19.04 disco
+###  echo deb http://ppa.launchpad.net/seafile/seafile-client/ubuntu disco main |sudo tee /etc/apt/sources.list.d/seafile-ubuntu-seafile-client-eoan.list
