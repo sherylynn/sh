c0d6a22063539a08484f23dd159ab1fae0e036b7
sherylynn <352281674@qq.com>
2019-12-25 21:46:00 +0800

    split string with '"'

--- win-git/github_release.sh
+++ win-git/github_release.sh
@@ -1,7 +1,10 @@
-get_latest_release(){
+get_latest_release_version(){
   local user_repos=$1
   curl --silent "https://api.github.com/repos/${user_repos}/releases/latest" |
-    #awk '$0 ~ /tag_name/'
-    grep '"tag_name":'
+  #awk '$0 ~ /tag_name/'
+  grep '"tag_name":' |
+  #use '"' to split string and print $4
+  awk -F '["]' '{print $4}'  
 }
-echo $(get_latest_release iawia002/annie)
+echo $(get_latest_release_version iawia002/annie)
+echo $(get_latest_release_version nodejs/node)
