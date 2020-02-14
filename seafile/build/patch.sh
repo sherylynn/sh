f0c36fcab3e286914a1853eb95299f3980db51b5
sherylynn <352281674@qq.com>
2020-02-14 12:39:31 -0500

    update for webdav for seafile

--- seafile/build/patch.sh
+++ seafile/build/patch.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
 sed -i 's/127.0.0.1/0.0.0.0/g' conf/gunicorn.conf.py 
+sed -i 's/enabled = false/enabled = true/g' conf/seafdav.conf
+WEBDAV='share_name = webdav'
+#sed -i "/share_name/c $webdav" conf/seafdav.conf 
+sed -i '/share_name/c share_name = \/webdav' conf/seafdav.conf 
--- seafile/seafile.sh
+++ seafile/seafile.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 docker run -itd --name seafile_db --restart=always -v /root/seafile_drive/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD="seafile" -e MYSQL_DATABASE="seafile" mysql --default-authentication-plugin=mysql_native_password --skip-mysqlx
 docker run -itd --name seafile_app --restart=always \
-  -p 0.0.0.0:8000:8000 -p 0.0.0.0:8082:8082 \
+  -p 8000:8000 -p 8082:8082  -p 8080:8080 \
   -v /root/seafile_drive/seafile-data:/home/haiwen/seafile-data \
+  -v /root/seafile_drive/seafile-conf:/home/haiwen/conf \
   --link seafile_db:db seafile /bin/bash
 docker attach seafile_app
 #第一次初始化用上面的进 seafile-server-* 运行 setup-mysql 然后把后面的文件拷贝下来
