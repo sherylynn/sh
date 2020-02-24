446b55e551331449fb40f8042852846c5765ff3c
sherylynn <352281674@qq.com>
2020-02-24 23:49:37 +0800

    make apktool lastest and suit to zsh

--- win-git/apktool.sh
+++ win-git/apktool.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+. $(dirname "$0")/toolsinit.sh
+AUTHOR=iBotPeaches
+NAME=Apktool
+
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+SOFT_HOME=$(install_path)/${NAME}
+
 INSTALL_PATH=$HOME/tools
-SOFT_HOME=$INSTALL_PATH/apktool
-#LIBS_HOME=$INSTALL_PATH/lua
-LIBS_VERSION=2.4.0
+#沙雕的version是 v2.4.1 而不是2.4.1
+fuck_SOFT_VERSION=$(get_github_release_version $AUTHOR/$NAME)
+array_fuck_SOFT_VERSION=(${fuck_SOFT_VERSION//v/})
+SOFT_VERSION=${array_fuck_SOFT_VERSION[@]}
+echo $SOFT_VERSION
 DEX_VERSION=2.0
-BASH_DIR=$INSTALL_PATH/rc
-TOOLSRC_NAME=apktoolrc
-TOOLSRC=$BASH_DIR/${TOOLSRC_NAME}
-if [[ "$(uname)" =~ (MINGW)|(MSYS) ]]; then
-  BASH_FILE=~/.bash_profile
-  PLATFORM=windows
-elif [[ "$(uname)" == *Linux* ]]; then
-  BASH_FILE=~/.bashrc
-  PLATFORM=linux
-elif [[ "$(uname)" == *Darwin* ]]; then
-  BASH_FILE=~/.bash_profile
+
+PLATFORM=$(platform)
+if [[ "$PLATFORM" == "macos" ]]; then
   PLATFORM=osx
+elif [[ "$PLATFORM" == "win" ]];then
+  PLATFORM=windows
+elif [[ "$PLATFORM" == "linux" ]];then
+  PLATFORM=linux
 fi
 
 BIN_FILE_NAME=apktool
@@ -25,41 +31,32 @@ else
   BIN_FILE_PACK=${BIN_FILE_NAME}
 fi
 
-LIBS_FILE_NAME=${BIN_FILE_NAME}_${LIBS_VERSION}
-LIBS_FILE_PACK=${LIBS_FILE_NAME}.jar
+LIBS_FILE_NAME=${BIN_FILE_NAME}_${SOFT_VERSION}
+SOFT_FILE_PACK=${LIBS_FILE_NAME}.jar
 
 DEX_FILE_NAME=dex2jar
 DEX_FILE_PACK=${DEX_FILE_NAME}-${DEX_VERSION}.zip
-DEX_HOME=${INSTALL_PATH}/${DEX_FILE_NAME}
+DEX_HOME=$(install_path)/${DEX_FILE_NAME}
 #--------------------------
 # Install LIBS
 #--------------------------
-if [ ! -d "${SOFT_HOME}" ]; then
-  mkdir -p ${SOFT_HOME}
+
+SOFT_URL=https://github.com/$AUTHOR/$NAME/releases/download/v${SOFT_VERSION}/${SOFT_FILE_PACK}
+
+if [[ "$(${BIN_FILE_PACK} --version)" != *${SOFT_VERSION}* ]]; then
+  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+
+  rm -rf ${SOFT_HOME} && \
+  mkdir -p ${SOFT_HOME} && \
+    cp $(cache_folder)/${SOFT_FILE_PACK}  ${SOFT_HOME}/${BIN_FILE_NAME}.jar 
 fi
 
-if ! command -v apktool && ! command -v apktool.bat ;then
-  if [ ! -d "${INSTALL_PATH}" ]; then
-    mkdir $INSTALL_PATH
-  fi
-
-  if [ ! -f "${BIN_FILE_PACK}" ]; then
-    echo https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/${PLATFORM}/${BIN_FILE_PACK}
-    curl -o ${BIN_FILE_PACK} -L https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/${PLATFORM}/${BIN_FILE_PACK}
-  fi
-  
-  if [ ! -f "${LIBS_FILE_PACK}" ]; then
-    curl -o ${LIBS_FILE_PACK} -L https://github.com/iBotPeaches/Apktool/releases/latest/download/${LIBS_FILE_PACK}
-  fi
-
-  if [ ! -f "${BIN_FILE_NAME}.jar" ]; then
-    mv ${LIBS_FILE_PACK} ${BIN_FILE_NAME}.jar
-  fi
-
-  mv ${BIN_FILE_PACK} $SOFT_HOME/${BIN_FILE_PACK} && \
-  mv ${BIN_FILE_NAME}.jar $SOFT_HOME/${BIN_FILE_NAME}.jar
-
+if [ ! -f "${BIN_FILE_PACK}" ]; then
+  echo https://raw.githubusercontent.com/$AUTHOR/$NAME/master/scripts/${PLATFORM}/${BIN_FILE_PACK}
+  curl -o ${BIN_FILE_PACK} -L https://raw.githubusercontent.com/$AUTHOR/$NAME/master/scripts/${PLATFORM}/${BIN_FILE_PACK}
 fi
+
+mv ${BIN_FILE_PACK} $SOFT_HOME/${BIN_FILE_PACK}
 if ! command -v d2j-dex2jar && ! command -v d2j-dex2jar.bat ;then
   if [ ! -f "${DEX_FILE_PACK}" ]; then
     curl -o ${DEX_FILE_PACK} -L https://sourceforge.net/projects/dex2jar/files/${DEX_FILE_PACK}/download
@@ -75,12 +72,9 @@ if ! command -v d2j-dex2jar && ! command -v d2j-dex2jar.bat ;then
   rm -rf ${DEX_FILE_PACK}
 fi
 #--------------------------
-if [ ! -d "${BASH_DIR}" ]; then
-  mkdir $BASH_DIR
-fi
-if [[ "$(cat ${BASH_FILE})" != *${TOOLSRC_NAME}* ]]; then
-  echo "test -f ${TOOLSRC} && . ${TOOLSRC}" >> ${BASH_FILE}
-fi
 
-echo "export PATH=$SOFT_HOME:"'$PATH'> $TOOLSRC
-echo "export PATH=$DEX_HOME:"'$PATH'>>$TOOLSRC
+echo "export PATH=$SOFT_HOME:"'$PATH'> ${TOOLSRC}
+echo "export PATH=$DEX_HOME:"'$PATH'>>${TOOLSRC}
+
+#https://github.com/iBotPeaches/Apktool/releases/download/v2.4.1/apktool_2.4.1.jar
+#https://github.com/iBotPeaches/Apktool/releases/download/v2.4.1/apktool_v2.4.1.jar
