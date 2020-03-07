bca6e48a7e284df8cafc4dca58471ab010af5afc
sherylynn <352281674@qq.com>
2020-03-07 00:41:43 -0500

    update brightness.sh

---
+++ debian/brightness.sh
@@ -0,0 +1,16 @@
+#brightness_file=/sys/class/backlight/acpi_video0/brightness
+brightness_file=~/1.txt
+
+increase(){
+  echo $[$(cat $brightness_file)+1] | sudo tee $brightness_file
+}
+decrease(){
+  echo $[$(cat $brightness_file)-1] | sudo tee $brightness_file
+}
+case $1 in
+  -) $(decrease) ;;
+  +) $(increase) ;;
+  *) $(increase) ;;
+esac
+
+cat $brightness_file
