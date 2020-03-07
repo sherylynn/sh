c7dd1330eef317adb912d026b9174488f58a7463
sherylynn <352281674@qq.com>
2020-03-07 17:49:58 +0800

    finish brightness.sh for 4741g debian

--- debian/brightness.sh
+++ debian/brightness.sh
@@ -1,17 +1,17 @@
-#brightness_file=/sys/class/backlight/acpi_video0/brightness
-brightness_file=~/1.txt
+brightness_file=/sys/class/backlight/acpi_video0/brightness
+#brightness_file=~/1.txt
 
 increase(){
-  echo $[$(cat $brightness_file)+1] | sudo tee $brightness_file
+  echo $(($(cat $brightness_file)+1)) | sudo tee $brightness_file
 }
 decrease(){
-  echo $[$(cat $brightness_file)-1] | sudo tee $brightness_file
+  echo $(($(cat $brightness_file)-1)) | sudo tee $brightness_file
 }
 case $1 in
-  -) $(decrease) ;;
-  +) $(increase) ;;
-  *) $(increase) ;;
+  -) echo $(decrease ) ;;
+  +) echo $(increase ) ;;
+  *) echo $(increase ) "\nuse '-'' to decrease;+ to increase" ;;
 esac
 
-cat $brightness_file
-echo 1> $brightness_file
+#cat $brightness_file
+#echo 1>$brightness_file
