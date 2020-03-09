5883c759dd31000a53cc54294523bdb6e5d98d5a
sherylynn <352281674@qq.com>
2020-03-10 02:37:58 +0800

    update docker mirrors

--- docker_mirrors.sh
+++ docker_mirrors.sh
@@ -1,9 +1,10 @@
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://8znzd6qv.mirror.aliyuncs.com"]
+  "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]
 }
 EOF
+#"registry-mirrors": ["https://8znzd6qv.mirror.aliyuncs.com"]
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 #https://hlef81mt.mirror.aliyuncs.com
