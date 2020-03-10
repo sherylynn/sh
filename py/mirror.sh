56c53491fd1ed8a3b7d625c32f6fd9b293a1b26b
sherylynn <352281674@qq.com>
2020-03-11 03:59:26 +0800

    add fail namesilo for chinese telecom [fail because 443 is blocked]

--- py/mirror.sh
+++ py/mirror.sh
@@ -1,2 +1,3 @@
 pip3 install -i https://mirrors.aliyun.com/pypi/simple pip -U
+pip3 install -i https://mirrors.aliyun.com/pypi/simple pip3 -U
 pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple
