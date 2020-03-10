d485aa937d9d1cd32fc44829c63dbda863828e8a
sherylynn <352281674@qq.com>
2020-03-10 21:33:43 +0800

    add mirror for wireguard

--- debian/wireguard.sh
+++ debian/wireguard.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+hostname="http://deb.debian.org/debian/"
+mirror="http://mirrors.ustc.edu.cn/debian/"
+read -p "should we use china mirror ? y or n : " RESTART
+case $RESTART in
+  y) hostname=$mirror ;;
+  n) echo "enjoying" ;;
+esac
+
+echo $hostname
+
 sudo apt update
 sudo apt-get upgrade 
 
-echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee --append /etc/apt/sources.list.d/unstable.list
+echo "deb $hostname unstable main" | sudo tee --append /etc/apt/sources.list.d/unstable.list
 
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' | sudo tee --append /etc/apt/preferences.d/limit-unstable
 sudo apt-get update
