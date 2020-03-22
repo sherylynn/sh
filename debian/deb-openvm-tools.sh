7dd1210f062ee06dc5ba89cce431687955934d48
sherylynn <352281674@qq.com>
2020-03-22 12:51:01 +0800

    add vmware but fail

--- debian/deb-openvm-tools.sh
+++ debian/deb-openvm-tools.sh
@@ -6,6 +6,7 @@ sudo apt-get install open-vm-tools-desktop -y
 sudo tee /etc/systemd/system/mnt.hgfs.service <<-'EOF'
 [Unit]
 Description=Load VMware shared folders
+;open-vm-tools now has not these service
 ;Requires=vmware-vmblock-fuse.service
 ;After=vmware-vmblock-fuse.service
 ConditionPathExists=.host:/
@@ -16,7 +17,8 @@ Type=oneshot
 RemainAfterExit=yes
 ExecStart=
 ExecStart=/usr/bin/vmhgfs-fuse -o allow_other -o auto_unmount .host:/ /mnt/hgfs
-;-o umask=007专门的权限给770
+;-o umask=000 
+;专门的权限给777
 [Install]
 WantedBy=multi-user.target
 EOF
---
+++ seafile/install_with_vmware.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+vmware_folder=/mnt/hgfs/hdd
+hostname=$1
+help_message="add your hostname as args"
+greeting_message="now start process"
+case $hostname in
+  "")echo $help_message && exit;;
+  *)echo "$greeting_message\nyour hostname is $hostname" ;;
+esac
+
+#copy configure
+cp ./docker-compose_vm_pdf.yml ./docker-compose_vm_hostname.yml
+
+# change hostname
+
+vi ./docker-compose_vm_hostname.yml -c ":%s/pdf.sherylynn.win/$hostname/g" -c ":wq!"
+
+# link to new vmfile
+vi ./docker-compose_vm_hostname.yml -c ":%s/vmfile_docker_pdf/vmfile_docker_hostname/g" -c ":wq!"
+
+# change first /root to $HOME ,not all
+
+vi ./docker-compose_vm_hostname.yml -c ":%s#/root#$vmware_folder" -c ":wq!"
+
+server_start(){
+  # start server
+  docker-compose -f ./docker-compose_vm_hostname.yml up -d
+}
+
+server_stop(){
+  # stop server if server is running
+  docker-compose -f ./docker-compose_vm_hostname.yml down
+}
+
+server_restart(){
+  if [[ "$(docker ps -a |grep seafile)" == *"seafile"* ]] ;then
+    echo $(server_stop )
+    echo $(server_start )
+  else
+    echo $(server_start )
+  fi
+  echo "server finish"
+}
+#read -p "should we restart server ? y or n : " RESTART
+RESTART=y
+case $RESTART in
+  y) echo $(server_restart) ;;
+  n) echo "enjoying" ;;
+esac
