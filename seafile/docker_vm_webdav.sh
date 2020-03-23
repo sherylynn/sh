b8c447a92e833533692a5764743050ab7bcdafdb
sherylynn <352281674@qq.com>
2020-03-24 01:21:41 +0800

    add vmware webdav

---
+++ seafile/docker_vm_webdav.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+configureFile=/mnt/seafile/seafile_drive/seafile-data/seafile/conf/seafdav.conf
+sed -i 's/enabled = false/enabled = true/g' $configureFile
+WEBDAV='share_name = webdav'
+sed -i 's/fastcgi = false/fastcgi = true/g' $configureFile
+sed -i '/share_name/c share_name = \/seafdav' $configureFile
