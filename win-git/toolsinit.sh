33eaeaca3afd183bdbeb0bb955ea4e5465627b5a
sherylynn <352281674@qq.com>
2020-03-30 14:57:19 +0800

    move proxy back to http

--- win-git/toolsinit.sh
+++ win-git/toolsinit.sh
@@ -299,13 +299,13 @@ alias ze="zedit"
 
 proxy(){
   #export http_proxy=http://127.0.0.1:8087
-  #export http_proxy=http://127.0.0.1:10808
-  #export https_proxy=http://127.0.0.1:10808
+  export http_proxy=http://127.0.0.1:10808
+  export https_proxy=http://127.0.0.1:10808
 
-  export http_proxy=socks5://127.0.0.1:1080
-  export https_proxy=socks5://127.0.0.1:1080
-  export ALL_PROXY=socks5://127.0.0.1:1080
-  export all_proxy=socks5://127.0.0.1:1080
+  #export http_proxy=socks5://127.0.0.1:1080
+  #export https_proxy=socks5://127.0.0.1:1080
+  #export ALL_PROXY=socks5://127.0.0.1:1080
+  #export all_proxy=socks5://127.0.0.1:1080
 
 
   #git config --global https.proxy http://127.0.0.1:10808
