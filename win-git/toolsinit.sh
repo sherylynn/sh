bf7fcac9b439f329dc33748d6b71b4b0d5160144
sherylynn <352281674@qq.com>
2020-07-22 02:23:29 +0800

    change proxy to port 10808

--- win-git/toolsinit.sh
+++ win-git/toolsinit.sh
@@ -313,11 +313,11 @@ proxy(){
   #export all_proxy=socks5://127.0.0.1:1080
 
 
-  #git config --global https.proxy http://127.0.0.1:10808
-  #git config --global https.proxy https://127.0.0.1:10808
+  git config --global http.proxy http://127.0.0.1:10808
+  git config --global https.proxy https://127.0.0.1:10808
 
-  git config --global http.proxy socks5://127.0.0.1:1080
-    git config --global https.proxy socks5://127.0.0.1:1080
+  #git config --global http.proxy socks5://127.0.0.1:1080
+  #git config --global https.proxy socks5://127.0.0.1:1080
 
 }
 
