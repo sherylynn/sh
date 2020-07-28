5fd58cb54897fa43e29f052c55ad1cc9b8cc2ce8
sherylynn <352281674@qq.com>
2020-07-28 18:06:23 +0800

    update

--- debian/wsl_init.sh
+++ debian/wsl_init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo vi /etc/passwd -c "%s/home/mnt\/c\/Users/g" -c 'wq!'
-sudo ln -s /mnt/c /c
+#sudo vi /etc/passwd -c "%s/home/mnt\/c\/Users/g" -c 'wq!'
+#sudo ln -s /mnt/c /c
+
+sudo apt install curl axel wget htop vim -y
--- win-git/nodejs_all.sh
+++ win-git/nodejs_all.sh
@@ -88,17 +88,17 @@ yrm use taobao
 npm i -g yarn webpack http-server babel-cli pm2 typescript ts-node tslint eslint
 yarn config set cache-folder "$(install_path)/yarn-cache"
 #-----------------------
-if [[ $WIN_PATH ]]; then
-  if [[ ${PLATFORM} == win ]]; then
-    setx NPM_CONFIG_PREFIX $(cygpath -w $NPM_CONFIG_PREFIX)
-    setx NPM_CONFIG_CACHE $(cygpath -w $NPM_CONFIG_CACHE)
-    setx YARN_CACHE_FOLDER $(cygpath -w $YARN_CACHE_FOLDER)
-    setx ELECTRON_MIRROR 'http://npm.taobao.org/mirrors/electron/'
-    setx SQLITE3_BINARY_SITE 'http://npm.taobao.org/mirrors/sqlite3'
-    setx PHANTOMJS_CDNURL 'http://npm.taobao.org/mirrors/phantomjs'
-
-    winENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
-    echo $winENV
-    setx Path "$winENV"
-  fi
-fi
+#if [[ $WIN_PATH ]]; then
+#  if [[ ${PLATFORM} == win ]]; then
+#    setx NPM_CONFIG_PREFIX $(cygpath -w $NPM_CONFIG_PREFIX)
+#    setx NPM_CONFIG_CACHE $(cygpath -w $NPM_CONFIG_CACHE)
+#    setx YARN_CACHE_FOLDER $(cygpath -w $YARN_CACHE_FOLDER)
+#    setx ELECTRON_MIRROR 'http://npm.taobao.org/mirrors/electron/'
+#    setx SQLITE3_BINARY_SITE 'http://npm.taobao.org/mirrors/sqlite3'
+#    setx PHANTOMJS_CDNURL 'http://npm.taobao.org/mirrors/phantomjs'
+#
+#    winENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
+#    echo $winENV
+#    setx Path "$winENV"
+#  fi
+#fi
