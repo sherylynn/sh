4a6fc32f709e01e59141d8c10ebc9ef777ccd82a
sherylynn <352281674@qq.com>
2020-10-22 12:14:51 +0800

    add apt install for e2fsprogs

--- termux/e2fsprogs.sh
+++ termux/e2fsprogs.sh
@@ -1,4 +1,5 @@
-file_path=~/storage/download/deb_arm64.img
+apt install e2fsprogs -y
+file_path=~/storage/downloads/deb_arm64.img
 dd if=/dev/zero bs=1M count=1024 >> $file_path
 e2fsck -f $file_path
 resize2fs $file_path
