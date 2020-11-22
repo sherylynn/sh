0df6c77b19da39b1225e41fff2b9951895153762
sherylynn <352281674@qq.com>
2020-11-23 02:31:05 +0800

    add blueprints for build

---
+++ maruos/blueprints.sh
@@ -0,0 +1,13 @@
+cd ~
+git clone https://github.com/maruos/blueprints
+cd blueprints
+sudo apt install -y \
+    binfmt-support \
+    debootstrap \
+    fakeroot \
+    git \
+    lxc \
+    make \
+    qemu-user-static \
+    ubuntu-archive-keyring \
+sudo ./build.sh -b debian -n buster-container -- -r buster -a arm64 --minimal
