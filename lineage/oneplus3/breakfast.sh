53ccf487f563c23cda8bf89ddd9affe7fc8ee9a6
sherylynn <352281674@qq.com>
2020-12-02 19:28:40 +0800

    rename xml for oneplus3 lineage building

--- lineage/oneplus3/breakfast.sh
+++ lineage/oneplus3/breakfast.sh
@@ -1,17 +1,4 @@
 #!/bin/zsh
-#fix error not metion in instucntion
-# 按照官方教程，会莫名其妙缺少这个文件夹，需要clone回来
-cd ~/android/lineage/device
-mkdir -p oppo
-cd oppo
-git clone https://github.com/LineageOS/android_device_oppo_common common
-# 还莫名其妙缺了 kernel 需要手动clone
-
-cd ~/android/lineage
-mkdir -p kernel/oneplus
-cd ~/android/lineage/kernel/oneplus
-git clone https://github.com/LineageOS/android_kernel_oneplus_msm8996 -b lineage-16.0 msm8996
-# after finished above 
 cd ~/android/lineage
 source build/envsetup.sh
 #按照官方教程，会莫名其妙报错
--- lineage/oneplus3/pre_breakfast.sh
+++ lineage/oneplus3/pre_breakfast.sh
@@ -1,17 +1,6 @@
-# Extract proprietary blobs
-# in https://wiki.lineageos.org/extracting_blobs_from_zips.html#extracting-proprietary-blobs-from-block-based-otas
+cp ./oneplus3.xml ~/android/lineage/.repo/local_manifests/oneplus3.xml
 
-# or adb your phone and
+cd ~/android/lineage
+repo sync -j 8 
 #
-
-# 需要先 adb shell 到手机 然后执行 su 通过
-
-# 不然会确实 dashd 文件
-cd  ~/android/lineage/device/oneplus/oneplus3
-./extract-files.sh
-#######
-#cp ./roomservice.xml ~/android/lineage/.repo/local_manifests/roomservice.xml
-
-#cd ~/android/lineage
-#repo sync -j 8 
-#
+#以上方法均缺失
