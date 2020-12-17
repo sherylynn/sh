2acfbd67de3e31a5b0ace5d6b533663689700a35
sherylynn <352281674@qq.com>
2020-12-17 18:36:14 +0800

    add source maruos support for oneplus3

---
+++ maruos/oneplus3/lunch-sherylynn.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+cd ~/android/maruos
+source build/envsetup.sh
+#按照官方教程，会莫名其妙报错
+#C_VERSION TARGET_PLATFORM_VERSION TARGET_PRODUCT print report_config : invalid parameter name
+# 需要去掉本地语义化设置
+setopt shwordsplit
+export LC_ALL=C
+
+export LLVM_ENABLE_THREADS=1
+#lunch
+#lunch maru_oneplus3-userdebug
+#mka
+#需要用brunch而不是lunch和mka
+# 对 ~/android/maruos/system/sepolicy/public/domian.te
+# 的1385进行了注释
+croot
+brunch maru_oneplus3-userdebug
---
+++ maruos/oneplus3/pre_breakfast-sherylynn-v0.8.sh
@@ -0,0 +1,10 @@
+rm ~/android/maruos
+ln -s ~/android/maruos-v0.8 ~/android/maruos
+mkdir -p ~/android/maruos/.repo/local_manifests/
+cp ./oneplus3-sherylynn-v0.8.xml ~/android/maruos/.repo/local_manifests/oneplus3.xml
+#cp ./default.xml ~/android/maruos/.repo/manifests/
+
+cd ~/android/maruos
+repo sync -j 8 --force-sync
+#
+#以上方法均缺失
