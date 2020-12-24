de5097b79c632761c8d35fc6caabf7723c59c96d
sherylynn <352281674@qq.com>
2020-12-24 09:58:47 +0800

    add tmux

--- debian/iptables.sh
+++ debian/iptables.sh
@@ -5,7 +5,7 @@
 sudo apt install iptables -y
 pro='tcp'
 HOSTNAME=$(hostname -I)
-NAT_Host=$(node_split "$HOSTNAME" ' ' 0)
+#NAT_Host=$(node_split "$HOSTNAME" ' ' 0)
 echo $NAT_Host
 #NAT_Host='10.0.0.12'
 #NAT_Host='119.28.13.48'
@@ -19,4 +19,4 @@ iptables -t nat -A PREROUTING -i eth0 -p $pro --dport $NAT_Port -j DNAT --to-des
 #iptables -t nat -A PREROUTING -d $NAT_Host -p $pro --dport $NAT_Port -j DNAT --to-destination $Dst_Host:$Dst_Port
 #iptables -t nat -A POSTROUTING -m $pro -p $pro --dport $Dst_Port -d $Dst_Host -j SNAT --to-source $NAT_Host
 #iptables -t nat -A POSTROUTING -d 104.243.29.121 -p tcp --dport 24630 -j SNAT --to 0.0.0.0
-iptables-save
+#iptables-save
---
+++ win-git/tmux.sh
@@ -0,0 +1,2 @@
+sudo apt install tmux -y
+echo "source-file ~/sh/win-git/tmux.conf" > ~/.tmux.conf
