32f8292a6a82c52db0cb21447e64622c764b930d
sherylynn <352281674@qq.com>
2020-12-29 14:10:34 +0800

    change maruos 0.8 to myself

--- maruos/repo_sync-v0.8.sh
+++ maruos/repo_sync-v0.8.sh
@@ -15,6 +15,6 @@ cd ~/android/maruos-v0.8
 #repo init -u https://github.com/maruos/manifest.git -b maru-0.7 maruos
 #repo sync -j 8 --force-sync
 #repo init -u https://github.com/pintaf/manifest -b maru-0.7-pintaf --no-clone-bundle --depth=1
-repo init -u git://github.com/deepak-divakar/manifest.git -b maru-lineage-17.1 --no-clone-bundle --depth=1
+repo init -u git://github.com/sherylynn/manifest.git -b maru-0.8 --no-clone-bundle --depth=1
 repo sync --jobs=8 --fetch-submodules --current-branch --no-clone-bundle --force-sync
 # after finished should 
