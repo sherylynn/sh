43fae2258caf07683b2955e18f0a068230d20b36
sherylynn <352281674@qq.com>
2021-01-09 23:17:57 +0800

    update oneplus3 build script

--- maruos/oneplus3/lunch-sherylynn.sh
+++ maruos/oneplus3/lunch-sherylynn.sh
@@ -4,9 +4,11 @@ source build/envsetup.sh
 #按照官方教程，会莫名其妙报错
 #C_VERSION TARGET_PLATFORM_VERSION TARGET_PRODUCT print report_config : invalid parameter name
 # 需要去掉本地语义化设置
+croot
 setopt shwordsplit
 export LC_ALL=C
 export LD_BIND_NOW=1
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 export LLVM_ENABLE_THREADS=1
 #lunch
 #lunch maru_oneplus3-userdebug
@@ -14,5 +16,4 @@ export LLVM_ENABLE_THREADS=1
 #需要用brunch而不是lunch和mka
 # 对 ~/android/maruos/system/sepolicy/public/domian.te
 # 的1385进行了注释
-croot
 brunch maru_oneplus3-userdebug
