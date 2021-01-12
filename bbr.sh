9ff9e37d086bcce6c8b86bc01d186258f2768584
sherylynn <352281674@qq.com>
2021-01-12 17:13:48 +0800

    update bbr script

--- bbr.sh
+++ bbr.sh
@@ -1,3 +1,9 @@
-wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/YankeeBBR/master/bbr.sh && bash bbr.sh install
-重启后
-bash bbr.sh start
+#wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/YankeeBBR/master/bbr.sh && bash bbr.sh install
+#重启后
+#bash bbr.sh start
+
+
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+lsmod | grep bbr
