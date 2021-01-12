02de5fc637258c1d571b48cfebe9c8e1f124dcb0
sherylynn <352281674@qq.com>
2021-01-12 23:29:32 +0800

    update mirror

--- mac/mirror_brew_ustc.sh
+++ mac/mirror_brew_ustc.sh
@@ -4,8 +4,17 @@ git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
 # 替换homebrew-core.git:
 cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
 git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
-# 应用生效
-brew update
+
+# 替换homebrew-cask.git:
+cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+# 替换homebrew-cask-versions.git:
+cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask-versions
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask-versions.git
 # 替换homebrew-bottles:
 echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.bash_profile
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.zshrc
 source ~/.bash_profile
+source ~/.zshrc
+# 应用生效
+brew update
