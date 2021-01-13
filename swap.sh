04424ede056faaefbdaf701c74fc45aedeac2b04
sherylynn <352281674@qq.com>
2021-01-13 21:49:40 +0800

    add args

--- swap.sh
+++ swap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+if [ $1 ]; then
+  G=$1
+else
+  G=1
+fi
 if [ -f "$HOME/swap" ]; then
   echo "swap on"
 else
-  dd if=/dev/zero of=$HOME/swap bs=1024 count=1000000
-  mkswap -f $HOME/swap
-  sudo swapon $HOME/swap
-  echo '/home/'${USER}'/swap swap swap defaults 0 0' |sudo tee -a /etc/fstab
+  dd if=/dev/zero of=$HOME/swap bs=1024 count=${G}000000
+  sudo mkswap -f $HOME/swap
 fi
+sudo swapon $HOME/swap
+echo '/home/'${USER}'/swap swap swap defaults 0 0' |sudo tee -a /etc/fstab
