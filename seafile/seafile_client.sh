58e492b769e2a15ef27fe25c313787a49437c2e2
sherylynn <352281674@qq.com>
2021-01-13 16:07:50 +0800

    update

--- seafile/seafile_client.sh
+++ seafile/seafile_client.sh
@@ -1,16 +1,22 @@
 # for debian
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61
 
-echo deb http://deb.seadrive.org stretch main | sudo tee /etc/apt/sources.list.d/seafile.list
+#echo deb http://deb.seadrive.org stretch main | sudo tee /etc/apt/sources.list.d/seafile.list
 
-sudo apt update
+#sudo apt update
 
-sudo apt install seafile-gui
+#sudo apt install seafile-gui
 # for ubuntu
-sudo add-apt-repository ppa:seafile/seafile-client
+#sudo add-apt-repository ppa:seafile/seafile-client
 
-sudo apt-get update
+#sudo apt-get update
 
-sudo apt-get install seafile-gui
+#sudo apt-get install seafile-gui
 ### miss for 19.10 just for 19.04 disco
 ###  echo deb http://ppa.launchpad.net/seafile/seafile-client/ubuntu disco main |sudo tee /etc/apt/sources.list.d/seafile-ubuntu-seafile-client-eoan.list
+
+
+sudo wget https://linux-clients.seafile.com/seafile.asc -O /usr/share/keyrings/seafile-keyring.asc
+sudo bash -c "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/buster/ stable main' > /etc/apt/sources.list.d/seafile.list"
+sudo apt update
+sudo apt install -y seafile-gui
--- vhd/vtoyboot.sh
+++ vhd/vtoyboot.sh
@@ -1,4 +1,6 @@
 cd ~
 git clone https://github.com/ventoy/vtoyboot
 cd vtoyboot/vtoyboot
-bash vtoyboot.sh
+echo "your password"
+sudo apt install grub-pc-bin
+sudo bash ~/vtoyboot/vtoyboot/vtoyboot.sh
