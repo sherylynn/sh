05163fff23988c14d76f0cc3ab2645e16996ce80
sherylynn <352281674@qq.com>
2021-01-15 21:26:02 +0800

    add tmp args for build lineage

--- maruos/raphael/lunch-sherylynn.sh
+++ maruos/raphael/lunch-sherylynn.sh
@@ -4,6 +4,9 @@ source build/envsetup.sh
 #按照官方教程，会莫名其妙报错
 #C_VERSION TARGET_PLATFORM_VERSION TARGET_PRODUCT print report_config : invalid parameter name
 # 需要去掉本地语义化设置
+if [ $1 ]; then
+  export export JAVACMD_OPTIONS="-Djava.io.tmpdir=$1"
+fi
 croot
 setopt shwordsplit
 export LC_ALL=C
