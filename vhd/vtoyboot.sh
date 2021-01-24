3928c774702281dcc6fcf68bac9d7ac5bdb9de5b
sherylynn <352281674@qq.com>
2021-01-24 14:52:11 +0800

    add git update for vtoyboot

--- vhd/vtoyboot.sh
+++ vhd/vtoyboot.sh
@@ -1,6 +1,8 @@
 cd ~
 git clone https://github.com/ventoy/vtoyboot
 cd vtoyboot/vtoyboot
+git checkout .
+git pull
 echo "your password"
 sudo apt install grub-pc-bin
 sudo bash ~/vtoyboot/vtoyboot/vtoyboot.sh
