6412a24b435b3d2ab2634bf6daf160debe2cda5e
sherylynn <352281674@qq.com>
2021-01-24 10:22:48 +0800

    changer normal.sh for debian

---
+++ debian/desktop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install remmina calibre virtualbox gimp openshot telegram-desktop chromium gparted -y
+sudo apt install steam -y
+#输入法ibus版本  用户数据在 ~/.config/ibus/rime
+sudo apt install ibus-rime -y
+#fcitx版本 用户数据在 ~/.config/fcitx/rime
+sudo apt-get install fcitx-rime -y
+#lmms 乐曲制作
+sudo apt install lmms -y
--- debian/normal.sh
+++ debian/normal.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-sudo apt install remmina calibre virtualbox gimp openshot telegram-desktop chromium gparted -y
-sudo apt install steam -y
-#输入法ibus版本  用户数据在 ~/.config/ibus/rime
-sudo apt install ibus-rime -y
-#fcitx版本 用户数据在 ~/.config/fcitx/rime
-sudo apt-get install fcitx-rime -y
-#lmms 乐曲制作
-sudo apt install lmms -y
+sudo apt install htop nload git ssh vim ncdu remmina tmux zsh curl wget gparted -y
