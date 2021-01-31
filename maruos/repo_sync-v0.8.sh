cac0a69a20856e10332020e1012388cc1e536bf8
sherylynn <352281674@qq.com>
2021-02-01 01:28:59 +0800

    add sync force

--- maruos/repo_sync-v0.8.sh
+++ maruos/repo_sync-v0.8.sh
@@ -10,7 +10,7 @@ cd ~/android/maruos-v0.8
 #repo init -u git://github.com/sherylynn/manifest.git -b maru-0.8 --no-clone-bundle --depth=1
 repo init -u git://github.com/sherylynn/manifest.git -b maru-0.8 
 #repo sync --jobs=8 --fetch-submodules --current-branch --no-clone-bundle --force-sync
-repo sync --jobs=8
+repo sync --jobs=8 --force-sync
 # after finished should 
 rm ~/android/maruos
 ln -s ~/android/maruos-v0.8 ~/android/maruos
