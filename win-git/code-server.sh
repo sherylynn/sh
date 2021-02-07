63092095c8ec7b5ca047ac795f9e6db7f0ea8bc0
sherylynn <352281674@qq.com>
2021-02-07 17:03:10 +0800

    reuse vscode extension for code-serer add systemd startup for code-server

--- win-git/code-server.sh
+++ win-git/code-server.sh
@@ -1,1 +1,11 @@
 curl -fsSL https://code-server.dev/install.sh |sh
+#systemd autostart
+#sudo systemctl enable code-server@$USER
+#sudo systemctl start code-server@$USER
+
+#configure
+#~/.config/code-server/config.yaml
+
+#reuse extension
+rm -rf ~/.local/share/code-server
+ln -s ~/.vscode ~/.local/share/code-server
