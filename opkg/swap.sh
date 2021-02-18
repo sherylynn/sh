94b091d33a0a9f38ad70408045ea76c401778c36
sherylynn <352281674@qq.com>
2021-02-18 06:03:12 +0000

    fix args in busybox

--- opkg/swap.sh
+++ opkg/swap.sh
@@ -8,7 +8,7 @@ if [ -f "$HOME/swap" ]; then
   echo "swap on"
 else
   dd if=/dev/zero of=$HOME/swap bs=1024 count=${G}000
-  mkswap -f $HOME/swap
+  mkswap $HOME/swap
 fi
 swapon $HOME/swap
 echo '/opt/home/admin/swap swap swap defaults 0 0' |tee -a /etc/mtab
