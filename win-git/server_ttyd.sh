b1d6e5a2c4ff8db07215b49a956b77607f9e052e
sherylynn <352281674@qq.com>
2021-03-01 16:55:42 +0800

    add ttyd

---
+++ win-git/server_ttyd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+realpath(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)/$x
+
+}
+realpathdir(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)
+
+}
+cd $(realpathdir ./server_code-server.sh)
+pwd
+#load env
+test -f ../../tools/rc/ttydrc && . ../../tools/rc/ttydrc
+
+echo $(node -v)
+echo $(go version)
+echo $PATH
+ttyd login -t fontSize=20
---
+++ win-git/ttyd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. $(dirname "$0")/toolsinit.sh
+AUTHOR=tsl0922
+NAME=ttyd
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+SOFT_HOME=$(install_path)/${NAME}
+SOFT_VERSION=$(get_github_release_version $AUTHOR/$NAME)
+echo "soft version is $SOFT_VERSION"
+
+case $(arch) in 
+  amd64) SOFT_ARCH=x86_64;;
+  386) SOFT_ARCH=386;;
+  armhf) SOFT_ARCH=armhf;;
+  aarch64) SOFT_ARCH=aarch64;;
+esac
+# uname Linux .bashrc uname Darwin MINGW64 .bash_profile
+PLATFORM=$(platform)
+SOFT_FILE_NAME=${NAME}
+SOFT_FILE_PACK=${NAME}.${SOFT_ARCH}
+
+
+SOFT_URL=https://github.com/${AUTHOR}/${NAME}/releases/download/${SOFT_VERSION}/${SOFT_FILE_PACK}
+if [[ $(platform) == *linux* ]]; then
+  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+  
+  rm -rf ${SOFT_HOME} && mkdir -p ${SOFT_HOME}
+  cp $(cache_folder)/${SOFT_FILE_PACK} ${SOFT_HOME}/${SOFT_FILE_NAME}
+  chmod 777 ${SOFT_HOME}/${SOFT_FILE_NAME}
+  echo "export PATH=$SOFT_HOME:"'$PATH' > ${TOOLSRC}
+
+  ./systemd_ttyd.sh
+fi
