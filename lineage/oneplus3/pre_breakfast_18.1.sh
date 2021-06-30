5239ad69bca7cfeec7d8c50d61b7e620aab7cd66
sherylynn <352281674@qq.com>
2021-06-30 22:31:00 +0800

    update repo

--- lineage/oneplus3/pre_breakfast_18.1.sh
+++ lineage/oneplus3/pre_breakfast_18.1.sh
@@ -2,6 +2,6 @@ cp ./oneplus3_18.1.xml ~/android/lineage/.repo/local_manifests/oneplus3.xml
 cp ./../oppo_common/oppo_common_18.1.xml ~/android/lineage/.repo/local_manifests/oppo_common.xml
 
 cd ~/android/lineage
-repo sync -j 8 
+repo sync -j 8  --force-sync
 #
 #以上方法均缺失
