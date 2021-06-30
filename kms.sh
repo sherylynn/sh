f1efbcbc2dcbd6055759fbacb8dc9cfa27cd6d70
sherylynn <352281674@qq.com>
2021-06-30 23:39:27 +0800

    new repo for py-kms

--- kms.sh
+++ kms.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/ThunderEX/py-kms.git ~/py-kms
+#git clone https://github.com/ThunderEX/py-kms.git ~/py-kms
+git clone https://github.com/SystemRage/py-kms ~/py-kms
 cd ~/py-kms
-python server.py 0.0.0.0 1688
+#python server.py 0.0.0.0 1688
+python3 py-kms/pykms_Server.py 0.0.0.0 1688
