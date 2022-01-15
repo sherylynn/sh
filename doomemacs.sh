ebcfcbe2114f1998b45f96b50320d9cdc68ff59c
sherylynn <352281674@qq.com>
2022-01-15 23:57:05 +0800

    because of emacs-mac can't read softlink so use chemacs2 for load different emacs distro

--- doomemacs.sh
+++ doomemacs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d_doom
 rm ~/.emacs.d
-ln -s ~/.emacs.d_doom ~/.emacs.d
+git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d_doom
+#ln -s ~/.emacs.d_doom ~/.emacs.d
 git clone https://github.com/sherylynn/doom-private ~/.doom.d
-~/.emacs.d/bin/doom install
+~/.emacs.d_doom/bin/doom install
+
+tee ~/.emacs-profiles.el <<-'EOF'
+(("default"   . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("legacy" . ((user-emacs-directory . "~/.emacs.legacy")))
+ ("spacemacs"   . ((user-emacs-directory . "~/.emacs.d_space"))))
+EOF
--- mac/app.sh
+++ mac/app.sh
@@ -1,7 +1,7 @@
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 #normal app
-brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer visual-studio-code telegram-desktop steam gimp squirrel visual-studio-code libreoffice virtualbox openshot-video-editor emacs
+brew cask install onyx skim iina firefox chromium calibre v2rayx qq wechat anydesk teamviewer telegram-desktop steam gimp squirrel visual-studio-code libreoffice virtualbox openshot-video-editor emacs
 #macx86 app
 brew cask install clover-configurator kext-utility hwsensors
 #cli app
@@ -30,3 +30,7 @@ brew install wireguard-tools
 <<'config_path'
 /usr/local/etc/wireguard/xxx.conf
 config_path
+#emacs 
+brew tap railwaycat/emacsmacport
+brew install emacs-mac --with-modules
+ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
