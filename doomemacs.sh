2279259a68ae7e24910911c7da1366bc58093bcd
sherylynn <352281674@qq.com>
2022-01-17 23:40:34 +0800

    add doom to path

--- doomemacs.sh
+++ doomemacs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+. $(dirname "$0")/win-git/toolsinit.sh
+NAME=doom
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
 #rm ~/.emacs.d
 rm -rf ~/.emacs.d
-git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/plexus/chemacs2.git ~/.emacs.d
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d_doom
 #ln -s ~/.emacs.d_doom ~/.emacs.d
 git clone https://github.com/sherylynn/doom-private ~/.doom.d
@@ -12,3 +16,5 @@ tee ~/.emacs-profiles.el <<-'EOF'
  ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
  ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
 EOF
+SOFT_BIN=~/.emacs.d_doom/bin
+echo 'export PATH=$PATH:'${SOFT_BIN}>${TOOLSRC}
