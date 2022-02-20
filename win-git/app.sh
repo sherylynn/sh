ad9674b279eb9a0a7c1eeb550421e2cbbae01481
sherylynn <352281674@qq.com>
2022-02-20 15:46:25 +0800

    [2022-02-20 日 15:46]

--- win-git/app.sh
+++ win-git/app.sh
@@ -1,6 +1,9 @@
 brew install gcc 
 brew install htop ncdu neovim
 # for doom emacs
-brew install emacs ripgrep git fd coreutils cmake libtool libvterm
+brew untap sherylynn/homebrew-emacsx11
+brew tap sherylynn/homebrew-emacsx11
+brew install emacsx11
+brew install ripgrep git fd coreutils cmake libtool libvterm
 # for doom emacs orgmode support mouse or M-x xterm-mouse-mode 
 brew install tmux ttyd
