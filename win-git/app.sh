e2f59f93fa91fd5b8a219607df4300717c42ed99
sherylynn <352281674@qq.com>
2022-02-20 17:52:45 +0800

    [2022-02-20 日 17:52]

--- win-git/app.sh
+++ win-git/app.sh
@@ -5,5 +5,7 @@ brew untap sherylynn/homebrew-emacsx11
 brew tap sherylynn/homebrew-emacsx11
 brew install emacsx11
 brew install ripgrep git fd coreutils cmake libtool libvterm
+# for vterm support
+brew install gcc@5
 # for doom emacs orgmode support mouse or M-x xterm-mouse-mode 
 brew install tmux ttyd
