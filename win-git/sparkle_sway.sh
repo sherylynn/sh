e03a6cec22b10b2ef5a33c6ac4015a68b81b7a39
sherylynn <352281674@qq.com>
2022-07-17 10:19:12 +0800

    update

--- win-git/sparkle_sway.sh
+++ win-git/sparkle_sway.sh
@@ -5,14 +5,12 @@ sudo chmod 777 /sparkle/wayland-0
 XDG_RUNTIME_DIR=/sparkle Xwayland &
 sleep 1
 export DISPLAY=:0
-#export XDG_RUNTIME_DIR="/tmp/swaywm"
-export XDG_RUNTIME_DIR="/tmp/weston"
+export XDG_RUNTIME_DIR="/tmp/swaywm"
+#export XDG_RUNTIME_DIR="/tmp/weston"
 mkdir -p $XDG_RUNTIME_DIR
-chmod 0700 $XDG_RUNTIME_DIR
-weston -B rdp-backend.so --rdp4-key ~/rdpkey/rdp-security.key
-##weston -B drm-backend.so
+sudo chmod 0700 $XDG_RUNTIME_DIR
+#weston -B rdp-backend.so --rdp4-key ~/rdpkey/rdp-security.key
 ##weston -B wayland-backend.so --display=:0
-#sway
-#weston -B x11-backend.so --height 2295 --width 1080
+sway
 #xfce4-session
 #XDG_RUNTIME_DIR=/sparkle sway
