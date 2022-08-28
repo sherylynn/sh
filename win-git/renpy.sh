915f3a3f80bd63b99c20cec6fa576a464078c243
sherylynn <352281674@qq.com>
2022-08-28 20:43:36 +0800

    add renpy to windows

--- win-git/renpy.sh
+++ win-git/renpy.sh
@@ -5,7 +5,7 @@ NAME=renpy
 TOOLSRC_NAME=${NAME}rc
 TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
 SOFT_HOME=$(install_path)/${NAME}
-SOFT_VERSION=7.5.1
+SOFT_VERSION=7.4.5
 SOFT_ARCH=x86_64
 OS=windows
 cd ~
@@ -13,31 +13,25 @@ cd ~
 #安装 emacs
 #--------------------------------------
 if [[ $(platform) == *win* ]]; then
-  PLATFORM=windows
-  case $(arch) in 
-    amd64) SOFT_ARCH=x86_64;;
-    386) SOFT_ARCH=i686;;
-  esac
-
-  SOFT_FILE_NAME=${NAME}-${SOFT_VERSION}-${SOFT_ARCH}
-  SOFT_FILE_PACK=$(soft_file_pack $SOFT_FILE_NAME)
-  # init pwd
-  cd $HOME
-
-  SOFT_URL=http://mirrors.nju.edu.cn/gnu/${NAME}/${PLATFORM}/${NAME}-$(echo ${SOFT_VERSION}|cut -d '.' -f 1)/${SOFT_FILE_PACK} 
-  #if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
-  if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
-    $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
-    $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
-    
-    rm -rf ${SOFT_HOME} && \
-      mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME} 
-  fi
   #--------------new .toolsrc-----------------------
-  SOFT_ROOT=${SOFT_HOME}/bin
+  mkdir -p $SOFT_HOME
+  case $(arch) in 
+    amd64) SOFT_ARCH="";;
+    386) SOFT_ARCH="";;
+    armhf) SOFT_ARCH=arm;;
+    aarch64) SOFT_ARCH=arm;;
+  esac
+  SOFT_FILE_NAME=${NAME}-${SOFT_VERSION}-sdk${SOFT_ARCH}
+  #SOFT_FILE_PACK=$(soft_file_pack $SOFT_FILE_NAME)
+  SOFT_FILE_PACK=$SOFT_FILE_NAME.zip
+  SOFT_URL=https://www.renpy.org/dl/${SOFT_VERSION}/${SOFT_FILE_PACK} 
+  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+  $(cache_unpacker ${SOFT_FILE_PACK} ${SOFT_FILE_NAME})
+  rm -rf $SOFT_HOME && \
+    mv $(cache_folder)/${SOFT_FILE_NAME} $SOFT_HOME 
+  #--------------new .toolsrc-----------------------
+  SOFT_ROOT=${SOFT_HOME}/${SOFT_FILE_NAME}
   export PATH=$PATH:${SOFT_ROOT}
-  echo "set HOME=$(cygpath $HOME -d)">${SOFT_ROOT}/emacs_win.cmd
-  echo "emacs" >> ${SOFT_ROOT}emacs_win.cmd
 
   echo 'export PATH=$PATH:'${SOFT_ROOT}>${TOOLSRC}
 fi
