a969f6c1e14c6cf4c8e4d907f6abe70df021b5ef
sherylynn <352281674@qq.com>
2022-09-20 15:56:56 +0800

    update

--- win-git/mesa.sh
+++ win-git/mesa.sh
@@ -34,6 +34,7 @@ git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
 #git clone --depth 1 --branch 21.3 https://gitlab.freedesktop.org/mesa/mesa.git
 sudo apt-get install meson -y
 sudo apt build-dep mesa -y
+sudo apt build-dep libdrm -y
 cd mesa
 meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled  -D egl=enabled  -D gles2=enabled -D glvnd=true -D glx=dri  -D libunwind=disabled -D osmesa=true  -D shared-glapi=enabled -D microsoft-clc=disabled  -D valgrind=disabled --prefix /usr -D gles1=disabled -D freedreno-kgsl=true
 
--- win-git/server_vnc.sh
+++ win-git/server_vnc.sh
@@ -13,6 +13,10 @@ echo $(node -v)
 
 #x11vnc -forever -display :0 -usepw
 VNC_DISPLAY=0
+export MESA_LOADER_DRIVER_OVERRIDE=zink
+export GALLIUM_DRIVER=zink
 rm "/tmp/.X${VNC_DISPLAY}-lock" "/tmp/.X11-unix/X${VNC_DISPLAY}"
 #vncserver :${VNC_DISPLAY} -depth 16 -dpi 100 -geometry 1080x2295
-vncserver :${VNC_DISPLAY} -depth 16 -dpi 100 -geometry 1600x900
+#vncserver :${VNC_DISPLAY} -depth 16 -dpi 100 -geometry 1600x900
+tightvncserver :${VNC_DISPLAY} -depth 16 -dpi 100 -geometry 1366x768
+#tigervncserver :${VNC_DISPLAY} -depth 16 -dpi 100 -geometry 1600x900
