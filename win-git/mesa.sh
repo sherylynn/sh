8c89ba1d29cb810f068646687b8b59870cce7dda
sherylynn <352281674@qq.com>
2022-09-20 14:23:16 +0800

    update mesa

--- win-git/mesa.sh
+++ win-git/mesa.sh
@@ -1,8 +1,37 @@
 cd ~
+mkdir -p dir
+cd dir
+curl -LO https://dri.freedesktop.org/libdrm/libdrm-2.4.109.tar.xz
+
+git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland.git
+git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
+#libdrm 
+tar -xf libdrm-2.4.109.tar.xz
+cd libdrm-2.4.109
+mkdir build
+cd build
+meson -Dintel=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dvmwgfx=false -Dvc4=false ..
+sudo ninja install
+
+#libwayland 
+cd ~/dir/wayland
+mkdir build
+cd build
+meson -Ddocumentation=false ..
+sudo ninja install
+
+#wayland-protocols
+cd ~/dir/wayland-protocols
+mkdir build
+cd build
+meson  ..
+sudo ninja install 
+
+cd ~
 #git clone --depth 1 --branch 22.2 https://gitlab.freedesktop.org/mesa/mesa.git
-#git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
-rm -rf mesa
-git clone --depth 1 --branch 21.3 https://gitlab.freedesktop.org/mesa/mesa.git
+git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
+#rm -rf mesa
+#git clone --depth 1 --branch 21.3 https://gitlab.freedesktop.org/mesa/mesa.git
 sudo apt-get install meson -y
 sudo apt build-dep mesa -y
 cd mesa
