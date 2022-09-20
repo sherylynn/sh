97904dd5d7ec22148a90f4087cf30e3cf14f5a32
sherylynn <352281674@qq.com>
2022-09-20 19:02:21 +0800

    update

--- win-git/mesa.sh
+++ win-git/mesa.sh
@@ -6,18 +6,22 @@ curl -LO https://dri.freedesktop.org/libdrm/libdrm-2.4.109.tar.xz
 git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland.git
 git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
 #libdrm 
+sudo apt build-dep libdrm -y
 tar -xf libdrm-2.4.109.tar.xz
 cd libdrm-2.4.109
 mkdir build
 cd build
 meson -Dintel=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dvmwgfx=false -Dvc4=false ..
+#不安装
 sudo ninja install
 
 #libwayland 
 cd ~/dir/wayland
+sudo apt build-dep wayland -y
 mkdir build
 cd build
 meson -Ddocumentation=false ..
+#不安装
 sudo ninja install
 
 #wayland-protocols
@@ -25,16 +29,17 @@ cd ~/dir/wayland-protocols
 mkdir build
 cd build
 meson  ..
+#不安装
 sudo ninja install 
 
 cd ~
 #git clone --depth 1 --branch 22.2 https://gitlab.freedesktop.org/mesa/mesa.git
-git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
 #rm -rf mesa
+sudo cp /usr/include/libdrm/* /usr/include/ -r
+git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
 #git clone --depth 1 --branch 21.3 https://gitlab.freedesktop.org/mesa/mesa.git
 sudo apt-get install meson -y
 sudo apt build-dep mesa -y
-sudo apt build-dep libdrm -y
 cd mesa
 meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled  -D egl=enabled  -D gles2=enabled -D glvnd=true -D glx=dri  -D libunwind=disabled -D osmesa=true  -D shared-glapi=enabled -D microsoft-clc=disabled  -D valgrind=disabled --prefix /usr -D gles1=disabled -D freedreno-kgsl=true
 
---
+++ win-git/mesa_weston.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e 
+export XDG_RUNTIME_DIR="/tmp/weston"
+mkdir -p $XDG_RUNTIME_DIR
+chmod 0700 $XDG_RUNTIME_DIR
+XDG_RUNTIME_DIR="/tmp/weston" Xwayland &
+sleep 1
+# if linux deploy has vnc so let it be :1
+# if not set display number :0
+export DISPLAY=:0
+#export XDG_RUNTIME_DIR="/tmp/swaywm"
+VNC_DISPLAY=0
+rm -rf "/tmp/.X${VNC_DISPLAY}-lock" "/tmp/.X11-unix/X${VNC_DISPLAY}"
+export MESA_LOADER_DRIVER_OVERRIDE=zink
+export GALLIUM_DRIVER=zink
+weston --xwayland -B rdp-backend.so --rdp4-key ~/rdpkey/rdp-security.key
+##weston -B drm-backend.so
+##weston -B wayland-backend.so --display=:0
+#sway
+#weston -B x11-backend.so --height 2295 --width 1080
+#xfce4-session
+#XDG_RUNTIME_DIR=/sparkle sway
