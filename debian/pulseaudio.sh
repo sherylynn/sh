cd2d637482f925b6cd0f0e027b5c92afec0e5b4d
sherylynn <352281674@qq.com>
2022-09-30 15:42:31 +0800

    add auto pulseaudio

--- debian/pulseaudio.sh
+++ debian/pulseaudio.sh
@@ -2,3 +2,4 @@ sudo apt install pulseaudio -y
 sudo rm -rf /etc/pulse/client.conf.d/00-disable-autospawn.conf
 sudo cp ~/sh/debian/conf_pulseaudio/00-enable-autospawn.conf /etc/pulse/client.conf.d/
 sudo cp ~/sh/debian/conf_pulseaudio/default.pa /etc/pulse/
+sudo ~/sh/win-git/init_d_pulseaudio.sh
---
+++ win-git/server_pulseaudio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_NAME="pulseaudio"
+realpath(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)/$x
+
+}
+realpathdir(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)
+
+}
+cd $(realpathdir ./server_${SCRIPT_NAME}.sh)
+pwd
+#load env
+test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
+
+echo $(whoami)
+# login need systemd user root
+#ttyd -p 3000 -t fontSize=18 login
+# login need systemd user $(whoami)
+${SCRIPT_NAME} --start
+#su $(whoami) -c 'ttyd -p 3000 -t fontSize=18 ssh localhost'
