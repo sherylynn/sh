ea166d052fdd4dade49e0db0103aea10f971e0ab
sherylynn <352281674@qq.com>
2022-10-19 16:17:22 +0800

    add study_xxqg

---
+++ win-git/server_study_xxqg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SCRIPT_NAME="study_xxqg"
+realpath(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)/$x
+
+}
+realpathdir(){
+  local x=$1
+  echo $(cd $(dirname $0);pwd)
+
+}
+cd $(realpathdir ./server_${SCRIPT_NAME}.sh)
+pwd
+#load env
+test -f ../../tools/rc/noderc && . ../../tools/rc/noderc
+test -f ../../tools/rc/cclsrc && . ../../tools/rc/cclsrc
+test -f ../../tools/rc/golangrc && . ../../tools/rc/golangrc
+test -f ../../tools/rc/pythonrc && . ../../tools/rc/pythonrc
+
+test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
+
+echo $(node -v)
+echo $(go version)
+echo $PATH
+cd ../../${SCRIPT_NAME}
+./${SCRIPT_NAME} 
