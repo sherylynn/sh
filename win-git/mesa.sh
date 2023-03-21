80e8ecaf2bf38911d3d97b126b351a54d4535d48
sherylynn <352281674@qq.com>
2023-03-22 02:25:00 +0800

    add comment

--- win-git/mesa.sh
+++ win-git/mesa.sh
@@ -8,6 +8,8 @@ echo 'export GALLIUM_DRIVER=zink'>>${TOOLSRC}
 echo 'export XDG_RUNTIME_DIR=/tmp'>>${TOOLSRC}
 
 #需要安装mesa后，调用termux下的命令就可以启动了
+#termux 下 运行mesa.sh 再切换su 运行 mesa.sh
+#并把/data/data/com.termux/files/usr/tmp/ 挂载 /tmp/
 
 echo 'export GALLIUM_DRIVER=virpipe' >${TOOLSRC}
 echo 'export MESA_GL_VERSION_OVERRIDE=4.0'>> ${TOOLSRC}
