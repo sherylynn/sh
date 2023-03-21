60f4263b48d9be0f6a5befa5b6a98fe234ebe8a7
sherylynn <352281674@qq.com>
2023-03-21 17:51:36 +0800

    change freedreno render to virglrender

--- win-git/mesa.sh
+++ win-git/mesa.sh
@@ -2,73 +2,13 @@
 . $(dirname "$0")/toolsinit.sh
 TOOLSRC_NAME=mesarc
 TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
-cd ~
-mkdir -p dir
-cd dir
-curl -LO https://dri.freedesktop.org/libdrm/libdrm-2.4.109.tar.xz
-
-sudo apt install tigervnc-standalone-server -y
-
-git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland.git
-git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
-git clone --depth 1 https://github.com/glmark2/glmark2.git
-#libdrm 
-sudo apt build-dep libdrm -y
-tar -xf libdrm-2.4.109.tar.xz
-cd libdrm-2.4.109
-mkdir build
-cd build
-meson -Dintel=false -Dradeon=false -Damdgpu=false -Dnouveau=false -Dvmwgfx=false -Dvc4=false ..
-#不安装
-#sudo ninja install
-
-#libwayland 
-cd ~/dir/wayland
-sudo apt build-dep wayland -y
-mkdir build
-cd build
-meson -Ddocumentation=false ..
-#不安装
-#sudo ninja install
-
-#wayland-protocols
-cd ~/dir/wayland-protocols
-mkdir build
-cd build
-meson  ..
-#不安装
-#sudo ninja install 
-
-cd ~
-#git clone --depth 1 --branch 22.2 https://gitlab.freedesktop.org/mesa/mesa.git
-#success
-git clone --depth 1 --branch 22.0 https://gitlab.freedesktop.org/mesa/mesa.git
-#git clone --depth 1 https://github.com/Heasterian/mesa.git
-#rm -rf mesa
-#git clone --single-branch --shallow-since 2022-02-01 https://gitlab.freedesktop.org/mesa/mesa.git
-sudo cp /usr/include/libdrm/* /usr/include/ -r
-#git clone --depth 1 --branch 22.1 https://gitlab.freedesktop.org/mesa/mesa.git
-#git clone --depth 1 --branch 21.3 https://gitlab.freedesktop.org/mesa/mesa.git
-sudo apt-get install meson -y
-sudo apt build-dep mesa -y
-sudo apt remove llvm-15 llvm-15-dev -y
-sudo apt autoremove -y
-sudo apt install llvm llvm-dev llvm-runtime -y
-cd mesa
-meson build -D platforms=x11,wayland -D gallium-drivers=swrast,virgl,zink -D vulkan-drivers=freedreno -D dri3=enabled  -D egl=enabled  -D gles2=enabled -D glvnd=true -D glx=dri  -D libunwind=disabled -D osmesa=true  -D shared-glapi=enabled -D microsoft-clc=disabled  -D valgrind=disabled --prefix /usr -D gles1=disabled -D freedreno-kgsl=true
-
-cd build
-sudo ninja install
-
-#glmark2
-sudo apt install -y libjpeg-dev libpng-dev
-cd ~/dir/glmark2
-mkdir build
-cd build
-meson  -Dflavors=x11-gl ..
-sudo ninja install
-
 
 echo 'export MESA_LOADER_DRIVER_OVERRIDE=zink'>${TOOLSRC}
 echo 'export GALLIUM_DRIVER=zink'>>${TOOLSRC}
+echo 'export XDG_RUNTIME_DIR=/tmp'>>${TOOLSRC}
+
+#需要安装mesa后，调用termux下的命令就可以启动了
+
+echo 'export GALLIUM_DRIVER=virpipe' >${TOOLSRC}
+echo 'export MESA_GL_VERSION_OVERRIDE=4.0'>> ${TOOLSRC}
 echo 'export XDG_RUNTIME_DIR=/tmp'>>${TOOLSRC}
