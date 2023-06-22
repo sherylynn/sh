98ba815d9f8eb9ecc932d9297d87bbd774a99310
sherylynn <352281674@qq.com>
2023-06-22 16:39:36 +0800

    add usb watchdog(will restart all usb hub)

--- opkg/usb_tether.sh
+++ opkg/usb_tether.sh
@@ -10,3 +10,40 @@ opkg install usbutils
 #mount /dev/sda1 test_mount
 #cp -r /overlay/* test_mount/
 #set mount point in luci ui
+# Install packages
+opkg update
+opkg install hub-ctrl
+ 
+# Save connectivity checking script
+cat << "EOF" > /root/wan-watchdog.sh
+#!/bin/sh
+ 
+# Fetch WAN gateway
+. /lib/functions/network.sh
+network_flush_cache
+network_find_wan NET_IF
+network_get_gateway NET_GW "${NET_IF}"
+ 
+# Check WAN connectivity
+TRIES="0"
+while [ "${TRIES}" -lt 5 ]
+do
+    if ping -c 1 -w 3 "${NET_GW}" &> /dev/null
+    then exit 0
+    else let TRIES++
+    fi
+done
+ 
+# Restart network
+/etc/init.d/network stop
+hub-ctrl -h 0 -P 1 -p 0
+sleep 1
+hub-ctrl -h 0 -P 1 -p 1
+/etc/init.d/network start
+EOF
+chmod +x /root/wan-watchdog.sh
+ 
+# Add cron job
+cat << "EOF" >> /etc/crontabs/root
+* * * * * /root/wan-watchdog.sh
+EOF
