d03263950e9e0b48dcf8dadb27444ebd0ca5ec1c
sherylynn <352281674@qq.com>
2023-07-06 23:44:47 +0800

    add cron for adb watchdog

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -1,4 +1,18 @@
+opkg install iptables
+opkg install iptables-mod-nat-extra
+
+
+# Save connectivity checking script
+cat << "EOF" > /root/adb-watchdog.sh
+#!/bin/sh
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.route_localnet=1
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 5900 -j DNAT --to-destination 127.0.0.1:5900
+EOF
+chmod +x /root/adb-watchdog.sh
+ 
+# Add cron job
+cat << "EOF" >> /etc/crontabs/root
+* * * * * /root/adb-watchdog.sh
+EOF
