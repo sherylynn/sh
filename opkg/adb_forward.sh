b5c0701b7950a3a4f7a593682bfc65fd290c269e
sherylynn <352281674@qq.com>
2023-07-06 23:51:15 +0800

    add port for adb forward

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -5,6 +5,8 @@ opkg install iptables-mod-nat-extra
 # Save connectivity checking script
 cat << "EOF" > /root/adb-watchdog.sh
 #!/bin/sh
+adb -a forward tcp:10086 tcp:10086
+adb -a forward tcp:5900 tcp:5900
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.route_localnet=1
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
