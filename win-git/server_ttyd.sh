7ec9582ca6715ca0ff0288ce59aeb8790e1decc2
sherylynn <352281674@qq.com>
2023-07-12 09:06:39 +0800

    add comment for ttyd script

--- win-git/server_ttyd.sh
+++ win-git/server_ttyd.sh
@@ -20,4 +20,6 @@ echo $(whoami)
 #ttyd -p 3000 -t fontSize=18 login
 # login need systemd user $(whoami)
 ${SCRIPT_NAME} -6 -p 3000 -t fontSize=18 ssh 127.0.0.1
+#↑这个命令有一个缺点，不能开启本机的免密码(密钥)方式登录，不然会直接ssh进去，而login也有login的问题
+#官方配置中login再加上-f可以指定用户
 #su $(whoami) -c 'ttyd -p 3000 -t fontSize=18 ssh localhost'
