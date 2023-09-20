5f34de50bffabcade2ada903ed79dd733b538b42
sherylynn <352281674@qq.com>
2023-09-20 11:12:23 +0800

    finish arm mac koreader(should release with -d)

--- mac/koreader.sh
+++ mac/koreader.sh
@@ -3,6 +3,11 @@ cd ~
 git clone https://github.com/koreader/koreader.git
 export PATH="$(brew --prefix)/opt/gettext/bin:$(brew --prefix)/opt/gnu-getopt/bin:$(brew --prefix)/opt/bison/bin:$(brew --prefix)/opt/grep/libexec/gnubin:${PATH}"
 export PATH="/usr/local/bin:/usr/local/sbin:${PATH/:\/usr\/local\/bin/}"
-export MACOSX_DEPLOYMENT_TARGET=10.15;
+export LDFLAGS="-L/opt/homebrew/opt/libiconv/lib"
+#export DYLD_LIBRARY="/opt/homebrew/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libiconv/include"
+export MACOSX_DEPLOYMENT_TARGET=12.00
 cd koreader && ./kodev fetch-thirdparty
-./kodev release macos
+#./kodev build
+./kodev release macos -d
+#./kodev release macos
