d7407e7c0512c775e392034bd7a9950f1a8c5380
sherylynn <352281674@qq.com>
2023-09-29 17:55:15 +0800

    success rime in archlinux

--- archlinux/normal.sh
+++ archlinux/normal.sh
@@ -5,6 +5,15 @@ TOOLSRC_NAME=ibusrimerc
 TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
 #sudo pacman -Syu gvim android-tools fcitx5-rime fcitx5-configtool
 sudo pacman -Syu gvim android-tools
-sudo pacman -S ibus-rime
+#sudo pacman -S ibus-rime
 
-echo 'GTK_IM_MODULE=ibus QT_IM_MODULE=ibus XMODIFIERS=@im=ibus'> ${TOOLSRC}
+#echo 'GTK_IM_MODULE=ibus QT_IM_MODULE=ibus XMODIFIERS=@im=ibus'> ${TOOLSRC}
+sudo pacman -S fcitx5-im
+tee ~/.xprofile <<-'EOF'
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+
+fcitx5 &
+EOF
+sudo pacman -S fcitx5-rime
