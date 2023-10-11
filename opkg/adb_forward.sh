a2676b691ed79825e5ae2c79bbcb9df9b06402f2
sherylynn <352281674@qq.com>
2023-10-11 20:57:10 +0800

    add adb port

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -9,15 +9,18 @@ cat << "EOF" > /root/adb-watchdog.sh
 adb -a forward tcp:10086 tcp:10086
 adb -a forward tcp:10808 tcp:10808
 adb -a forward tcp:3000 tcp:3000
+adb -a forward tcp:5555 tcp:5555
 adb -a forward tcp:5900 tcp:5900
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.route_localnet=1
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destination 127.0.0.1:10808
+iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 5555 -j DNAT --to-destination 127.0.0.1:5555
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 5900 -j DNAT --to-destination 127.0.0.1:5900
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destination 127.0.0.1:10808
+iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 5555 -j DNAT --to-destination 127.0.0.1:5555
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 5900 -j DNAT --to-destination 127.0.0.1:5900
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
 EOF
