50643632296b00cbc58bfacd5ba3e7ee480a6962
sherylynn <352281674@qq.com>
2023-10-31 10:38:08 +0800

    avoid sv-down termux service

--- termux/termux_service_ttyd.sh
+++ termux/termux_service_ttyd.sh
@@ -7,7 +7,8 @@ SCRIPT_NAME=ttyd
 	echo '#!/bin/sh' > ${SCRIPT_NAME}/run
 	echo 'exec $PREFIX/../home/sh/termux/server_'${SCRIPT_NAME}'.sh' >> ${SCRIPT_NAME}/run
 	chmod +x ${SCRIPT_NAME}/run
-	touch ${SCRIPT_NAME}/down
+#	touch ${SCRIPT_NAME}/down
+#	if touch will sv-down ${SCRIPT_NAME}
 	ln -sf $PREFIX/share/termux-services/svlogger ${SCRIPT_NAME}/log/run
 # Setup shortcuts
 mkdir -p ~/.shortcuts
