737ae28432e7a27ae977613c3091b488df7566f0
sherylynn <352281674@qq.com>
2023-11-03 11:14:58 +0800

    fix doomemacs in termux (installer)

---
+++ termux/doomemacs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+. $(dirname "$0")/../win-git/toolsinit.sh
+NAME=doom
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+#rm ~/.emacs.d
+rm -rf ~/.emacs.d
+git clone --depth 1 https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d_doom
+#ln -s ~/.emacs.d_doom ~/.emacs.d
+git clone https://github.com/sherylynn/doom-private ~/.doom.d_pc
+~/.emacs.d_doom/bin/doom install
+
+tee ~/.emacs-profiles.el <<-'EOF'
+(("default"   . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
+EOF
+SOFT_BIN=~/.emacs.d_doom/bin
+echo 'export PATH=$PATH:'${SOFT_BIN}>${TOOLSRC}
+#
+rm -rf ~/.doom.d
+mv ~/.doom.d_pc ~/.doom.d
---
+++ termux/emacs.sh
@@ -0,0 +1,3 @@
+pkg install emacs 
+pkg install gh fd ripgrep
+pkg install cmake libvterm librime
