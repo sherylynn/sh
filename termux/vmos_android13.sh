00016519b98295bd2f297f1512bd141b80097c61
sherylynn <352281674@qq.com>
2024-01-10 22:26:12 +0800

    update

--- termux/vmos_android13.sh
+++ termux/vmos_android13.sh
@@ -8,13 +8,12 @@
 
 #adb shell "su -c 'settings put global settings_enable_monitor_phantom_proc false'"
 ANDROID_VERSION=sudo getprop |grep ro.build.version.release |awk -F '[][]' '{print $4}'
-if [[ ANDROID_VERSION == 12 ]]; then
+if [[ $ANDROID_VERSION == 12 ]]; then
   sudo device_config put activity_manager max_phantom_processes 2147483647
 fi
-if [[ ANDROID_VERSION == 13 ]]; then
+if [[ $ANDROID_VERSION == 13 ]]; then
   sudo settings put global settings_enable_monitor_phantom_proc false
 fi
-if [[ ANDROID_VERSION == 14 ]]; then
-  sudo settings put global settings_enable_monitor_phantom_proc false
+if [[ $ANDROID_VERSION == 14 ]]; then
   sudo setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
 fi
