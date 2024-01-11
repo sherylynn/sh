27f7c91bd8dbdcd3eecd22d42de3a8977bc009e0
sherylynn <352281674@qq.com>
2024-01-11 08:58:38 +0800

    update

--- termux/vmos_android13.sh
+++ termux/vmos_android13.sh
@@ -1,3 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
 #adb connect 127.0.0.1:5555
 #adb wait-for-device
 #adb devices
@@ -7,10 +8,13 @@
 
 
 #adb shell "su -c 'settings put global settings_enable_monitor_phantom_proc false'"
-ANDROID_VERSION=sudo getprop |grep ro.build.version.release |awk -F '[][]' '{print $4}'
+ANDROID_VERSION=$(sudo getprop |grep ro.build.version.release] |awk -F '[][]' '{print $4}')
+#命令太长需要括号起来
+echo $ANDROID_VERSION
 if [[ $ANDROID_VERSION == 12 ]]; then
-  sudo device_config put activity_manager max_phantom_processes 2147483647
-  sudo /system/bin/dumpsys package com.google.android.gms | grep WRITE_DEVICE_CONFIG
+  echo "android12"
+  adb shell "su -c '/system/bin/device_config set_sync_disabled_for_tests persistent'"
+  adb shell "su -c '/system/bin/device_config put activity_manager max_phantom_processes 2147483647'"
 fi
 if [[ $ANDROID_VERSION == 13 ]]; then
   sudo settings put global settings_enable_monitor_phantom_proc false
