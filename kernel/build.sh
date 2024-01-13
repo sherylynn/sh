23bcf8e765ebf2f403819ad074242a5f2ec40594
sherylynn <352281674@qq.com>
2024-01-13 14:18:34 +0800

    update

---
+++ kernel/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+args="-j $(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LDGOLD=aarch64-linux-gnu-ld.gold LLVM_AR=llvm-ar LLVM_DIS=llvm-dis"
+#cd ~/neko_kernel_xiaomi_gauguin/arch/arm64/configs/vendor
+cd ~/neko_kernel_xiaomi_gauguin
+make ${args} gauguin_user_defconfig
+make ${args}
+#make ${args}
--- kernel/install.sh
+++ kernel/install.sh
@@ -6,7 +6,7 @@ SOFT_HOME=$(install_path)/${TOOLSRC_NAME}
 
 sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 # install repo in ~/sh/win-git/repo.sh
-brew install bc bison ccache curl flex git gnupg make imagemagick sdl
+#brew install bc bison ccache curl flex git gnupg make imagemagick sdl
 
 mkdir -p ~/tools
 cd ~/tools
