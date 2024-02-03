0b108c70def2c9ae62b212debedc6926dded8e30
sherylynn <352281674@qq.com>
2024-02-04 01:40:47 +0800

    add power stayon

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -6,7 +6,6 @@ opkg install adb
 # Save connectivity checking script
 cat << "EOF" > /root/adb-watchdog.sh
 #!/bin/sh
-adb shell svc usb setFunctions rndis
 adb -a forward tcp:10086 tcp:10086
 adb -a forward tcp:10808 tcp:10808
 adb -a forward tcp:3000 tcp:3000
@@ -32,4 +31,41 @@ chmod +x /root/adb-watchdog.sh
 # Add cron job
 cat << "EOF" >> /etc/crontabs/root
 * * * * * /root/adb-watchdog.sh
+EOF
+
+
+
+cat << "EOF" > /root/adb-wan_check.sh
+#!/bin/sh
+function network() {
+	#超时时间
+	local timeout=30
+
+	#目标网站
+	local target=www.baidu.com
+
+	#获取响应状态码
+	local ret_code=$(curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1)
+
+	if [ "x$ret_code" = "x200" ]; then
+		#网络畅通
+		return 1
+	else
+		#网络不畅通
+		return 0
+	fi
+
+	return 0
+}
+
+network
+if [ $? -eq 0 ]; then
+  adb shell svc usb setFunctions rndis
+  adb shell svc power stayon usb
+fi
+EOF
+chmod +x /root/adb-wan_check.sh
+
+cat << "EOF" >> /etc/crontabs/root
+* * * * * /root/adb-wan_check.sh
 EOF
