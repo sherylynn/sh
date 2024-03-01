b2151f4b5ecb0c229e1ae3b93c564f0cb45dffaf
sherylynn <352281674@qq.com>
2024-03-01 18:59:57 +0800

    change network check function

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -1,7 +1,7 @@
 opkg install iptables
 opkg install iptables-mod-nat-extra
 opkg install adb
-opkg install curl
+#opkg install curl
 
 
 # Save connectivity checking script
@@ -10,6 +10,7 @@ cat << "EOF" > /root/adb-watchdog.sh
 adb -a forward tcp:10086 tcp:10086
 adb -a forward tcp:10808 tcp:10808
 adb -a forward tcp:3000 tcp:3000
+adb -a forward tcp:3333 tcp:3333
 adb -a forward tcp:5555 tcp:5555
 adb -a forward tcp:5900 tcp:5900
 adb -a forward tcp:5244 tcp:5244
@@ -20,32 +21,41 @@ iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destin
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 5555 -j DNAT --to-destination 127.0.0.1:5555
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 5900 -j DNAT --to-destination 127.0.0.1:5900
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
+iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 3333 -j DNAT --to-destination 127.0.0.1:3333
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destination 127.0.0.1:10808
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 5555 -j DNAT --to-destination 127.0.0.1:5555
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 5244 -j DNAT --to-destination 127.0.0.1:5244
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 5900 -j DNAT --to-destination 127.0.0.1:5900
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
+iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 3333 -j DNAT --to-destination 127.0.0.1:3333
 
 function network() {
 	#超时时间
 	local timeout=30
 
 	#目标网站
-	local target=www.baidu.com
+	#local target=www.baidu.com
+	local target=114.114.114.114
 
 	#获取响应状态码
-	local ret_code=$(curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1)
+	#local ret_code=$(curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1)
+	local ping_code=`ping -c 2 -W 5 $target|grep '64 bytes'|wc -l`
 
-	if [ "x$ret_code" = "x200" ]; then
+	#if [ "x$ret_code" = "x200" ]; then
+		#网络畅通
+	#	return 1
+	#else
+		#网络不畅通
+	#	return 0
+	#fi
+	if [ ${ping_code} -ne 0 ];then
 		#网络畅通
 		return 1
 	else
 		#网络不畅通
 		return 0
 	fi
-
-	return 0
 }
 
 ANDROID_NAME=$(adb shell getprop ro.product.name)
