ce1086522b607fbcbe1d58c356e1290c43811a27
sherylynn <352281674@qq.com>
2024-04-02 17:15:34 +0800

    use myemacs

---
+++ myemacs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. $(dirname "$0")/win-git/toolsinit.sh
+NAME=my
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+#rm ~/.emacs.d
+rm -rf ~/.emacs.d
+git clone --depth 1 https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/sherylynn/myemacs.d.git ~/.emacs.d_my
+#ln -s ~/.emacs.d_doom ~/.emacs.d
+#git clone https://github.com/sherylynn/doom-private ~/.doom.d
+#~/.emacs.d_doom/bin/doom install
+
+tee ~/.emacs-profiles.el <<-'EOF'
+(("default"   . ((user-emacs-directory . "~/.emacs.d_my")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
+EOF
+#SOFT_BIN=~/.emacs.d_doom/bin
+#echo 'export PATH=$PATH:'${SOFT_BIN}>${TOOLSRC}
