ab93941dacb6fdc5e671995f9a302439cd4a02b8
sherylynn <352281674@qq.com>
2024-04-07 00:57:27 +0800

    add android emacs

---
+++ termux/myemacs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. $(dirname "$0")/win-git/toolsinit.sh
+NAME=my
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+#rm ~/.emacs.d
+rm -rf ~/.emacs.d
+rm -rf /data/data/org.gnu.emacs/files/.emacs.d
+git clone --depth 1 https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/plexus/chemacs2.git /data/data/org.gnu.emacs/files/.emacs.d
+git clone --depth 1 https://github.com/sherylynn/myemacs.d.git ~/.emacs.d_my
+git clone --depth 1 https://github.com/sherylynn/myemacs.d.git /data/data/org.gnu.emacs/files/.emacs.d_my
+#ln -s ~/.emacs.d_doom ~/.emacs.d
+#git clone https://github.com/sherylynn/doom-private ~/.doom.d
+#~/.emacs.d_doom/bin/doom install
+
+#还需要加入earlyinit
+tee ~/.emacs-profiles.el <<-'EOF'
+(("default"   . ((user-emacs-directory . "~/.emacs.d_my")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
+EOF
+#SOFT_BIN=~/.emacs.d_doom/bin
+#echo 'export PATH=$PATH:'${SOFT_BIN}>${TOOLSRC}
