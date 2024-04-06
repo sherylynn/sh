b93a75155c98fc64ccc709d2001d6d2f6d41d0fc
sherylynn <352281674@qq.com>
2024-04-07 02:08:54 +0800

    兼容termux内部的emacs-x

--- termux/myemacs.sh
+++ termux/myemacs.sh
@@ -15,7 +15,14 @@ git clone --depth 1 https://github.com/sherylynn/myemacs.d.git /data/data/org.gn
 #~/.emacs.d_doom/bin/doom install
 
 #还需要加入earlyinit
+#映射了也没用，termux中无法直接emacs，我也不是doom，用不到
+#ln -s /data/data/org.gnu.emacs/lib/libandroid-emacs.so /data/data/com.termux/files/usr/bin/emacs
 tee ~/.emacs-profiles.el <<-'EOF'
+(("default"   . ((user-emacs-directory . "~/.emacs.d_my")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
+ ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
+EOF
+tee /data/data/org.gnu.emacs/files/.emacs-profiles.el <<-'EOF'
 (("default"   . ((user-emacs-directory . "~/.emacs.d_my")))
  ("doom" . ((user-emacs-directory . "~/.emacs.d_doom")))
  ("space"   . ((user-emacs-directory . "~/.emacs.d_space"))))
