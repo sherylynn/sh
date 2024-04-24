213f2ee6546a3a325b03a63543829173db64bebd
sherylynn <352281674@qq.com>
2024-04-25 01:29:38 +0800

    update

--- termux/emacs_native.sh
+++ termux/emacs_native.sh
@@ -1,6 +1,6 @@
-rm /data/data/org.gnu.emacs/files
+rm -rf /data/data/org.gnu.emacs/files
 ln -s /data/data/com.termux/files/home /data/data/org.gnu.emacs/files
-rm /data/data/com.termux/files/home/fonts
+rm -rf /data/data/com.termux/files/home/fonts
 ln -s /data/data/com.termux/files/home/.local/share/fonts /data/data/com.termux/files/home/fonts
-rm /data/data/com.termux/files/home/fonts/font.ttf
+rm -rf /data/data/com.termux/files/home/fonts/font.ttf
 ln -s /data/data/com.termux/files/home/.termux/font.ttf /data/data/com.termux/files/home/fonts/font.ttf
