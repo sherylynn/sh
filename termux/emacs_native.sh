3164f13abc55e1254b9897ce815481c40828b2b9
sherylynn <352281674@qq.com>
2024-04-26 01:05:46 +0800

    add font from /system/fonts/DroidSans.ttf

--- termux/emacs_native.sh
+++ termux/emacs_native.sh
@@ -1,6 +1,10 @@
 rm -rf /data/data/org.gnu.emacs/files
 ln -s /data/data/com.termux/files/home /data/data/org.gnu.emacs/files
 rm -rf /data/data/com.termux/files/home/fonts
+mkdir -p /data/data/com.termux/files/home/.local/share/fonts
 ln -s /data/data/com.termux/files/home/.local/share/fonts /data/data/com.termux/files/home/fonts
 rm -rf /data/data/com.termux/files/home/fonts/font.ttf
-ln -s /data/data/com.termux/files/home/.termux/font.ttf /data/data/com.termux/files/home/fonts/font.ttf
+rm  /data/data/com.termux/files/home/.termux/font.ttf
+#ln -s /system/fonts/SourceSansPro-Regular.ttf /data/data/com.termux/files/home/.termux/font.ttf
+ln -s /system/fonts/DroidSansMono.ttf /data/data/com.termux/files/home/.termux/font.ttf
+ln -s /system/fonts/DroidSansMono.ttf /data/data/com.termux/files/home/fonts/font.ttf
