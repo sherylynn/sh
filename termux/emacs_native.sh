10f28664b82e0531e6e0df92d77c21a118b23919
sherylynn <352281674@qq.com>
2024-04-26 21:45:52 +0800

    [2024-04-26 Fri 21:45]

--- termux/emacs_native.sh
+++ termux/emacs_native.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . $(dirname "$0")/../win-git/toolsinit.sh
 FONT_NAME=PingFang-Regular.ttf
-FONT_URL=https://github.com/ShmilyHTT/PingFang/blob/master/PingFang-Regular.ttf
+FONT_URL=https://github.com/ShmilyHTT/PingFang/raw/master/PingFang-Regular.ttf
 
 #干掉默认的emacs的HOME，链接termux的HOME
 rm -rf /data/data/org.gnu.emacs/files
@@ -17,7 +17,8 @@ rm  /data/data/com.termux/files/home/.termux/font.ttf
 ln -s /system/fonts/DroidSansMono.ttf /data/data/com.termux/files/home/.termux/font.ttf
 #设置emacs的字体
 ln -s /system/fonts/DroidSansMono.ttf /data/data/com.termux/files/home/fonts/font.ttf
+#ln -s /system/fonts/SourceSansPro-Regular.ttf /data/data/com.termux/files/home/fonts/font.ttf
 #下载苹方字体
-$(cache_downloader PingFang.ttf $FONT_URL)
+$(cache_downloader $FONT_NAME $FONT_URL)
 rm /data/data/com.termux/files/home/fonts/PingFang-Regular.ttf 
 ln -s /data/data/com.termux/files/home/tools/cache/PingFang-Regular.ttf /data/data/com.termux/files/home/fonts/PingFang-Regular.ttf 
