d61b4d9cb3ff15d4e04a7a996cf097cd945134e9
sherylynn <352281674@qq.com>
2024-06-14 21:13:48 +0800

    suit start debian for apatch

--- termux/chroot/start_debian.sh
+++ termux/chroot/start_debian.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
+#apatch busybox
+busybox=/data/adb/ap/bin/busybox
+
 #Path of DEBIAN rootfs
 debian_folder_path="/data/data/com.termux/files/home/Desktop/chrootdebian"
 DEBIANPATH=$debian_folder_path
 
 # Fix setuid issue
-busybox mount -o remount,dev,suid /data
+sudo $busybox mount -o remount,dev,suid /data
 
-busybox mount --bind /dev $DEBIANPATH/dev
-busybox mount --bind /sys $DEBIANPATH/sys
-busybox mount --bind /proc $DEBIANPATH/proc
-busybox mount -t devpts devpts $DEBIANPATH/dev/pts
+sudo $busybox mount --bind /dev $DEBIANPATH/dev
+sudo $busybox mount --bind /sys $DEBIANPATH/sys
+sudo $busybox mount --bind /proc $DEBIANPATH/proc
+sudo $busybox mount -t devpts devpts $DEBIANPATH/dev/pts
 
 # /dev/shm for Electron apps
-mkdir $DEBIANPATH/dev/shm
-busybox mount -t tmpfs -o size=256M tmpfs $DEBIANPATH/dev/shm
+sudo mkdir $DEBIANPATH/dev/shm
+sudo $busybox mount -t tmpfs -o size=256M tmpfs $DEBIANPATH/dev/shm
 
 # Mount sdcard
-mkdir $DEBIANPATH/sdcard
-busybox mount --bind /sdcard $DEBIANPATH/sdcard
+sudo mkdir $DEBIANPATH/sdcard
+sudo $busybox mount --bind /sdcard $DEBIANPATH/sdcard
 
 # chroot into DEBIAN
-#busybox chroot $DEBIANPATH /bin/su - root
-#busybox chroot $DEBIANPATH /bin/su - root -c 'export XDG_RUNTIME_DIR=${TMPDIR} && export PULSE_SERVER=tcp:127.0.0.1:4713 && sudo service dbus start && su - droidmaster -c "env DISPLAY=:0 startxfce4"'
-busybox chroot $DEBIANPATH /bin/su - droidmaster -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && dbus-launch --exit-with-session startxfce4'
+#sudo $busybox chroot $DEBIANPATH /bin/su - root
+#sudo $busybox chroot $DEBIANPATH /bin/su - root -c 'export XDG_RUNTIME_DIR=${TMPDIR} && export PULSE_SERVER=tcp:127.0.0.1:4713 && sudo service dbus start && su - lynn -c "env DISPLAY=:0 startxfce4"'
+sudo $busybox chroot $DEBIANPATH /bin/su - lynn -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && dbus-launch --exit-with-session startxfce4'
