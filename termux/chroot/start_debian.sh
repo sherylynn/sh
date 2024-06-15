121c7c8fea8f2c5a098a46ded22b8d07dc6bc03e
sherylynn <352281674@qq.com>
2024-06-15 16:23:49 +0800

    update

--- termux/chroot/start_debian.sh
+++ termux/chroot/start_debian.sh
@@ -6,6 +6,282 @@ busybox=/data/adb/ap/bin/busybox
 debian_folder_path="/data/data/com.termux/files/home/Desktop/chrootdebian"
 DEBIANPATH=$debian_folder_path
 
+is_mounted()
+{
+    local mount_point="$1"
+    [ -n "${mount_point}" ] || return 1
+    if $(grep -q " ${mount_point%/} " /proc/mounts); then
+        return 0
+    else
+        return 1
+    fi
+}
+
+container_mounted()
+{
+    if [ "${METHOD}" = "chroot" ]; then
+        is_mounted "${CHROOT_DIR}"
+    else
+        return 0
+    fi
+}
+
+mount_part()
+{
+    case "$1" in
+    root)
+        msg -n "/ ... "
+        if ! is_mounted "${CHROOT_DIR}" ; then
+            [ -d "${CHROOT_DIR}" ] || mkdir -p "${CHROOT_DIR}"
+            local mnt_opts
+            [ -d "${TARGET_PATH}" ] && mnt_opts="bind" || mnt_opts="rw,relatime"
+            mount -o ${mnt_opts} "${TARGET_PATH}" "${CHROOT_DIR}" &&
+            mount -o remount,exec,suid,dev "${CHROOT_DIR}"
+            is_ok "fail" "done" || return 1
+        else
+            msg "skip"
+        fi
+    ;;
+    proc)
+        msg -n "/proc ... "
+        local target="${CHROOT_DIR}/proc"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -t proc proc "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    sys)
+        msg -n "/sys ... "
+        local target="${CHROOT_DIR}/sys"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -t sysfs sys "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    system)
+        msg -n "/system ... "
+        local target="${CHROOT_DIR}/system"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -o ro /system "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    vendor)
+        msg -n "/vendor ... "
+        local target="${CHROOT_DIR}/vendor"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -o ro /vendor "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    apex)
+        msg -n "/apex ... "
+        local target="${CHROOT_DIR}/apex"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -o ro /apex "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    com.android.runtime)
+        msg -n "/apex/com.android.runtime ... "
+        local target="${CHROOT_DIR}/apex/com.android.runtime"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -o ro /apex/com.android.runtime "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    dev)
+        msg -n "/dev ... "
+        local target="${CHROOT_DIR}/dev"
+        if ! is_mounted "${target}" ; then
+            [ -d "${target}" ] || mkdir -p "${target}"
+            mount -o bind /dev "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    shm)
+        msg -n "/dev/shm ... "
+        if ! is_mounted "/dev/shm" ; then
+            [ -d "/dev/shm" ] || mkdir -p /dev/shm
+            mount -o rw,nosuid,nodev,mode=1777 -t tmpfs tmpfs /dev/shm
+        fi
+        local target="${CHROOT_DIR}/dev/shm"
+        if ! is_mounted "${target}" ; then
+            mount -o bind /dev/shm "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    pts)
+        msg -n "/dev/pts ... "
+        if ! is_mounted "/dev/pts" ; then
+            [ -d "/dev/pts" ] || mkdir -p /dev/pts
+            mount -o rw,nosuid,noexec,gid=5,mode=620,ptmxmode=000 -t devpts devpts /dev/pts
+        fi
+        local target="${CHROOT_DIR}/dev/pts"
+        if ! is_mounted "${target}" ; then
+            mount -o bind /dev/pts "${target}"
+            is_ok "fail" "done"
+        else
+            msg "skip"
+        fi
+    ;;
+    fd)
+        if [ ! -e "/dev/fd" -o ! -e "/dev/stdin" -o ! -e "/dev/stdout" -o ! -e "/dev/stderr" ]; then
+            msg -n "/dev/fd ... "
+            [ -e "/dev/fd" ] || ln -s /proc/self/fd /dev/
+            [ -e "/dev/stdin" ] || ln -s /proc/self/fd/0 /dev/stdin
+            [ -e "/dev/stdout" ] || ln -s /proc/self/fd/1 /dev/stdout
+            [ -e "/dev/stderr" ] || ln -s /proc/self/fd/2 /dev/stderr
+            is_ok "fail" "done"
+        fi
+    ;;
+    tty)
+        if [ ! -e "/dev/tty0" ]; then
+            msg -n "/dev/tty ... "
+            ln -s /dev/null /dev/tty0
+            is_ok "fail" "done"
+        fi
+    ;;
+    tun)
+        if [ ! -e "/dev/net/tun" ]; then
+            msg -n "/dev/net/tun ... "
+            [ -d "/dev/net" ] || mkdir -p /dev/net
+            mknod /dev/net/tun c 10 200
+            is_ok "fail" "done"
+        fi
+    ;;
+    binfmt_misc)
+        multiarch_support || return 0
+        local binfmt_dir="/proc/sys/fs/binfmt_misc"
+        if ! is_mounted "${binfmt_dir}" ; then
+            msg -n "${binfmt_dir} ... "
+            mount -t binfmt_misc binfmt_misc "${binfmt_dir}"
+            is_ok "fail" "done"
+        fi
+    ;;
+    esac
+
+    return 0
+}
+
+container_mount()
+{
+    [ "${METHOD}" = "chroot" ] || return 0
+
+    if [ $# -eq 0 ]; then
+        container_mount root proc sys system vendor apex com.android.runtime dev shm pts fd tty tun binfmt_misc
+        return $?
+    fi
+
+    params_check TARGET_PATH || return 1
+
+    msg -n "Checking file system ... "
+    fs_check
+    is_ok "skip" "done"
+
+    msg "Mounting the container: "
+    local item
+    for item in $*
+    do
+        mount_part ${item} || return 1
+    done
+
+    return 0
+}
+
+container_umount()
+{
+    params_check TARGET_PATH || return 1
+    container_mounted || { msg "The container is not mounted." ; return 0; }
+
+    msg -n "Release resources ... "
+    local is_release=0
+    local lsof_full=$(lsof | awk '{print $1}' | grep -c '^lsof')
+    if [ "${lsof_full}" -eq 0 ]; then
+        local pids=$(lsof | grep "${CHROOT_DIR%/}" | awk '{print $1}' | uniq)
+    else
+        local pids=$(lsof | grep "${CHROOT_DIR%/}" | awk '{print $2}' | uniq)
+    fi
+    kill_pids ${pids}; is_ok "fail" "done"
+
+    msg "Unmounting partitions: "
+    local is_mnt=0
+    local mask
+    for mask in '.*' '*'
+    do
+        local parts=$(cat /proc/mounts | awk '{print $2}' | grep "^${CHROOT_DIR%/}/${mask}$" | sort -r)
+        local part
+        for part in ${parts}
+        do
+            local part_name=$(echo ${part} | sed "s|^${CHROOT_DIR%/}/*|/|g")
+            msg -n "${part_name} ... "
+            for i in 1 2 3
+            do
+                umount ${part} && break
+                sleep 1
+            done
+            is_ok "fail" "done"
+            is_mnt=1
+        done
+    done
+    [ "${is_mnt}" -eq 1 ]; is_ok " ...nothing mounted"
+
+    local loop=$(losetup -a | grep "${TARGET_PATH%/}" | awk -F: '{print $1}')
+    if [ -n "${loop}" ]; then
+        msg -n "Disassociating loop device ... "
+        losetup -d "${loop}"
+        is_ok "fail" "done"
+    fi
+
+    return 0
+}
+
+container_start()
+{
+    container_mounted || { msg "The container is not mounted." ; return 1; }
+
+    DO_ACTION='do_start'
+    if [ $# -gt 0 ]; then
+        component_exec "$@"
+    else
+        component_exec "${INCLUDE}"
+    fi
+}
+
+container_stop()
+{
+    container_mounted || { msg "The container is not mounted." ; return 1; }
+
+    DO_ACTION='do_stop'
+    if [ $# -gt 0 ]; then
+        component_exec "$@"
+    else
+        component_exec "${INCLUDE}"
+    fi
+}
+
 # Fix setuid issue
 sudo $busybox mount -o remount,dev,suid /data
 
