83cc1cc993e8a1bf45ca0691905fe16ad9ae9884
sherylynn <352281674@qq.com>
2024-06-17 00:16:01 +0800

    update vnc in termux chroot

--- win-git/server_noVNC.sh
+++ win-git/server_noVNC.sh
@@ -20,6 +20,10 @@ echo $(whoami)
 #novnc -p 3000 -t fontSize=18 login
 # login need systemd user $(whoami)
 cd ../../tools/noVNC
-#./utils/novnc_proxy --vnc 127.0.0.1:5900 --listen 10000
-./utils/novnc_proxy --vnc 127.0.0.1:5900 --listen 10086
+rm -rf /tmp/.X*
+rm -rf /tmp/.x*
+vncserver -kill :0
+vncserver -geometry 1920x966 :0
+./utils/novnc_proxy --vnc 127.0.0.1:5900 --listen 10000
+#./utils/novnc_proxy --vnc 127.0.0.1:5900 --listen 10086
 #su $(whoami) -c 'novnc -p 3000 -t fontSize=18 ssh localhost'
