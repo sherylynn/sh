4207058641d2e68f73d55b42693e4580be44cfd3
sherylynn <352281674@qq.com>
2024-06-17 23:00:49 +0800

    apt autoremove -y

--- termux/chroot/chroot_debian_installer.sh
+++ termux/chroot/chroot_debian_installer.sh
@@ -5,7 +5,7 @@ debian_run_scrpit="/data/data/com.termux/files/home/sh/termux/chroot/start_debia
 debian_xfce_scrpit="/data/data/com.termux/files/home/sh/termux/chroot/startxfce4_chrootDebian.sh"
 
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
-apt update && apt upgrade -y
+apt update && apt upgrade -y && apt autoremove -y
 pkg install x11-repo root-repo termux-x11-nightly -y
 apt update
 pkg install tsu pulseaudio virglrenderer-android -y
