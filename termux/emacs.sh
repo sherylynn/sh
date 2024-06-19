7c1f2925ccccfe3c89c85b9aa0795a6505f13eeb
sherylynn <352281674@qq.com>
2024-06-20 00:06:31 +0800

    update

--- termux/emacs.sh
+++ termux/emacs.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-sudo apt install emacs -y
-#apt install gh fd ripgrep -y
-sudo apt install cmake libvterm librime -y
-sudo apt install build-essential -y
+pkg install emacs 
+pkg install gh fd ripgrep
+pkg install cmake libvterm librime
