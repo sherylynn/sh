282c993770d90e3e6e905b609ee1a834ed1bb0e3
sherylynn <352281674@qq.com>
2024-06-20 15:50:21 +0800

    fix emacs install in native debian

--- debian/emacs.sh
+++ debian/emacs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install emacs -y
 #apt install gh fd ripgrep -y
-sudo apt install cmake libvterm librime -y
+sudo apt install cmake libvterm-dev librime-dev -y
 sudo apt install build-essential -y
