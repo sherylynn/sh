9fd3a1984cb30168a91d7fe15b5110e4294e1c2f
sherylynn <352281674@qq.com>
2024-06-20 11:50:45 +0800

    fix firefox source

--- debian/firefox.sh
+++ debian/firefox.sh
@@ -6,7 +6,9 @@ sudo install -d -m 0755 /etc/apt/keyrings
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 
 #把 Mozilla APT 库添加到源列表中： 
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+#echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+sudo cp ~/sh/debian/mozilla.list /etc/apt/sources.list.d/
+
 
 #配置 APT 优先使用 Mozilla 库中的包：
 echo '
