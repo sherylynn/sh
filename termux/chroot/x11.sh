b2f877e5bf1c5c28da520b1731331e416e9f2f56
sherylynn <352281674@qq.com>
2024-06-23 04:38:53 +0800

    back

--- termux/chroot/x11.sh
+++ termux/chroot/x11.sh
@@ -40,6 +40,6 @@ export XMODIFIERS="@im=fcitx" && \
 #fcitx5 && \
 export GALLIUM_DRIVER=virpipe && \
 export MESA_GL_VERSION_OVERRIDE=4.0 && \
-vncserver -kill :0 && \
+#vncserver -kill :0 && \
 #rm -rf /tmp/.X* && \
 dbus-launch --exit-with-session startxfce4'
