a0fe2bdc889f913a31f5c6cbf6f3466e02e1008f
sherylynn <352281674@qq.com>
2024-06-23 02:52:09 +0800

    update

--- debian/testing_mirror.sh
+++ debian/testing_mirror.sh
@@ -6,6 +6,8 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 425956BB3E31DF51
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B8F213033DB6B2D6
 
 sudo apt update
+sudo apt remove fonts-dejavu-mono -y
+sudo apt install fonts-dejavu-core -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
