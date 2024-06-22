67da0c9e718e07295899bcd6c6471c1743897b3b
sherylynn <352281674@qq.com>
2024-06-22 15:01:13 +0800

    update

--- termux/chroot/noVNC.sh
+++ termux/chroot/noVNC.sh
@@ -37,4 +37,4 @@ export GTK_IM_MODULE="fcitx" && \
 export QT_IM_MODULE="fcitx" && \
 export XMODIFIERS="@im=fcitx" && \
 #fcitx5 && \
-dbus-launch --exit-with-session startxfce4'
+dbus-launch --exit-with-session zsh ~/sh/win-git/noVNC.sh'
