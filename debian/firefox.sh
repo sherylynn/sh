dab6888130a80ba991e56b16a1e89500fabec6ae
sherylynn <352281674@qq.com>
2024-06-23 08:24:46 +0000

    fix firefox error

--- debian/firefox.sh
+++ debian/firefox.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+. $(dirname "$0")/../win-git/toolsinit.sh
+TOOLSRC_NAME=mesarc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
 #创建一个保存 APT 库密钥的目录： 
 sudo install -d -m 0755 /etc/apt/keyrings 
 
@@ -17,13 +20,22 @@ Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
 
-#更新软件列表并安装 Firefox .deb 包： 
-sudo apt remove firefox-esr -y && sudo apt autoremove -y
-sudo apt-get update && sudo apt-get install firefox -y
+#原来和版本无关
+notneed()
+{
+    #更新软件列表并安装 Firefox .deb 包： 
+    sudo apt remove firefox-esr -y && sudo apt autoremove -y
+    sudo apt-get update && sudo apt-get install firefox -y
 
-#使用 .deb 包设置 Firefox 语言
-sudo apt-get install firefox-l10n-zh-cn -y
+    #使用 .deb 包设置 Firefox 语言
+    sudo apt-get install firefox-l10n-zh-cn -y
 
-#如果是testing版本
-sudo apt-get install firefox-nightly -y
-sudo apt-get install firefox-nightly-l10n-zh-cn -y
+    #如果是testing版本
+    sudo apt-get install firefox-nightly -y
+    sudo apt-get install firefox-nightly-l10n-zh-cn -y
+}
+if [[ $(whoami) == "root" ]]; then
+    #如果是root则关闭sandbox
+    echo 'export MOZ_DISABLE_CONTENT_SANDBOX=1'>${TOOLSRC}
+    echo 'export MOZ_X11_EGL=1'>>${TOOLSRC}
+fi
