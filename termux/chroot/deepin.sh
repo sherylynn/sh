35e04417dfc724021dbd627495af2d3e48223cdf
sherylynn <352281674@qq.com>
2024-06-26 15:24:59 +0800

    还是放弃了

--- termux/chroot/deepin.sh
+++ termux/chroot/deepin.sh
@@ -44,9 +44,10 @@ configure_debian_chroot() {
     fi
 
     #sudo debootstrap --arch=arm64 bookworm $DEBIAN_DIR http://mirrors.tuna.tsinghua.edu.cn/debian
-    #sudo debootstrap --arch=arm64 testing $DEBIAN_DIR http://mirrors.tuna.tsinghua.edu.cn/debian
+    sudo debootstrap --arch=arm64 testing $DEBIAN_DIR http://mirrors.tuna.tsinghua.edu.cn/debian
     #sudo debootstrap --arch=arm64 beige $DEBIAN_DIR http://community-packages.deepin.com/beige/
-    sudo debootstrap --arch=arm64 beige $DEBIAN_DIR http://community-packages.deepin.com/beige/
+    # deepin需要mmdebstrap
+    #sudo debootstrap --arch=arm64 beige $DEBIAN_DIR http://community-packages.deepin.com/beige/
 
     container_mounted || container_mount
     #git config
