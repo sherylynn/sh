a25434f18c66b419b50a0fe2f9fee7fb7910b9ab
sherylynn <352281674@qq.com>
2024-06-26 10:22:52 +0800

    update

--- termux/init.sh
+++ termux/init.sh
@@ -1,11 +1,12 @@
 pkg install apt openssh -y
 pkg install htop coreutils tsu getconf vim zsh git wget curl -y termux-api
 #pkg install nodejs golang python
-pkg install tmux ttyd termux-services -y
+#pkg install tmux ttyd termux-services -y
+pkg install termux_services -y
 sv-enable sshd
 passwd
 #doom emacs
-pkg install emacs ripgrep fd librime gflags -y
+#pkg install emacs ripgrep fd librime gflags -y
 pkg install lua53 -y
 ln -s /data/data/com.termux/files/usr/bin/lua5.3 /data/data/com.termux/files/usr/bin/lua
 #cat bashrc >> $HOME/.bashrc
@@ -13,6 +14,6 @@ chsh -s zsh
 mkdir -p ~/.termux
 mkdir -p ~/bin
 ln -s -f ~/sh/termux/termux.properties $HOME/.termux/termux.properties
-ln -s -f /data/data/com.termux/files/usr/bin/nvim $HOME/bin/termux-file-editor
+#ln -s -f /data/data/com.termux/files/usr/bin/nvim $HOME/bin/termux-file-editor
 termux-setup-storage
 #ln -s -f ~/sh/termux/bashrc $HOME/.bashrc
