ecdc8b9abab88dbafcaa695b3dee8f0102e5929d
sherylynn <352281674@qq.com>
2024-06-26 10:31:55 +0800

    update

--- termux/init.sh
+++ termux/init.sh
@@ -2,7 +2,7 @@ pkg install apt openssh -y
 pkg install htop coreutils tsu getconf vim zsh git wget curl -y termux-api
 #pkg install nodejs golang python
 #pkg install tmux ttyd termux-services -y
-pkg install termux_services -y
+pkg install termux-services -y
 sv-enable sshd
 passwd
 #doom emacs
