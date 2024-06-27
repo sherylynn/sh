98cde3018cd85019178c1be116685a80184eb355
sherylynn <352281674@qq.com>
2024-06-27 11:38:23 +0800

    fix remove

--- termux/chroot/remove.sh
+++ termux/chroot/remove.sh
@@ -1,4 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+. $(dirname "$0")/../../win-git/toolsinit.sh
+. ./cli.sh
+
 ./unchroot.sh
 ./unchroot.sh
 ./unchroot.sh
-sudo rm -rf ~/Desktop/chrootdebian/
+
+#sudo rm -rf ~/Desktop/chrootdebian/
+sudo rm -rf $DEBIAN_DIR
