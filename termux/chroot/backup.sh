49b054c5397400c47cdca5471fc087f675c5a368
sherylynn <352281674@qq.com>
2024-06-28 23:22:07 +0800

    update

--- termux/chroot/backup.sh
+++ termux/chroot/backup.sh
@@ -15,7 +15,6 @@ fi
 echo "在外部termux中彻底进行备份"
 
 #sudo tar -zpcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap $DEBIAN_DIR
-
 #去掉了文件夹以可以在使用时进行备份
 sudo tar --use-compress-program="pigz -9" -pcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap --exclude=$DEBIAN_DIR/dev --exclude=$DEBIAN_DIR/proc --exclude=$DEBIAN_DIR/sys --exclude=$DEBIAN_DIR/sdcard --exclude=$DEBIAN_DIR/tmp $DEBIAN_DIR
 
