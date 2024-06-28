8c67134abafe0365940eb14655d0c9843c834f84
sherylynn <352281674@qq.com>
2024-06-28 23:14:23 +0800

    update

--- termux/chroot/backup.sh
+++ termux/chroot/backup.sh
@@ -5,6 +5,7 @@ apt install getconf tsu
 . ./cli.sh
 . ./unchroot.sh
 
+pkg install pigz -y
 cd ~
 if [[$(exist termux-setup-storage) == 1]];then
   termux-setup-storage
@@ -13,4 +14,8 @@ else
 fi
 echo "在外部termux中彻底进行备份"
 
-sudo tar -zpcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap $DEBIAN_DIR
+#sudo tar -zpcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap $DEBIAN_DIR
+
+#去掉了文件夹以可以在使用时进行备份
+sudo tar --use-compress-program="pigz -9" -pcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap --exclude=$DEBIAN_DIR/dev --exclude=$DEBIAN_DIR/proc --exclude=$DEBIAN_DIR/sys --exclude=$DEBIAN_DIR/sdcard --exclude=$DEBIAN_DIR/tmp $DEBIAN_DIR
+
