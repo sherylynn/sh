39f09b2a043c7836ba9e9cbb6de640f285688538
sherylynn <352281674@qq.com>
2024-06-28 20:37:25 +0800

    rime

--- termux/server_configure.sh
+++ termux/server_configure.sh
@@ -6,7 +6,7 @@ pkg install zsh -y
 zsh ~/sh/termux/vmos_android13.sh
 zsh ~/sh/termux/init.sh
 zsh ~/sh/termux/emacs.sh
-zsh ~/sh/termux/myemacs.sh
+zsh ~/sh/myemacs.sh
 zsh ~/sh/termux/emacs_native.sh 
 zsh ~/sh/termux/ttyd.sh
 zsh ~/sh/win-git/zlua_new.sh 
