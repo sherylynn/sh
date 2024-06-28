e7a1ba600c3cfb29b8c7cfc08f16b01b64353e29
sherylynn <352281674@qq.com>
2024-06-28 10:27:41 +0800

    update

---
+++ termux/chroot/backup.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#------------------init function----------------
+. $(dirname "$0")/../../win-get/toolsinit.sh
+. ./cli.sh
+cd ~
+mkdir -p ~/download
+#tar -zpcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache ./*
+#tar -pcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache -I pigz ./*
+
+#tar --use-compress-program="pigz -9" -pcvf ~/download/backup.tar.gz --exclude=download --exclude=.cache  ./*
+#./* 会没有隐藏文件
+#tar --exclude=download --exclude=.cache  -pcvf - ./*  |pigz --best > ~/download/backup.tar.gz
+#tar --exclude=.dbus --exclude=.config --exclude=download --exclude=.x* --exclude=.vnc* --exclude=.cache  -pcvf - ./  |pigz --best > ~/download/backup.tar.gz
+
+tar --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config/chromium --exclude=.config/gtk-3.0 --exclude=.config/pulse --exclude=.config/dconf --exclude=.config/htop  --exclude=download --exclude=.x* --exclude=.vnc* --exclude=.cache  -pcvf - ./  |pigz --best > ~/download/backup_$(arch).tar.gz
