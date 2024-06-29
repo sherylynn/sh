f76b866f14855201040c1ffbcba65c50715ad10e
sherylynn <352281674@qq.com>
2024-06-29 14:17:26 +0800

    确保文件读写权限存在

--- termux/chroot/recovery.sh
+++ termux/chroot/recovery.sh
@@ -3,6 +3,11 @@
 . ./cli.sh
 #. ./unchroot.sh
 . ./remove.sh
+if [[$(exist termux-setup-storage) == 1]];then
+  termux-setup-storage
+else
+  mkdir -p ~/storage/downloads
+fi
 cd /
 pkg install pigz -y
 sudo tar -I pigz -xvf ~/storage/downloads/backup_chroot_$(arch).tar.gz
