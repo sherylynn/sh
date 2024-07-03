67d3319be8a745110131e029d9acb66fec4bf630
sherylynn <352281674@qq.com>
2024-07-03 11:40:14 +0800

    fix deps

--- termux/emacs.sh
+++ termux/emacs.sh
@@ -5,4 +5,10 @@ if [ ! -f "~/.emacs.d/elpa/vterm-2*/vterm-module.so"]; then
     echo "need to download vterm and rime build deps"
     pkg install cmake build-essential wget tsu -y
 fi
+
+if [ ! -f "~/.emacs.d/elpa/rime-2*/librime-emacs.so"]; then
+    echo "need to download vterm and rime build deps"
+    pkg install cmake build-essential wget tsu -y
+fi
+
 pkg install librime libvterm -y
