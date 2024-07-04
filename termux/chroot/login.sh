4c14216d869536fa4fe341a75e8792c0ca416bd5
sherylynn <352281674@qq.com>
2024-07-05 01:50:56 +0800

    add login.sh for chroot

---
+++ termux/chroot/login.sh
@@ -0,0 +1,32 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+. ./cli.sh
+# Kill all old prcoesses
+sudo killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
+
+
+# Execute chroot script
+container_mounted || container_mount
+#before_mount_fun
+
+termux_data_path=/data/data/com.termux/files/home
+termux_gitcredentials=$termux_data_path/.git-credentials
+termux_gitconfig=$termux_data_path/.gitconfig
+
+test -f  $termux_gitconfig && sudo cp $termux_gitconfig $CHROOT_DIR/root/
+test -f  $termux_gitcredentials && sudo cp $termux_gitcredentials $CHROOT_DIR/root/
+
+unset LD_PRELOAD LD_DEBUG
+sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
+export GTK_IM_MODULE="fcitx" && \
+export QT_IM_MODULE="fcitx" && \
+export XMODIFIERS="@im=fcitx" && \
+#fcitx5 && \
+export GALLIUM_DRIVER=virpipe && \
+export MESA_GL_VERSION_OVERRIDE=4.0 && \
+zsh ~/tools/rc/allToolsrc; \
+zsh '
+#dbus-launch --exit-with-session startxfce4'
+#startxfce4'
+#vncserver -kill :0 && \
+#rm -rf /tmp/.X* && \
