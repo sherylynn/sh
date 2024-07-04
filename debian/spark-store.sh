a8f24a1d0d77459b4d0fa611e217073a373ef44c
sherylynn <352281674@qq.com>
2024-07-05 00:25:32 +0800

    spark-store to cmd version

--- debian/spark-store.sh
+++ debian/spark-store.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 . $(dirname "$0")/../win-git/toolsinit.sh
-
 mirrors=https://gitee.com/spark-store-project/spark-store/releases/download/4.2.13.1/
 case $(arch) in 
   amd64) SOFT_ARCH=amd64;;
@@ -11,7 +10,10 @@ case $(arch) in
     ;;
 esac
 
-lib_name=spark-store_4.2.13.1_$SOFT_ARCH.deb
+#直接安装商店真的很卡，不如安装终端版本，连图标都不用费心了
+#lib_name=spark-store_4.2.13.1_$SOFT_ARCH.deb
+#还方便自动化
+lib_name=spark-store-console_4.2.12_all.deb
 lib_url=$mirrors$lib_name
 proxy
 $(cache_downloader $lib_name $lib_url)
