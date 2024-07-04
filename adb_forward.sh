5124ea2c14e1ea220657541fc6d068236be30b87
sherylynn <352281674@qq.com>
2024-07-04 12:30:23 +0000

    add adb_forward for linux

---
+++ adb_forward.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+adb forward tcp:10086 tcp:10086
+adb forward tcp:10000 tcp:10000
+adb forward tcp:10808 tcp:10808
+adb forward tcp:3000 tcp:3000
+adb forward tcp:3333 tcp:3333
+#adb forward tcp:5555 tcp:5555
+#adb forward tcp:5900 tcp:5900
+#adb forward tcp:5244 tcp:5244
+adb forward tcp:8022 tcp:8022
+#adb forward tcp:1122 tcp:1122
