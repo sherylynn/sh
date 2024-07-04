2d45bde6ee6a6a8f99dd6a745ea5915a5facfaff
sherylynn <352281674@qq.com>
2024-07-05 00:26:50 +0800

    update

--- debian/spark-store.sh
+++ debian/spark-store.sh
@@ -12,7 +12,7 @@ esac
 
 #直接安装商店真的很卡，不如安装终端版本，连图标都不用费心了
 #lib_name=spark-store_4.2.13.1_$SOFT_ARCH.deb
-#还方便自动化
+#还方便自动化, 商店版本chroot时候的gpu用virgl有问题，只能用pipe来算
 lib_name=spark-store-console_4.2.12_all.deb
 lib_url=$mirrors$lib_name
 proxy
