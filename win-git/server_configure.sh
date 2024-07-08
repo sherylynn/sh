41be71baea7bff9ea76a0043717560323abe2a63
sherylynn <352281674@qq.com>
2024-07-08 23:28:34 +0800

    add koreader for debian install

--- win-git/server_configure.sh
+++ win-git/server_configure.sh
@@ -43,6 +43,10 @@ zsh ~/sh/win-git/move2zsh.sh
 zsh ~/sh/win-git/zlua_new.sh
 zsh ~/sh/win-git/init_d_noVNC.sh 
 zsh ~/sh/win-git/noVNC.sh 
+zsh ~/sh/win-git/koreader.sh 
+if [ -d "/sdcard" ]; then
+    sudo ln -s /sdcard/Download/BaiduNetdisk/_pcs_.workspace/ /root/Documents/百度云盘
+fi
 #zsh ~/sh/debian/wps.sh 
 zsh ~/sh/debian/firefox.sh
 #确保toolsrc正确
