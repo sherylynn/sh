5419f01387a27da58802ab97e8c0a94ab4c1e742
sherylynn <352281674@qq.com>
2024-07-09 23:31:25 +0800

    add rsync

---
+++ termux/chroot/sync.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#------------------init function----------------
+apt install getconf tsu pigz -y
+. $(dirname "$0")/../../win-get/toolsinit.sh
+. ./cli.sh
+. ./unchroot.sh
+
+cd ~
+if [ ! -d "~/storage/downloads"]; then
+  termux-setup-storage
+fi
+rsync -rvz -e 'ssh -p 2222' --progress ./dir user@host:/path
+echo "在外部 termux 中彻底进行备份"
+
+#sudo tar -zpcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap $DEBIAN_DIR
+#去掉了文件夹以可以在使用时进行备份
+sudo tar --use-compress-program="pigz -9" -pcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap --exclude=$DEBIAN_DIR/dev --exclude=$DEBIAN_DIR/proc --exclude=$DEBIAN_DIR/sys --exclude=$DEBIAN_DIR/sdcard --exclude=$DEBIAN_DIR/tmp $DEBIAN_DIR
+
