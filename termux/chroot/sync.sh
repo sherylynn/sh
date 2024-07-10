4977415f2226109b4e77c3f84bc93171f209695b
sherylynn <352281674@qq.com>
2024-07-10 14:44:58 +0800

    update rsync

--- termux/chroot/sync.sh
+++ termux/chroot/sync.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #------------------init function----------------
-apt install getconf tsu pigz -y
+target_host=@1
+apt install getconf tsu pigz rsync -y
 . $(dirname "$0")/../../win-get/toolsinit.sh
 . ./cli.sh
 . ./unchroot.sh
@@ -9,10 +10,10 @@ cd ~
 if [ ! -d "~/storage/downloads"]; then
   termux-setup-storage
 fi
-rsync -rvz -e 'ssh -p 2222' --progress ./dir user@host:/path
 echo "在外部 termux 中彻底进行备份"
+rsync -rvz -e 'ssh -p 2222' --progress ${DEBIAN_DIR} root@${target_host}:${DEBIAN_DIR}
 
 #sudo tar -zpcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap $DEBIAN_DIR
 #去掉了文件夹以可以在使用时进行备份
-sudo tar --use-compress-program="pigz -9" -pcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap --exclude=$DEBIAN_DIR/dev --exclude=$DEBIAN_DIR/proc --exclude=$DEBIAN_DIR/sys --exclude=$DEBIAN_DIR/sdcard --exclude=$DEBIAN_DIR/tmp $DEBIAN_DIR
+#sudo tar --use-compress-program="pigz -9" -pcvf ~/storage/downloads/backup_chroot_$(arch).tar.gz --exclude=~/storage --exclude=swap --exclude=$DEBIAN_DIR/dev --exclude=$DEBIAN_DIR/proc --exclude=$DEBIAN_DIR/sys --exclude=$DEBIAN_DIR/sdcard --exclude=$DEBIAN_DIR/tmp $DEBIAN_DIR
 
