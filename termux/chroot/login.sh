f065071b8b70dab3e7e978c16456d949c99cd6d4
sherylynn <352281674@qq.com>
2024-07-14 14:21:17 +0800

    change login.sh for root emacsclient

--- termux/chroot/login.sh
+++ termux/chroot/login.sh
@@ -4,7 +4,6 @@
 # Kill all old prcoesses
 sudo killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
 
-
 # Execute chroot script
 container_mounted || container_mount
 #before_mount_fun
@@ -13,11 +12,12 @@ termux_data_path=/data/data/com.termux/files/home
 termux_gitcredentials=$termux_data_path/.git-credentials
 termux_gitconfig=$termux_data_path/.gitconfig
 
-test -f  $termux_gitconfig && sudo cp $termux_gitconfig $CHROOT_DIR/root/
-test -f  $termux_gitcredentials && sudo cp $termux_gitcredentials $CHROOT_DIR/root/
+test -f $termux_gitconfig && sudo cp $termux_gitconfig $CHROOT_DIR/root/
+test -f $termux_gitcredentials && sudo cp $termux_gitcredentials $CHROOT_DIR/root/
 
 unset LD_PRELOAD LD_DEBUG
-sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
+#sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
+sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export PULSE_SERVER=127.0.0.1 && \
 export GTK_IM_MODULE="fcitx" && \
 export QT_IM_MODULE="fcitx" && \
 export XMODIFIERS="@im=fcitx" && \
