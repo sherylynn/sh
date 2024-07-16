f40ed0d551e6b24aef1de48a44a3b0fce6df9b80
sherylynn <352281674@qq.com>
2024-07-16 13:20:38 +0800

    give up emacsd

--- termux/emacs.sh
+++ termux/emacs.sh
@@ -1,17 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install emacs -y
-sv-enable emacsd
+#sv-enable emacsd
+#sv up emacsd
 
 #touch ~/hello_emacs.sh
 #pkg install gh fd ripgrep -y
 if [ ! -f "~/.emacs.d/elpa/vterm-2*/vterm-module.so" ]; then
-    echo "need to download vterm and rime build deps"
-    pkg install cmake build-essential wget tsu -y
+  echo "need to download vterm and rime build deps"
+  pkg install cmake build-essential wget tsu -y
 fi
 
 if [ ! -f "~/.emacs.d/elpa/rime-2*/librime-emacs.so" ]; then
-    echo "need to download vterm and rime build deps"
-    pkg install cmake build-essential wget tsu -y
+  echo "need to download vterm and rime build deps"
+  pkg install cmake build-essential wget tsu -y
 fi
 
 pkg install librime libvterm -y
