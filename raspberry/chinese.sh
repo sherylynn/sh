093c34396cfcb250bb4af0cead5cb30367c4d267
sherylynn <352281674@qq.com>
2024-10-17 01:17:56 +0800

    fix locales for r-base

--- raspberry/chinese.sh
+++ raspberry/chinese.sh
@@ -12,6 +12,7 @@ sudo apt install fcitx5 fcitx5-rime fonts-wqy-zenhei ttf-wqy-zenhei -y
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sudo apt install locales -y
 sudo locale-gen zh.CN.UTF-8
+sudo dpkg-reconfigure locales
 
 tee ${TOOLSRC} <<-'EOF'
 export LANG=zh_CN.UTF-8
