5902647768f9e47b774b5a246c672f9d1718b0fb
sherylynn <352281674@qq.com>
2024-10-20 10:54:14 +0800

    update

--- opkg/adb_forward.sh
+++ opkg/adb_forward.sh
@@ -3,9 +3,8 @@ opkg install iptables-mod-nat-extra
 opkg install adb
 #opkg install curl
 
-
 # Save connectivity checking script
-cat << "EOF" > /root/adb-watchdog.sh
+cat <<"EOF" >/root/adb-watchdog.sh
 #!/bin/sh
 adb -a forward tcp:10086 tcp:10086
 adb -a forward tcp:10000 tcp:10000
@@ -19,6 +18,8 @@ adb -a forward tcp:8022 tcp:8022
 adb -a forward tcp:1122 tcp:1122
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.route_localnet=1
+#清空原有规则以免重复
+iptables -t nat -F
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10000 -j DNAT --to-destination 127.0.0.1:10000
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destination 127.0.0.1:10808
@@ -28,6 +29,7 @@ iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 3000 -j DNAT --to-destina
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 3333 -j DNAT --to-destination 127.0.0.1:3333
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 8022 -j DNAT --to-destination 127.0.0.1:8022
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 1122 -j DNAT --to-destination 127.0.0.1:1122
+iptables-nft -t nat -F
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10086 -j DNAT --to-destination 127.0.0.1:10086
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10000 -j DNAT --to-destination 127.0.0.1:10000
 iptables-nft -t nat -A PREROUTING -i br-lan -p tcp --dport 10808 -j DNAT --to-destination 127.0.0.1:10808
@@ -83,11 +85,8 @@ else
 fi
 EOF
 chmod +x /root/adb-watchdog.sh
- 
+
 # Add cron job
-cat << "EOF" >> /etc/crontabs/root
+cat <<"EOF" >>/etc/crontabs/root
 * * * * * /root/adb-watchdog.sh
 EOF
-
-
-
