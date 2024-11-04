4a223cb7ca5384cf7281f6eb12d1a98bf985c31c
sherylynn <352281674@qq.com>
2024-11-04 13:12:51 +0800

    update kms

--- kms.sh
+++ kms.sh
@@ -1,5 +1,6 @@
 #git clone https://github.com/ThunderEX/py-kms.git ~/py-kms
-git clone https://github.com/SystemRage/py-kms ~/py-kms
+#git clone https://github.com/SystemRage/py-kms ~/py-kms
+git clone https://github.com/Py-KMS-Organization/py-kms ~/py-kms
 cd ~/py-kms
 #python server.py 0.0.0.0 1688
 python3 py-kms/pykms_Server.py 0.0.0.0 1688
