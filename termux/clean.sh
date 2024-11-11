fd7c8a355494d79a8eff2887e4685e5de9d4cb62
sherylynn <352281674@qq.com>
2024-11-11 22:06:48 +0800

    update

--- termux/clean.sh
+++ termux/clean.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 . $(dirname "$0")/../win-git/toolsinit.sh
-apt remove build-essential cmake -y
+#影响treesit, treesit需要随时用clang编译,不适合删除
+#apt remove build-essential cmake -y
 apt autoremove -y
 apt clean
 #可能删掉字体，不删了
