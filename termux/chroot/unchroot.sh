3ff2c2fc90f586f0eb24806c8880872838cecc0f
sherylynn <352281674@qq.com>
2024-11-23 21:05:27 +0800

    update

--- termux/chroot/unchroot.sh
+++ termux/chroot/unchroot.sh
@@ -6,10 +6,14 @@
 # Kill all old prcoesses for umount tmp
 sudo killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
 
-unset LD_PRELOAD LD_DEBUG
-stop_dbus
-stop_vnc
-stop_init
+if [ -e "$busybox" ]; then
+  unset LD_PRELOAD LD_DEBUG
+  stop_dbus
+  stop_vnc
+  stop_init
 
-container_umount
+  container_umount
 #after_umount_fun
+else
+  sudo ruri -U ./test
+fi
