4001c88c15cfcbc9777b7cf8f08fca509023471b
sherylynn <352281674@qq.com>
2024-11-26 05:30:48 +0800

    [2024-11-26 Tue 05:30]

--- termux/chroot/cli.sh
+++ termux/chroot/cli.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #kernelsu busybox
-test -f /data/adb/ksu/bin/busybox && busybox=/data/adb/ksu/bin/busybox
+sudo test -f /data/adb/ksu/bin/busybox && busybox=/data/adb/ksu/bin/busybox
 #apatch busybox
-test -f /data/adb/ap/bin/busybox && busybox=/data/adb/ap/bin/busybox
+sudo test -f /data/adb/ap/bin/busybox && busybox=/data/adb/ap/bin/busybox
 #for run in native emacs
 PREFIX=/data/data/com.termux/files/usr
 
