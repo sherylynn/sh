e547de8dab522cfa5ab4d3cd0e3f064954364473
sherylynn <352281674@qq.com>
2024-11-26 13:41:20 +0800

    update

--- win-git/scrcpy.sh
+++ win-git/scrcpy.sh
@@ -12,32 +12,58 @@ SOFT_ARCH=64
 # uname Linux .bashrc uname Darwin MINGW64 .bash_profile
 PLATFORM=$(platform)
 if [[ $(platform) == *linux* ]]; then
-  #deb apt
-  sudo apt install -y ffmpeg libsdl2-2.0-0 adb wget \
-                 gcc git pkg-config meson ninja-build libsdl2-dev \
-                 libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
-                 libusb-1.0-0 libusb-1.0-0-dev
-  cd $(install_path)
-  git clone https://github.com/${AUTHOR}/${NAME} $SOFT_HOME
-  cd $SOFT_HOME
-  git pull
-  #prebuilt server
-  SOFT_URL=https://github.com/Genymobile/${NAME}/releases/download/${SOFT_VERSION}/${NAME}-server-${SOFT_VERSION}
-  SOFT_FILE_NAME=${NAME}-server-${SOFT_VERSION}
-  $(cache_downloader $SOFT_FILE_NAME $SOFT_URL)
-  cp $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}/
-  #build
-  meson x --buildtype release --strip -Db_lto=true \
+  case $(arch) in
+    amd64) SOFT_ARCH=64 ;;
+    386) SOFT_ARCH=32 ;;
+  esac
+
+  if [[ $SOFT_ARCH == *64* ]]; then
+    SOFT_FILE_NAME=${NAME}-${PLATFORM}${SOFT_ARCH}-${SOFT_VERSION}
+    SOFT_FILE_PACK=$(soft_file_pack $SOFT_FILE_NAME)
+    # init pwd
+    cd $HOME
+
+    SOFT_URL=https://github.com/Genymobile/${NAME}/releases/download/${SOFT_VERSION}/${SOFT_FILE_PACK}
+    #if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
+    if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
+      $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+      $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
+
+      rm -rf ${SOFT_HOME} &&
+        mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}
+    fi
+    #--------------new .toolsrc-----------------------
+    export PATH=$PATH:${SOFT_HOME}
+
+    echo 'export PATH=$PATH:'${SOFT_HOME} >>${TOOLSRC}
+  else
+    #deb apt
+    sudo apt install -y ffmpeg libsdl2-2.0-0 adb wget \
+      gcc git pkg-config meson ninja-build libsdl2-dev \
+      libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
+      libusb-1.0-0 libusb-1.0-0-dev
+    cd $(install_path)
+    git clone https://github.com/${AUTHOR}/${NAME} $SOFT_HOME
+    cd $SOFT_HOME
+    git pull
+    #prebuilt server
+    SOFT_URL=https://github.com/Genymobile/${NAME}/releases/download/${SOFT_VERSION}/${NAME}-server-${SOFT_VERSION}
+    SOFT_FILE_NAME=${NAME}-server-${SOFT_VERSION}
+    $(cache_downloader $SOFT_FILE_NAME $SOFT_URL)
+    cp $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}/
+    #build
+    meson x --buildtype release --strip -Db_lto=true \
       -Dprebuilt_server=${SOFT_FILE_NAME}
-  ninja -Cx
-  #install
-  #sudo ninja -Cx install
-  echo 'alias scrcpy="'${SOFT_HOME}/run ${SOFT_HOME}/x'"'>${TOOLSRC}
+    ninja -Cx
+    #install
+    #sudo ninja -Cx install
+    echo 'alias scrcpy="'${SOFT_HOME}/run ${SOFT_HOME}/x'"' >${TOOLSRC}
+  fi
 fi
 if [[ $(platform) == *win* ]]; then
-  case $(arch) in 
-    amd64) SOFT_ARCH=64;;
-    386) SOFT_ARCH=32;;
+  case $(arch) in
+    amd64) SOFT_ARCH=64 ;;
+    386) SOFT_ARCH=32 ;;
   esac
 
   SOFT_FILE_NAME=${NAME}-${PLATFORM}${SOFT_ARCH}-${SOFT_VERSION}
@@ -50,19 +76,19 @@ if [[ $(platform) == *win* ]]; then
   if [[ "$(${NAME} --version)" != *${NAME}\ ${SOFT_VERSION}* ]]; then
     $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
     $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
-    
-    rm -rf ${SOFT_HOME} && \
-      mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME} 
+
+    rm -rf ${SOFT_HOME} &&
+      mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}
   fi
   #--------------new .toolsrc-----------------------
   export PATH=$PATH:${SOFT_HOME}
 
-  echo 'export PATH=$PATH:'${SOFT_HOME}>>${TOOLSRC}
+  echo 'export PATH=$PATH:'${SOFT_HOME} >>${TOOLSRC}
 
   #  ----windows bat----
-  if [[ $WIN_PATH  ]]; then
+  if [[ $WIN_PATH ]]; then
     if [[ $PLATFORM == windows ]]; then
-      windowsENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
+      windowsENV="$(echo -e ${PATH//:/;\\n}';' | sort | uniq | cygpath -w -f - | tr -d '\n')"
       echo $windowsENV
       setx Path "$windowsENV"
     fi
