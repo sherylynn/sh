3bca9b330acad932e718bf4e27818d9d11f2de97
sherylynn <352281674@qq.com>
2024-12-04 21:04:19 +0800

    update noVNC for ruri

--- termux/chroot/noVNC.sh
+++ termux/chroot/noVNC.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-. ./cli.sh
+. $(dirname "$0")/cli.sh
 # Kill all old prcoesses
 sudo killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
 
@@ -20,21 +20,42 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 # Start virgl server
 virgl_test_server_android &
 
-# Execute chroot script
-container_mounted || container_mount
-#before_mount_fun
+if [ -f ~/tools/rurima/rurima ]; then
 
-termux_data_path=/data/data/com.termux/files/home
-termux_gitcredentials=$termux_data_path/.git-credentials
-termux_gitconfig=$termux_data_path/.gitconfig
+  sudo mount -o remount,dev,suid /data
+  #sudo mount -o remount,suid /data
+  #挂载sdcard隐私文件
+  sdcard_link
+  #解除挂载
+  sudo rurima ruri -U $DEBIAN_DIR
+  #挂载
+  #sudo $busybox mount --bind $PREFIX/tmp $CHROOT_DIR/tmp
+  unset LD_PRELOAD LD_DEBUG
+  sudo rurima ruri -S -m /data/data/com.termux/files/usr/tmp /tmp -m /sdcard /sdcard -p $DEBIAN_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
+export GTK_IM_MODULE="fcitx" && \
+export QT_IM_MODULE="fcitx" && \
+export XMODIFIERS="@im=fcitx" && \
+#fcitx5 && \
+export GALLIUM_DRIVER=virpipe && \
+export MESA_GL_VERSION_OVERRIDE=4.0 && \
+cd ~ && \
+sh ~/sh/win-git/server_noVNC.sh'
+elif [ -n "$busybox" ]; then
+  # Execute chroot script
+  container_mounted || container_mount
+  #before_mount_fun
 
-test -f $termux_gitconfig && sudo cp $termux_gitconfig $CHROOT_DIR/root/
-test -f $termux_gitcredentials && sudo cp $termux_gitcredentials $CHROOT_DIR/root/
+  termux_data_path=/data/data/com.termux/files/home
+  termux_gitcredentials=$termux_data_path/.git-credentials
+  termux_gitconfig=$termux_data_path/.gitconfig
 
-unset LD_PRELOAD LD_DEBUG
-start_dbus
-#start_vnc
-sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
+  test -f $termux_gitconfig && sudo cp $termux_gitconfig $CHROOT_DIR/root/
+  test -f $termux_gitcredentials && sudo cp $termux_gitcredentials $CHROOT_DIR/root/
+
+  unset LD_PRELOAD LD_DEBUG
+  start_dbus
+  #start_vnc
+  sudo $busybox chroot $CHROOT_DIR /bin/su - root -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && \
 export GTK_IM_MODULE="fcitx" && \
 export QT_IM_MODULE="fcitx" && \
 export XMODIFIERS="@im=fcitx" && \
@@ -44,3 +65,5 @@ export MESA_GL_VERSION_OVERRIDE=4.0 && \
 cd ~ && \
 sh ~/sh/win-git/server_noVNC.sh'
 #dbus-launch --exit-with-session zsh ~/sh/win-git/server_noVNC.sh'
+
+fi
