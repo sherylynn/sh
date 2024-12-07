20a76086c2a9bc8b978f7b95d9f104971e05b41f
sherylynn <352281674@qq.com>
2024-12-07 23:08:48 +0800

    add ollama

---
+++ win-git/ollama.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+. $(dirname "$0")/toolsinit.sh
+AUTHOR=ollama
+NAME=ollama
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+SOFT_HOME=$(install_path)/${NAME}
+
+SOFT_VERSION=$(get_github_release_version $AUTHOR/$NAME)
+#SOFT_ARCH=amd64
+SOFT_ARCH=arm64
+
+case $(platform) in
+  macos) PLATFORM=darwin ;;
+  win) PLATFORM=windows ;;
+  linux) PLATFORM=linux ;;
+esac
+
+case $(arch) in
+  amd64) SOFT_ARCH=x86_64 ;;
+  386) SOFT_ARCH=32-bit ;;
+  armhf) SOFT_ARCH=ARM_v6 ;;
+  aarch64) SOFT_ARCH=aarch64 ;;
+esac
+
+SOFT_FILE_NAME=${NAME}-${PLATFORM}-${SOFT_ARCH}
+#SOFT_FILE_NAME=${SOFT_ARCH}
+#SOFT_FILE_PACK=$(soft_file_pack $SOFT_FILE_NAME)
+SOFT_FILE_PACK=$SOFT_FILE_NAME.tgz
+COMMAND_NAME=$SOFT_FILE_NAME
+# init pwd
+cd $HOME
+
+#https://github.com/ollama/ollama/releases/download/v0.5.1/ollama-linux-arm64.tgz
+SOFT_URL=https://github.com/${AUTHOR}/${NAME}/releases/download/${SOFT_VERSION}/${SOFT_FILE_PACK}
+
+if [[ "$(${NAME} -v)" != *${SOFT_VERSION}* ]]; then
+  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+  $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
+
+  rm -rf ${SOFT_HOME} &&
+    mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}
+fi
+
+SOFT_ROOT=${SOFT_HOME}
+
+export PATH=$PATH:${SOFT_ROOT}
+echo 'export PATH=$PATH:'${SOFT_ROOT} >${TOOLSRC}
