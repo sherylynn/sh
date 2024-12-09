4098283385c874111b32a6f06f1b024e148a772b
sherylynn <352281674@qq.com>
2024-12-09 17:16:26 +0800

    update script

--- debian/todesk.sh
+++ debian/todesk.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 . $(dirname "$0")/../win-git/toolsinit.sh
 
-#话说链接不带参数会被禁止，感觉不好用
-#wps_url=https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/11719/wps-office_11.1.0.11719_arm64.deb?t=1718868135&k=d1c05d330d0cb18c0bb9616b6402e2c9
+NAME=todesk
+SOFT_VERSION=v4.7.2.0
 
 case $(arch) in
-  amd64) SOFT_ARCH=x64 ;;
+  amd64)
+    SOFT_ARCH=amd64
+    ;;
   386) SOFT_ARCH=x86 ;;
   armhf) SOFT_ARCH=armv7l ;;
   aarch64)
-    todesk_url=https://dl.todesk.com/linux/todesk-v4.7.2.0-arm64.deb
-    todesk_name=todesk-v4.7.2.0-arm64.deb
+    SOFT_ARCH=arm64
     ;;
 esac
 
+SOFT_URL=https://dl.todesk.com/linux/${NAME}-${SOFT_VERSION}-${SOFT_ARCH}.deb
+SOFT_NAME=${NAME}-${SOFT_VERSION}-${SOFT_ARCH}.deb
 #不再使用固定版本，使用源自带版本
-$(cache_downloader $todesk_name $todesk_url)
+$(cache_downloader $SOFT_NAME $SOFT_URL)
 
 #解决todesk的问题
 sudo apt-get install libappindicator3-1
-sudo dpkg -i $(cache_folder)/$todesk_name
+sudo dpkg -i $(cache_folder)/$SOFT_NAME
--- debian/wechat.sh
+++ debian/wechat.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 . $(dirname "$0")/../win-git/toolsinit.sh
 
+NAME=WeChatLinux
+
 case $(arch) in
-  amd64) SOFT_ARCH=x64 ;;
+  amd64)
+    SOFT_ARCH=x86_64
+    ;;
   386) SOFT_ARCH=x86 ;;
   armhf) SOFT_ARCH=armv7l ;;
   aarch64)
-    wechat_url=https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_arm64.deb
-    wechat_name=WeChatLinux_arm64.deb
+    SOFT_ARCH=arm64
     ;;
 esac
 
-#不再使用固定版本，使用源自带版本
-$(cache_downloader $wechat_name $wechat_url)
+SOFT_URL=https://dldir1v6.qq.com/weixin/Universal/Linux/${NAME}_${SOFT_ARCH}.deb
+SOFT_NAME=${NAME}_${SOFT_ARCH}.deb
 
+#不再使用固定版本，使用源自带版本
+$(cache_downloader $SOFT_NAME $SOFT_URL)
 #解决微信的问题
-sudo dpkg -i $(cache_folder)/$wechat_name
+sudo dpkg -i $(cache_folder)/$SOFT_NAME
 sudo apt install libtiff6 -y
 sudo ln -s /usr/lib/aarch64-linux-gnu/libtiff.so.6 /usr/lib/aarch64-linux-gnu/libtiff.so.5
