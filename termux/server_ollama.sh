d751c7a7c57c3c02afee0b929231067e7411b7cf
sherylynn <352281674@qq.com>
2024-12-11 08:48:17 +0800

    add termux service for ollama

---
+++ termux/server_ollama.sh
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#change bash from /usr/bin/bash to realpath
+realpath() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )/$x
+
+}
+realpathdir() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )
+
+}
+cd $(realpathdir ./server_code-server.sh)
+pwd
+#load env
+test -f ../../tools/rc/ttydrc && . ../../tools/rc/ttydrc
+
+echo $(whoami)
+# login need systemd user root
+#ttyd -p 3000 -t fontSize=18 login
+# login need systemd user $(whoami)
+#su $(whoami) -c 'ttyd -p 3000 -t fontSize=18 ssh localhost'
+ollama serve
