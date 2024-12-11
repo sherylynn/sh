129c10dbc0dfbdc34abf29884c1f4b7e2ae4c34c
sherylynn <352281674@qq.com>
2024-12-11 15:35:29 +0800

    add llama.cpp for termux

---
+++ termux/llama.cpp.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+. $(dirname "$0")/../win-git/toolsinit.sh
+AUTHOR=ggerganov
+NAME=llama.cpp
+TOOLSRC_NAME=${NAME}rc
+TOOLSRC=$(toolsRC ${TOOLSRC_NAME})
+SOFT_HOME=$(install_path)/${NAME}
+SOFT_VERSION=$(get_github_release_version $AUTHOR/$NAME)
+echo "soft version is $SOFT_VERSION"
+
+case $(platform) in
+  macos) PLATFORM=darwin ;;
+  win) PLATFORM=windows ;;
+  linux) PLATFORM=linux ;;
+esac
+
+case $(arch) in
+  amd64) SOFT_ARCH=x86_64 ;;
+  386) SOFT_ARCH=32-bit ;;
+  armhf) SOFT_ARCH=ARM_v6 ;;
+  aarch64) SOFT_ARCH=arm64 ;;
+esac
+
+# uname Linux .bashrc uname Darwin MINGW64 .bash_profile
+
+SOFT_GIT_URL=https://github.com/${AUTHOR}/${NAME}
+
+if [[ $(platform) == *linux* ]]; then
+  #  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
+  pkg install git cmake -y
+
+  git clone ${SOFT_GIT_URL} ${SOFT_HOME}
+  #  rm -rf ${SOFT_HOME} && mkdir -p ${SOFT_HOME}
+  #  cp $(cache_folder)/${SOFT_FILE_PACK} ${SOFT_HOME}/${SOFT_FILE_NAME}
+  #  chmod 777 ${SOFT_HOME}/${SOFT_FILE_NAME}
+  cd ${SOFT_HOME}
+  cmake -B build -DBUILD_SHARED_LIBS=OFF
+  cmake --build build --config Release -j $(nproc)
+  SOFT_ROOT=$(install_path)/${NAME}/build/bin
+  echo "export PATH=$SOFT_ROOT:"'$PATH' >${TOOLSRC}
+  #echo "export PATH=$SOFT_HOME:"'$PATH' >${TOOLSRC}
+
+  #zsh ~/sh/termux/termux_service_${NAME}.sh
+  #sh ~/sh/termux/termux_service_${NAME}.sh
+  #sv-enable ${NAME}
+#  ./systemd_novnc.sh
+fi
