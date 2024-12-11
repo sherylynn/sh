50f3bb91b3a31a89533b5ef7dba9151970fb80ad
sherylynn <352281674@qq.com>
2024-12-11 21:57:25 +0800

    update

---
+++ win-git/server_llama.cpp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SCRIPT_NAME="llama.cpp"
+#change bash from /usr/bin/bash to realpath
+realpath() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )/$x
+
+}
+realpathdir() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )
+
+}
+cd $(realpathdir ./server_${SCRIPT_NAME}.sh)
+pwd
+#load env
+test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
+
+echo $(whoami)
+# login need systemd user root
+#ttyd -p 3000 -t fontSize=18 login
+# login need systemd user $(whoami)
+
+#cd ../../tools/noVNC
+cd ../../
+#su $(whoami) -c 'ttyd -p 3000 -t fontSize=18 ssh localhost'
+llama-server -m /sdcard/Download/Qwen2.5.1-Coder-7B-Instruct-Q4_0_4_8.gguf --host 0.0.0.0
