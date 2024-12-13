4d255a5b389960710feffc2f55790fcc4f342beb
sherylynn <352281674@qq.com>
2024-12-13 16:26:37 +0800

    update version

--- win-git/go.sh
+++ win-git/go.sh
@@ -9,39 +9,45 @@ GO_PROXY=https://goproxy.cn
 GO_PATH=$(install_path)/gopath
 GO_PATH_BIN=${GO_PATH}/bin
 GO_ROOT_BIN=$GO_ROOT/bin
-GO_VERSION=1.19
+GO_VERSION=1.23.4
+#GO_VERSION=1.19
 SOFT_ARCH=amd64
 #SOFT_ARCH=arm64
 #SOFT_ARCH=armv6l
 #arm64
 
 # uname Linux .bashrc uname Darwin MINGW64 .bash_profile
-case $(platform) in 
-  win) PLATFORM=windows;;
-  linux) PLATFORM=linux;;
-  wslinux) PLATFORM=linux;;
-  macos) PLATFORM=darwin;;
+case $(platform) in
+  win) PLATFORM=windows ;;
+  linux) PLATFORM=linux ;;
+  wslinux) PLATFORM=linux ;;
+  macos) PLATFORM=darwin ;;
 esac
 
-case $(arch) in 
-  amd64) SOFT_ARCH=amd64;;
-  386) SOFT_ARCH=386;;
-  armhf) SOFT_ARCH=armv6l;;
-  aarch64) SOFT_ARCH=arm64;;
+case $(arch) in
+  amd64) SOFT_ARCH=amd64 ;;
+  386) SOFT_ARCH=386 ;;
+  armhf) SOFT_ARCH=armv6l ;;
+  aarch64) SOFT_ARCH=arm64 ;;
 esac
 
 while getopts 'v:a:sc' OPT; do
   case $OPT in
     v)
-      GO_VERSION="$OPTARG";;
+      GO_VERSION="$OPTARG"
+      ;;
     a)
-      SOFT_ARCH="$OPTARG";;
+      SOFT_ARCH="$OPTARG"
+      ;;
     s)
-      Server="y";;
+      Server="y"
+      ;;
     c)
-      Client="y";;
+      Client="y"
+      ;;
     ?)
-      echo "Usage: `basename $0` [options] filename"
+      echo "Usage: $(basename $0) [options] filename"
+      ;;
   esac
 done
 
@@ -53,15 +59,15 @@ cd $HOME
 shift $(($OPTIND - 1))
 if [[ $(platform) == *mingw* ]]; then
   pacman -Syu mingw64/mingw-w64-x86_64-go
-  echo 'export GOPATH='${GO_PATH}>${TOOLSRC}
-  echo 'export PATH=$PATH:'${GO_PATH_BIN}>>${TOOLSRC}
+  echo 'export GOPATH='${GO_PATH} >${TOOLSRC}
+  echo 'export PATH=$PATH:'${GO_PATH_BIN} >>${TOOLSRC}
 else
   SOFT_URL=https://dl.google.com/go/${SOFT_FILE_PACK}
   if [ "$(go version)" != "go version go${GO_VERSION} ${PLATFORM}/${SOFT_ARCH}" ]; then
     $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
     $(cache_unpacker $SOFT_FILE_PACK $SOFT_FILE_NAME)
 
-    rm -rf ${SOFT_HOME} && \
+    rm -rf ${SOFT_HOME} &&
       mv $(cache_folder)/${SOFT_FILE_NAME} ${SOFT_HOME}
   fi
   #--------------new .toolsrc-----------------------
@@ -70,19 +76,19 @@ else
   export PATH=$PATH:${GO_ROOT_BIN}
   export PATH=$PATH:${GO_PATH_BIN}
 
-  echo 'export GOPATH='${GO_PATH}>${TOOLSRC}
-  echo 'export GOROOT='${GO_ROOT}>>${TOOLSRC}
-  echo 'export GO111MODULE=on'>>${TOOLSRC}
-  echo 'export GOPROXY='${GO_PROXY}>>${TOOLSRC}
-  echo 'export PATH=$PATH:'${GO_ROOT_BIN}>>${TOOLSRC}
-  echo 'export PATH=$PATH:'${GO_PATH_BIN}>>${TOOLSRC}
+  echo 'export GOPATH='${GO_PATH} >${TOOLSRC}
+  echo 'export GOROOT='${GO_ROOT} >>${TOOLSRC}
+  echo 'export GO111MODULE=on' >>${TOOLSRC}
+  echo 'export GOPROXY='${GO_PROXY} >>${TOOLSRC}
+  echo 'export PATH=$PATH:'${GO_ROOT_BIN} >>${TOOLSRC}
+  echo 'export PATH=$PATH:'${GO_PATH_BIN} >>${TOOLSRC}
 fi
 #  ----windows bat----
-if [[ $WIN_PATH  ]]; then
+if [[ $WIN_PATH ]]; then
   if [[ $PLATFORM == windows ]]; then
     setx GOROOT $(cygpath -w ${GO_ROOT})
     setx GOPATH $(cygpath -w ${GO_PATH})
-    windowsENV="$(echo -e ${PATH//:/;\\n}';' |sort|uniq|cygpath -w -f -|tr -d '\n')"
+    windowsENV="$(echo -e ${PATH//:/;\\n}';' | sort | uniq | cygpath -w -f - | tr -d '\n')"
     echo $windowsENV
     setx Path "$windowsENV"
   fi
