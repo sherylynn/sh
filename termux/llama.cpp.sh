0934e9b563a833bef19e7c34d5b5c02561e1db97
sherylynn <352281674@qq.com>
2024-12-18 03:05:06 +0800

    dp

--- termux/llama.cpp.sh
+++ termux/llama.cpp.sh
@@ -37,7 +37,9 @@ if [[ $(platform) == *linux* ]]; then
   #  $(cache_downloader $SOFT_FILE_PACK $SOFT_URL)
   pkg install git cmake ccache -y
   # opencl
-  pkg install opencl-headers opencl-clhpp opencl-vendor-driver python -y
+  #pkg install opencl-headers opencl-clhpp opencl-vendor-driver python -y
+  pkg install opencl-headers opencl-vendor-driver python -y
+  #pkg install opencl-headers opencl-clhpp clvk python -y
 
   git clone ${SOFT_GIT_URL} ${SOFT_HOME}
   git pull
