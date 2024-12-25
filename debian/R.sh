aa6884db59c5c1e619db20c9c73fad76ff2d0ae4
sherylynn <352281674@qq.com>
2024-12-25 16:35:28 +0800

    add need for R

--- debian/R.sh
+++ debian/R.sh
@@ -2,4 +2,4 @@ sudo apt-get install r-base -y
 git clone https://github.com/sherylynn/toys ~/toys
 
 #httpgd need
-sudo apt install libfontconfig1-dev libcairo2-dev libtiff-dev -y
+sudo apt install libfontconfig1-dev libcairo2-dev libtiff-dev cargo -y
