3ee5742f1df69602a97f505e8beffd3951bc623b
sherylynn <352281674@qq.com>
2024-12-25 16:35:59 +0800

    update comment

--- debian/R.sh
+++ debian/R.sh
@@ -1,5 +1,5 @@
 sudo apt-get install r-base -y
 git clone https://github.com/sherylynn/toys ~/toys
 
-#httpgd need
+#httpgd  R package need
 sudo apt install libfontconfig1-dev libcairo2-dev libtiff-dev cargo -y
