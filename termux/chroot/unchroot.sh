f744fc8f81ea843587bb3422a20d43ff6be99025
sherylynn <352281674@qq.com>
2025-01-15 09:32:55 +0800

    add kill termux-x11 for unchroot

--- termux/chroot/unchroot.sh
+++ termux/chroot/unchroot.sh
@@ -6,9 +6,14 @@
 # Kill all old prcoesses for umount tmp
 sudo killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock virgl_test_server
 
+#
+pkill -f com.termux.x11
+am broadcast -a com.termux.x11.ACTION_STOP -p com.termux.x11
+
 if [ -f ~/tools/rurima/rurima ]; then
   sudo killall sshd
   sudo killall emacs
+  sudo killall code-server
   sudo rurima ruri -m /sdcard /sdcard -m /data/data/com.termux/files/usr/tmp /tmp -m /dev /dev -m /dev/pts /dev/pts -m /dev/shm /dev/shm -m /sys /sys -m /proc /proc -p $DEBIAN_DIR /bin/su - root -c 'vncserver -kill :0 && vncserver -kill :1 && vncserver -kill :2 && killall Xvnc '
   sudo rurima ruri -U $DEBIAN_DIR
   sudo rurima ruri -U $DEBIAN_DIR
