e7d7a30b15379fcceb194e768a494f4f2ecb972d
sherylynn <352281674@qq.com>
2025-01-21 15:45:30 +0800

    update

--- termux/server_llama.cpp.sh
+++ termux/server_llama.cpp.sh
@@ -28,7 +28,8 @@ cd ../../
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/Qwen2.5-7B-Instruct-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 0 -t 8
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/Qwen2.5-7B-Instruct-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 99 -t 8
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/Qwen2.5-7B-Instruct-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 80 -t 8
-sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/DeepSeek-R1-Distill-Qwen-7B-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 80 -t 8
+#sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/DeepSeek-R1-Distill-Qwen-7B-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 80 -t 8
+sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/DeepSeek-R1-Distill-Qwen-1.5B-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 0 -t 8
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/phi-4-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 80 -t 8
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/phi-4-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 70 -t 8
 #sudo /data/local/tmp/llama.cpp/build/bin/llama-server -m /sdcard/Download/phi-4-Q4_0.gguf --host 0.0.0.0 --port 8888 -ngl 99 -t 8
