3a10a9a1a3c2ba4678700b8f082689d968833d9c
sherylynn <352281674@qq.com>
2025-01-23 07:10:40 +0800

    killall xrdp

--- termux/chroot/unchroot.sh
+++ termux/chroot/unchroot.sh
@@ -11,7 +11,7 @@ pkill -f com.termux.x11
 am broadcast -a com.termux.x11.ACTION_STOP -p com.termux.x11
 
 if [ -f ~/tools/rurima/rurima ]; then
-  sudo killall sshd emacs code-server
+  sudo killall sshd emacs code-server xrdp
   sudo rurima ruri -m /sdcard /sdcard -m /data/data/com.termux/files/usr/tmp /tmp -m /dev /dev -m /dev/pts /dev/pts -m /dev/shm /dev/shm -m /sys /sys -m /proc /proc -p $DEBIAN_DIR /bin/su - root -c 'vncserver -kill :0 && vncserver -kill :1 && vncserver -kill :2 && killall Xvnc '
   sudo rurima ruri -U $DEBIAN_DIR
 elif [ -n "$busybox" ]; then
