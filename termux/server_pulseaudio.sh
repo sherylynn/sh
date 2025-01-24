901e72801e834d6930bd49c34b602d4a49b6f3a1
sherylynn <352281674@qq.com>
2025-01-24 23:21:27 +0800

    又一次失败的尝试

---
+++ termux/server_pulseaudio.sh
@@ -0,0 +1,42 @@
+#!/data/data/com.termux/files/usr/bin/bash
+SCRIPT_NAME="pulseaudio"
+#change bash from /usr/bin/bash to realpath
+realpath() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )/$x
+
+}
+realpathdir() {
+  local x=$1
+  echo $(
+    cd $(dirname $0)
+    pwd
+  )
+
+}
+cd $(realpathdir ./server_${SCRIPT_NAME}.sh)
+pwd
+#load env
+test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
+
+echo $(whoami)
+. ./chroot/cli.sh
+
+# Kill all old prcoesses
+sudo killall -9 pulseaudio
+
+clean_tmp
+
+#从:0 换到:1
+XDG_RUNTIME_DIR=${TMPDIR}
+
+# Start Pulse Audio of Termux
+#pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+#去掉--start来让termux services管理进程
+
+#但是这个东西要求有x环境，不然连不上dbus
+pulseaudio --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
