5b6b74612904fb2b12322565ad8f206934158254
sherylynn <352281674@qq.com>
2025-01-25 13:08:41 +0800

    不知道怎么virgl又行了

--- termux/server_virgl.sh
+++ termux/server_virgl.sh
@@ -20,7 +20,7 @@ realpathdir() {
 cd $(realpathdir ./server_${SCRIPT_NAME}.sh)
 pwd
 #load env
-test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
+#test -f ../../tools/rc/${SCRIPT_NAME}rc && . ../../tools/rc/${SCRIPT_NAME}rc
 
 echo $(whoami)
 . ./chroot/cli.sh
@@ -29,18 +29,20 @@ echo $(whoami)
 #sudo pkill -f "termux-x11|Xwayland|pulseaudio|virgl_test_server_android"
 #sudo killall -9 $(pgrep -f "termux-x11|Xwayland|pulseaudio|virgl_test_server_android")
 sudo killall -9 termux-x11 pulseaudio virgl_test_server_android
-
+sudo pkill -f com.termux.x11
 clean_tmp
-
+sudo setenforce 0
 #从:0 换到:1
-XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1 -ac &
+#XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1 -ac &
+TERMUX_X11_DEBUG=1 termux-x11 :1 -ac -xstartup "virgl_test_server_android"
 
-sleep 3
-
+#sleep 3
+#echo 2
 # Start Pulse Audio of Termux
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
-pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+#pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+#pulseaudio --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 &
+#pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 
 # Start virgl server
 #virgl_test_server_android &
-virgl_test_server_android
+#virgl_test_server_android &
