d29c9a4147e54ca9a12c4ac6f1801c88aee6b8d5
sherylynn <352281674@qq.com>
2025-03-29 10:02:56 +0800

    update android studio

--- win-git/android.sh
+++ win-git/android.sh
@@ -14,7 +14,8 @@ export ANDROID_STUDIO_HOME=$LIBS_HOME
 export ANDROID_ABI=arm64-v8a
 export SDK_HOME=$SOFT_HOME
 export CLASSPATH=.:$LIBS_HOME/jre/lib/tools.jar:$LIBS_HOME/jre/lib/dt.jar:$LIBS_HOME/jre/jre/lib/rt.jar
-export JAVA_HOME=$LIBS_HOME/jre
+#export JAVA_HOME=$LIBS_HOME/jre
+export JAVA_HOME=$LIBS_HOME/jbr
 export GRADLE_HOME=$LIBS_HOME/gradle/gradle-4.10.1
 
 tee $TOOLSRC <<EOF
@@ -24,9 +25,11 @@ export ANDROID_HOME=$SOFT_HOME
 export ANDROID_ABI=$ANDROID_ABI
 export ANDROID_SDK_ROOT=$SOFT_HOME
 export ANDROID_SDK=$SOFT_HOME
-export ANDROID_NDK=$SOFT_HOME/ndk-bundle
+#export ANDROID_NDK=$SOFT_HOME/ndk-bundle
+export ANDROID_NDK=$SOFT_HOME/ndk
 export CLASSPATH=.:$LIBS_HOME/jre/lib/tools.jar:$LIBS_HOME/jre/lib/dt.jar:$LIBS_HOME/jre/jre/lib/rt.jar
-export JAVA_HOME=$LIBS_HOME/jre
+#export JAVA_HOME=$LIBS_HOME/jre
+export JAVA_HOME=$LIBS_HOME/jbr
 export GRADLE_HOME=$LIBS_HOME/gradle/gradle-4.10.1
 export PATH=\$PATH:$GRADLE_HOME/bin:$ANDROID_STUDIO_HOME/jre/jre/bin:$ANDROID_STUDIO_HOME/jre/bin:$ANDROID_STUDIO_HOME/bin:$SDK_HOME/emulator:$SDK_HOME/platform-tools:$ANDROID_STUDIO_HOME/jbr/bin
 EOF
