b22095e687c96c87b312ca7734ea4da78f99fccf
sherylynn <352281674@qq.com>
2025-07-02 00:35:47 +0800

    update

--- win-git/backup.sh
+++ win-git/backup.sh
@@ -7,12 +7,12 @@ cd ~
 
 BACKUP_DIR="$HOME/download"
 if [ -d "/sdcard/Download" ]; then
-    BACKUP_DIR="/sdcard/Download"
+  BACKUP_DIR="/sdcard/Download"
 fi
 mkdir -p "$BACKUP_DIR"
 
 if [ -d "/sdcard/Download" ]; then
-    tar --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config/chromium --exclude=.config/gtk-3.0 --exclude=.config/pulse --exclude=.config/dconf --exclude=.config/htop  --exclude=.x* --exclude=.vnc* --exclude=.cache  -pcvf - ./  |pigz --best > "$BACKUP_DIR"/backup_$(arch).tar.gz
-elif
-    tar --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config/chromium --exclude=.config/gtk-3.0 --exclude=.config/pulse --exclude=.config/dconf --exclude=.config/htop  --exclude=download --exclude=.x* --exclude=.vnc* --exclude=.cache  -pcvf - ./  |pigz --best > "$BACKUP_DIR"/backup_$(arch).tar.gz
+  tar --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config/chromium --exclude=.config/gtk-3.0 --exclude=.config/pulse --exclude=.config/dconf --exclude=.config/htop --exclude=.x* --exclude=.vnc* --exclude=.cache -pcvf - ./ | pigz --best >"$BACKUP_DIR"/backup_$(arch).tar.gz
+else
+  tar --exclude=.gvfs --exclude=.gnupg --exclude=.X* --exclude=.dbus --exclude=.config/chromium --exclude=.config/gtk-3.0 --exclude=.config/pulse --exclude=.config/dconf --exclude=.config/htop --exclude=download --exclude=.x* --exclude=.vnc* --exclude=.cache -pcvf - ./ | pigz --best >"$BACKUP_DIR"/backup_$(arch).tar.gz
 fi
