63a648a360d1990513e5ba05a28dbab64f15d49a
sherylynn <352281674@qq.com>
2026-07-13 22:32:08 +0800

    fix systemd novnc

--- win-git/systemd_noVNC.sh
+++ win-git/systemd_noVNC.sh
@@ -10,27 +10,24 @@ Wants=network-online.target systemd-networkd-wait-online.service
 [Service]
 EOF
 
-sudo tee -a /etc/systemd/system/${SCRIPT_NAME}.service <<EOF
-User=$(whoami)
-;Group=$(id -g -n)
-Type=simple
-;ExecStartPre=/bin/bash --login -c 'env > /tmp/.new-env-file'
-;EnvironmentFile=$HOME/.env.file
-PrivateTmp=true
-Restart=on-abnormal
-ExecStart=$(
+SCRIPT_DIR=$(
   cd "$(dirname "$0")"
   pwd
-)/server_${SCRIPT_NAME}.sh
+)
+
+sudo tee -a /etc/systemd/system/${SCRIPT_NAME}.service <<EOF
+User=$(whoami)
+Type=simple
+WorkingDirectory=${SCRIPT_DIR}
+Environment=DISPLAY=:5
+Environment=HOME=$HOME
+Environment=PATH=/usr/local/bin:/usr/bin:/bin:$HOME/tools/bin
+ProtectSystem=false
+Restart=on-abnormal
+ExecStart=/bin/bash ${SCRIPT_DIR}/server_${SCRIPT_NAME}.sh
 EOF
 
 sudo tee -a /etc/systemd/system/${SCRIPT_NAME}.service <<-'EOF'
-; Hide /home, /root, and /run/user. Nobody will steal your SSH-keys.
-;ProtectHome=true
-;after protect even if this script can not read home
-; Make /usr, /boot, /etc and possibly some more folders read-only.
-ProtectSystem=full
-
 [Install]
 WantedBy=multi-user.target
 EOF
