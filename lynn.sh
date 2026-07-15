0f6c4eb7b0ad6fd90c32e305d10c4978d9da8026
sherylynn <352281674@qq.com>
2026-07-15 14:39:20 +0800

    update

--- lynn.sh
+++ lynn.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/sherylynn/$1 $HOME/$1
+#git clone --depth 1 https://github.com/sherylynn/$1 $HOME/$1
+git clone https://github.com/sherylynn/$1 $HOME/$1
